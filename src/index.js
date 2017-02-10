5f0f58323c67386e0e8faa59adafc1e504ee36ed
cookfront <cookfront@gmail.com>
2017-02-10 16:06:03 +0800

    package.json整理

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import Sample from '../packages/sample/index.js';
 import Button from '../packages/button/index.js';
 import Switch from '../packages/switch/index.js';
-import 'es6-promise/auto';
-import axios from 'axios';
-import foreach from 'lodash/foreach';
-import filter from 'lodash/filter';
-import find from 'lodash/find';
-import map from 'lodash/map';
 // zenui
 import '../packages/zenui/src/index.pcss';
 
@@ -26,13 +20,6 @@ if (typeof window !== 'undefined' && window.Vue) {
 module.exports = {
   install,
   version: '0.0.1',
-  axios,
-  _: {
-    foreach,
-    filter,
-    find,
-    map
-  },
   Sample,
   Button,
   Switch
