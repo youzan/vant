10ed5982f646a169dec7a986f3e51739af84ebcf
cookfront <cookfront@gmail.com>
2017-02-27 21:35:17 +0800

    remove demo block

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -126,31 +126,6 @@ if (process.env.NODE_ENV === 'production') {
               slugify: slugify,
               permalink: true,
               permalinkBefore: true
-            }],
-            [require('markdown-it-container'), 'demo', {
-              validate: function(params) {
-                return params.trim().match(/^demo\s*(.*)$/);
-              },
-
-              render: function(tokens, idx) {
-                var m = tokens[idx].info.trim().match(/^demo\s*(.*)$/);
-                if (tokens[idx].nesting === 1) {
-                  var description = (m && m.length > 1) ? m[1] : '';
-                  var content = tokens[idx + 1].content;
-                  var html = convert(striptags.strip(content, ['script', 'style']));
-                  var script = striptags.fetch(content, 'script');
-                  var style = striptags.fetch(content, 'style');
-                  var descriptionHTML = description
-                    ? md.render(description)
-                    : '';
-
-                  return `<demo-block class="demo-box">
-                            <div class="source" slot="source">${html}</div>
-                            ${descriptionHTML}
-                            <div class="highlight" slot="highlight">`;
-                }
-                return '</div></demo-block>\n';
-              }
             }]
           ],
           preprocess: function(MarkdownIt, source) {
