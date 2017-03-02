98d1bc7157bc0d949e361b99a49b27f2a68b8ac5
cookfront <cookfront@gmail.com>
2017-03-02 19:37:10 +0800

    step component

--- docs/examples/steps.vue
+++ docs/examples/steps.vue
@@ -6,7 +6,6 @@
     <zan-steps :active="active" icon="certificate" icon-class="steps-success" title="等待商家发货" description="等待商家发货等待商家发货等待商家发货等待商家发货等待商家发货">
       <zan-step>买家下单</zan-step>
       <zan-step>商家接单</zan-step>
-      <zan-step>买家提货</zan-step>
       <zan-step>交易完成</zan-step>
     </zan-steps>
     <zan-button @click="nextStep">下一步</zan-button>
@@ -14,7 +13,6 @@
     <h2 class="page-sub-title">只显示步骤条</h2>
     <zan-steps :active="active">
       <zan-step>买家下单</zan-step>
-      <zan-step>商家接单</zan-step>
       <zan-step>买家提货</zan-step>
       <zan-step>交易完成</zan-step>
     </zan-steps>
