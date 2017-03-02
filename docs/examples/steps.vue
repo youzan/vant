c8f7888dd21d6cbf7db33b7ebdd7357ca9ba372d
cookfront <cookfront@gmail.com>
2017-03-02 13:59:27 +0800

    step component

--- docs/examples/steps.vue
+++ docs/examples/steps.vue
@@ -3,7 +3,7 @@
     <h1 class="page-title">Steps</h1>
 
     <h2 class="page-sub-title">基础用法</h2>
-    <zan-steps icon="topay">
+    <zan-steps icon="certificate" icon-class="steps-success" title="等待商家发货" description="等待商家发货">
     </zan-steps>
   </div>
 </template>
@@ -11,3 +11,9 @@
 <script>
 
 </script>
+
+<style>
+.steps-success {
+  color: #06bf04;
+}
+</style>
--- packages/steps/src/steps.vue
+++ packages/steps/src/steps.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="zan-steps">
-    <div class="zan-steps__desc" v-if="icon">
+    <div class="zan-steps__status" v-if="icon">
       <i class="zan-icon" :class="computedIconClass"></i>
+      <div class="zan-steps__message">
+        <div class="zan-steps__message-wrapper">
+          <h4 class="zan-steps__title" v-text="title"></h4>
+          <p class="zan-steps__desc" v-text="description"></p>
+        </div>
+      </div>
     </div>
     <div class="zan-steps__items">
       <slot></slot>
@@ -16,19 +22,22 @@ export default {
   props: {
     active: Number,
     icon: String,
-    // iconClass: String,
+    iconClass: {
+      type: String,
+      default: ''
+    },
     title: String,
     description: String
   },
 
   computed: {
-    // computedIconClass() {
-    //   let iconName = `zan-icon-${this.icon}`;
-    //   let result = (iconClass && iconClass.split(' ')) || [];
-    //   result.push(iconName);
+    computedIconClass() {
+      let iconName = `zan-icon-${this.icon}`;
+      let result = this.iconClass.split(' ');
+      result.push(iconName);
 
-    //   return result;
-    // }
+      return result;
+    }
   }
 };
 </script>
