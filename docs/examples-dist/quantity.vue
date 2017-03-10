09467b6d9066d6a832b07967f9b33efa53d1476b
cookfront <cookfront@gmail.com>
2017-03-10 15:10:04 +0800

    build docs

---
+++ docs/examples-dist/quantity.vue
@@ -0,0 +1,36 @@
+<template><section class="demo-quantity"><h1 class="demo-title">quantity</h1><example-block title="基础用法">
+                <zan-quantity v-model="quantity1"></zan-quantity>
+<p class="curr-quantity">当前值：{{ quantity1 }}</p>
+
+              </example-block><example-block title="禁用Quantity">
+                <zan-quantity v-model="quantity1" disabled></zan-quantity>
+
+              </example-block><example-block title="高级用法">
+                <zan-quantity v-model="quantity2" min="5" max="40" step="2" default-value="9"></zan-quantity>
+<p class="curr-quantity">当前值：{{ quantity2 }}</p>
+
+              </example-block></section></template>
+<style>
+@component-namespace demo {
+  @b quantity {
+    .zan-quantity {
+      margin: 15px;
+    }
+
+    .curr-quantity {
+      margin: 15px;
+    }
+  }
+}
+</style>
+<script>
+import Vue from "vue";import ExampleBlock from "../components/example-block";Vue.component("example-block", ExampleBlock);
+export default {
+  data() {
+    return {
+      quantity1: 1,
+      quantity2: null,
+    };
+  }
+};
+</script>
