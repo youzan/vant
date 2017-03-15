cf8b8dab2d21ce9dbf00f7beacd3fd8d6cbb06cd
cookfront <cookfront@gmail.com>
2017-03-15 17:25:28 +0800

    swipe components

--- docs/examples-dist/swipe.vue
+++ docs/examples-dist/swipe.vue
@@ -8,12 +8,26 @@
   </zan-swipe-item>
 </zan-swipe>
 
+              </example-block><example-block title="自动轮播">
+                <zan-swipe :auto-play="true">
+  <zan-swipe-item>
+    <img src="https://img.yzcdn.cn/upload_files/2017/03/14/FmTPs0SeyQaAOSK1rRe1sL8RcwSY.jpeg?imageView2/2/w/980/h/980/q/75/format/webp" alt="">
+  </zan-swipe-item>
+  <zan-swipe-item>
+    <img src="https://img.yzcdn.cn/upload_files/2017/03/14/FmTPs0SeyQaAOSK1rRe1sL8RcwSY.jpeg?imageView2/2/w/980/h/980/q/75/format/webp" alt="">
+  </zan-swipe-item>
+</zan-swipe>
+
               </example-block></section></template>
 <style>
 @component-namespace demo {
   @b swipe {
     .zan-swipe {
       height: 200px;
+
+      img {
+        width: 100%;
+      }
     }
   }
 }
---
+++ docs/examples-dist/uploader.vue
@@ -0,0 +1,29 @@
+<template><section class="demo-uploader"><h1 class="demo-title">uploader</h1><example-block title="基础用法">
+                <div class="uploader-container">
+    <zan-uploader :before-read="logContent" @file-readed="logContent">
+    </zan-uploader>
+</div>
+
+              </example-block><example-block title="自定义上传图标">
+                <div class="uploader-container">
+  <zan-uploader @file-readed="logContent">
+    <zan-icon name="camera"></zan-icon>
+  </zan-uploader>
+</div>
+
+              </example-block></section></template>
+<style>
+.uploader-container {
+  padding: 5px 15px;
+}
+</style>
+<script>
+import Vue from "vue";import ExampleBlock from "../components/example-block";Vue.component("example-block", ExampleBlock);
+export default {
+  methods: {
+    logContent(file) {
+      console.log(file)
+    }
+  }
+};  
+</script>
