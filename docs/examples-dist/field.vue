f7be36edcbf936765d6cd52bf934e2a3380b0975
jiangruowei <jiangruowei@youzan.com>
2017-03-22 17:10:01 +0800

    chore: rebuild docs

--- docs/examples-dist/field.vue
+++ docs/examples-dist/field.vue
@@ -1,8 +1,8 @@
 <template><section class="demo-field"><h1 class="demo-title">field</h1><example-block title="基础用法">
                 <zan-cell-group>
-  <zan-field type="text" label="用户名：" placeholder="请输入用户名" v-model="username"></zan-field>
-  <zan-field type="password" label="密码：" placeholder="请输入密码"></zan-field>
-  <zan-field type="textarea" label="个人介绍：" placeholder="请输入个人介绍"></zan-field>
+  <zan-field type="text" label="用户名：" placeholder="请输入用户名" v-model="username" required></zan-field>
+  <zan-field type="password" label="密码：" placeholder="请输入密码" required></zan-field>
+  <zan-field type="textarea" label="个人介绍：" placeholder="请输入个人介绍" required></zan-field>
 </zan-cell-group>
 
               </example-block><example-block title="无label的输入框">
@@ -23,6 +23,11 @@
               </example-block><example-block title="错误的输入框">
                 <zan-cell-group>
   <zan-field label="用户名：" type="text" placeholder="请输入用户名" error=""></zan-field>
+</zan-cell-group>
+
+              </example-block><example-block title="错误的输入框">
+                <zan-cell-group>
+  <zan-field label="留言：" type="textarea" placeholder="请输入留言" rows="1" autosize=""></zan-field>
 </zan-cell-group>
 
               </example-block></section></template>
--- docs/examples-dist/toast.vue
+++ docs/examples-dist/toast.vue
@@ -3,7 +3,19 @@
 <zan-button @click="showLoadingToast">加载Toast</zan-button>
 <zan-button @click="showSuccessToast">成功</zan-button>
 <zan-button @click="showFailToast">失败</zan-button>
+<zan-button @click="showForbidClickToast">背景不能点击</zan-button>
 <zan-button @click="showCustomizedToast(5000)">倒数5秒</zan-button>
+
+
+
+              </example-block><example-block title="手动关闭">
+                <zan-button @click="showToast">打开</zan-button>
+<zan-button @click="closeToast">关闭</zan-button>
+
+
+
+              </example-block><example-block title="手动关闭">
+                <zan-button @click="showHtmlToast">打开</zan-button>
 
 
 
@@ -35,6 +47,12 @@ export default {
     showFailToast() {
       Toast.fail('失败文案');
     },
+    showForbidClickToast() {
+      Toast({
+        message: '背景不能点击',
+        forbidClick: true
+      })
+    },
     showCustomizedToast(duration) {
       let leftSec = duration / 1000;
       let toast = Toast({
@@ -50,6 +68,18 @@ export default {
         }
         toast.message = (--leftSec).toString();
       }, 1000);
+    },
+    showToast() {
+      this.toast = Toast('我是提示文案，建议不超过十五字~');
+    },
+    closeToast() {
+      this.toast.clear();
+    },
+    showHtmlToast() {
+      Toast({
+        type: 'html',
+        message: '<em>HTML<em>'
+      })
     }
   }
 };
