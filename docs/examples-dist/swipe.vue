a6b4fe5a7b50d1d27b095a593a12a1bde44150b9
cookfront <cookfront@gmail.com>
2017-04-05 14:34:32 +0800

    lazyload

--- docs/examples-dist/swipe.vue
+++ docs/examples-dist/swipe.vue
@@ -1,20 +1,14 @@
 <template><section class="demo-swipe"><h1 class="demo-title">swipe</h1><example-block title="基础用法">
                 <zan-swipe>
-  <zan-swipe-item>
-    <img src="https://img.yzcdn.cn/upload_files/2017/03/14/FmTPs0SeyQaAOSK1rRe1sL8RcwSY.jpeg?imageView2/2/w/980/h/980/q/75/format/webp" alt="">
-  </zan-swipe-item>
-  <zan-swipe-item>
-    <img src="https://img.yzcdn.cn/upload_files/2017/03/15/FvexrWlG_WxtCE9Omo5l27n_mAG_.jpeg?imageView2/2/w/980/h/980/q/75/format/webp" alt="">
+  <zan-swipe-item v-for="img in images">
+    <img v-lazy="img" alt="">
   </zan-swipe-item>
 </zan-swipe>
 
               </example-block><example-block title="自动轮播">
-                <zan-swipe :auto-play="true" @pagechange:end="handlePageEnd">
-  <zan-swipe-item>
-    <img src="https://img.yzcdn.cn/upload_files/2017/03/14/FmTPs0SeyQaAOSK1rRe1sL8RcwSY.jpeg?imageView2/2/w/980/h/980/q/75/format/webp" alt="">
-  </zan-swipe-item>
-  <zan-swipe-item>
-    <img src="https://img.yzcdn.cn/upload_files/2017/03/15/FvexrWlG_WxtCE9Omo5l27n_mAG_.jpeg?imageView2/2/w/980/h/980/q/75/format/webp" alt="">
+                <zan-swipe auto-play="" @pagechange:end="handlePageEnd">
+  <zan-swipe-item v-for="img in autoImages">
+    <img v-lazy="img" alt="">
   </zan-swipe-item>
 </zan-swipe>
 
@@ -35,6 +29,19 @@
 <script>
 import Vue from "vue";import ExampleBlock from "../components/example-block";Vue.component("example-block", ExampleBlock);
 export default {
+  data() {
+    return {
+      autoImages: [
+        'https://img.yzcdn.cn/upload_files/2017/03/09/FvkZahKoq1vkxLQFdVWeLf2UCqDz.png',
+        'https://img.yzcdn.cn/upload_files/2017/03/09/Fk0rpe_svu9d5Xk3MUCWd1QeMXOu.png'
+      ],
+      images: [
+        'https://img.yzcdn.cn/upload_files/2017/03/14/FmTPs0SeyQaAOSK1rRe1sL8RcwSY.jpeg',
+        'https://img.yzcdn.cn/upload_files/2017/03/15/FvexrWlG_WxtCE9Omo5l27n_mAG_.jpeg'
+      ]
+    };
+  },
+
   methods: {
     handlePageEnd(page, index) {
       console.log(page, index);
