6c27dd5e617233b1722a97d6087b0dee7dfed86b
cookfront <cookfront@gmail.com>
2017-04-14 17:25:10 +0800

    badge unit test

--- test/unit/specs/badge.spec.js
+++ test/unit/specs/badge.spec.js
@@ -1,6 +1,14 @@
-// import Badge from 'packages/badge';
+import Vue from 'vue';
+import Badge from 'packages/badge';
 import BadgeGroup from 'packages/badge-group';
 import { mount } from 'avoriaz';
+
+Vue.component(Badge.name, Badge);
+Vue.component(BadgeGroup.name, BadgeGroup);
+
+const component = Vue.component({
+
+});
 
 describe('BadgeGroup', () => {
   let wrapper;
@@ -14,5 +22,22 @@ describe('BadgeGroup', () => {
     expect(wrapper.hasClass('zan-badge-group')).to.be.true;
     expect(wrapper.instance().activeKey).to.equal(0);
     expect(wrapper.data().badges.length).to.equal(0);
+  });
+});
+
+describe('Badge', () => {
+  let wrapper;
+  afterEach(() => {
+    wrapper && wrapper.destroy();
+  });
+
+  it('create a badge', () => {
+    wrapper = mount(Badge, {
+      propsData: {
+        title: 'test'
+      }
+    });
+
+    expect(wrapper.hasClass('zan-badge')).to.be.true;
   });
 });
