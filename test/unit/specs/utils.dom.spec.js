cae39b4e0c96b7171d680badfc322c04eda89641
cookfront <cookfront@gmail.com>
2017-04-25 19:42:12 +0800

    unit test

---
+++ test/unit/specs/utils.dom.spec.js
@@ -0,0 +1,29 @@
+import { hasClass, addClass, removeClass } from 'src/utils/dom';
+
+describe('Utils Dom', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = document.createElement('div');
+    wrapper.classList.add('test-class');
+    document.body.appendChild(wrapper);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(wrapper);
+  });
+
+  it('hasClass', () => {
+    expect(hasClass(wrapper, 'test-class')).to.be.true;
+    expect(hasClass()).to.be.false;
+  });
+
+  it('addClass and removeClass', () => {
+    expect(hasClass(wrapper, 'test-class')).to.be.true;
+
+    addClass(wrapper, 'other-class');
+    expect(hasClass(wrapper, 'other-class')).to.be.true;
+
+    removeClass(wrapper, 'other-class');
+    expect(hasClass(wrapper, 'other-class')).to.be.false;
+  });
+});
