78da8ff37f9849e9d59e943fb4b1f455436fe036
neverland <chenjiahan@buaa.edu.cn>
2017-07-20 14:35:08 +0800

    文档改版 (#55)

--- docs/src/ExamplesDocsApp.vue
+++ docs/src/ExamplesDocsApp.vue
@@ -1,40 +1,30 @@
 <template>
   <div class="app">
-    <page-header></page-header>
-    <div class="main-content">
-      <div class="page-container clearfix">
-        <side-nav :data="navConfig['zh-CN']" base="/component"></side-nav>
-        <div class="page-content">
-          <router-view></router-view>
-          <footer-nav></footer-nav>
-        </div>
-      </div>
-    </div>
-    <page-footer></page-footer>
+    <zan-doc :simulator="simulatorSrc" :config="config">
+      <router-view></router-view>
+    </zan-doc>
   </div>
 </template>
 
 <script>
-import Vue from 'vue';
 import 'highlight.js/styles/color-brewer.css';
-import navConfig from './nav.config.js';
-import SideNav from './components/side-nav';
-import DemoBlock from './components/demo-block';
-import FooterNav from './components/footer-nav';
-import PageHeader from './components/page-header';
-import PageFooter from './components/page-footer';
-
-Vue.component('side-nav', SideNav);
-Vue.component('demo-block', DemoBlock);
-Vue.component('footer-nav', FooterNav);
-Vue.component('page-header', PageHeader);
-Vue.component('page-footer', PageFooter);
+import docConfig from './doc.config';
 
 export default {
   data() {
     return {
-      navConfig: navConfig
+      config: docConfig['zh-CN']
     };
+  },
+  computed: {
+    simulatorSrc() {
+      const dir = this.$route.path.split('/').pop();
+      if (dir === 'quickstart' || dir === 'changelog') {
+        return '/zanui/vue/examples';
+      } else {
+        return `/zanui/vue/examples/component/${dir}`;
+      }
+    }
   }
 };
 </script>
