27b09f3f24955b344002abb152deefc78868e2c3
陈嘉涵 <chenjiahan@meituan.com>
2017-07-26 18:51:36 +0800

    fix: build vant css

--- packages/vant-css/gulpfile.js
+++ packages/vant-css/gulpfile.js
@@ -1,11 +1,10 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var cssmin = require('gulp-cssmin');
-var postcssPlugin = require('../../build/utils/postcss_pipe')();
 
 gulp.task('compile', function() {
   return gulp.src('./src/*.css')
-    .pipe(postcss(postcssPlugin))
+    .pipe(postcss())
     .pipe(cssmin())
     .pipe(gulp.dest('./lib'));
 });
