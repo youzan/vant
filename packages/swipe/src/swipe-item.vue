cc3aad3802a7dbf947421bc75b550021977c3af3
Raistlin916 <raistlin916@gmail.com>
2017-08-09 12:15:18 +0800

    fix: one page swipe error

--- packages/swipe/src/swipe-item.vue
+++ packages/swipe/src/swipe-item.vue
@@ -10,6 +10,13 @@ export default {
 
   beforeCreate() {
     this.$parent.swipes.push(this);
+  },
+
+  destroyed() {
+    const index = this.$parent.swipes.indexOf(this)
+    if (index > -1) {
+      this.$parent.swipes.splice(index, 1)
+    }
   }
 };
 </script>
