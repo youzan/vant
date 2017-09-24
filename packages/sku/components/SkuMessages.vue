f91f1984928d84f25ba74b1dbdcd51bdcff14401
niunai <colonD.wny@gmail.com>
2017-09-24 12:58:38 +0800

    fix: 漏改一处

--- packages/sku/components/SkuMessages.vue
+++ packages/sku/components/SkuMessages.vue
@@ -11,7 +11,7 @@
         v-model="messageValues[index]">
       </van-field>
       <van-field v-else
-        :key="goodsId + '-' + index"
+        :key="`${goodsId}-${index}`"
         :required="message.required == '1'"
         :label="message.name"
         :placeholder="placeholderMap[message.type]"
