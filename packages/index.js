435511e23de1f2a29a51012de173fe8be792a6ae
pangxie1991 <chenyao1102@gmail.com>
2017-09-26 20:13:56 +0800

    [build] 0.9.9

--- packages/index.js
+++ packages/index.js
@@ -53,7 +53,7 @@ import TreeSelect from './tree-select';
 import Uploader from './uploader';
 import Waterfall from './waterfall';
 
-const version = '0.9.8';
+const version = '0.9.9';
 const components = [
   Actionsheet,
   AddressList,
