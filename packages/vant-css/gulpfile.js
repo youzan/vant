cd9105ed7ed8d0d31be6a381873fcfcea2bcc5b3
陈嘉涵 <chenjiahan@meituan.com>
2017-09-29 17:24:54 +0800

    fix: use clean-css instead of gulp-cssmin

--- packages/vant-css/gulpfile.js
+++ packages/vant-css/gulpfile.js
@@ -1,8 +1,8 @@
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var cssmin = require('gulp-cssmin');
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const cssmin = require('gulp-clean-css');
 
-gulp.task('compile', function() {
+gulp.task('compile', () => {
   return gulp.src('./src/*.css')
     .pipe(postcss())
     .pipe(cssmin())
