560ccfdec269f9cb035f52fb21612169bf531e2b
neverland <chenjiahan@buaa.edu.cn>
2017-10-12 06:22:12 -0500

    [breaking change] reconstruct Swipe component (#194)
    * Swipe: reconstruct
    
    * [bugfix]: Swipe autoplay
    
    [bugfix] AddressEdit doc link

--- docs/src/ExamplesApp.vue
+++ docs/src/ExamplesApp.vue
@@ -4,6 +4,10 @@
 
 <style lang="postcss">
 body {
+  color: #333;
+  line-height: 1;
+  background-color: #f8f8f8;
+  font-family: Arial, Helvetica, "STHeiti STXihei", "Microsoft YaHei", Tohoma, sans-serif;
   -webkit-font-smoothing: antialiased;
 }
 </style>
