41320f963009a524bb767ed2b42610433ada57af
陈嘉涵 <chenjiahan@youzan.com>
2017-11-09 15:09:00 +0800

    [new feature] add spinner type loading

--- packages/loading/index.vue
+++ packages/loading/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <div :class="['van-loading', 'van-loading--' + type]">
-    <span :class="['van-loading__spinner', 'van-loading__spinner--' + type, 'van-loading__spinner--' + color]"></span>
+  <div :class="['van-loading', 'van-loading--' + type, 'van-loading--' + color]">
+    <span :class="['van-loading__spinner', 'van-loading__spinner--' + type, 'van-loading__spinner--' + color]">
+      <i v-if="type === 'spinner'" v-for="item in 12"></i>
+    </span>
   </div>
 </template>
 
 <script>
-const VALID_TYPES = ['gradient-circle', 'circle'];
+const VALID_TYPES = ['gradient-circle', 'circle', 'spinner'];
 const VALID_COLORS = ['black', 'white'];
 
 export default {
