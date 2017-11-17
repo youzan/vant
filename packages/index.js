badb49e91af61afc68f76ebea06e18dcf79244b3
陈嘉涵 <chenjiahan@youzan.com>
2017-11-17 15:15:43 +0800

    [build] 0.11.0

--- packages/index.js
+++ packages/index.js
@@ -62,7 +62,7 @@ import TreeSelect from './tree-select';
 import Uploader from './uploader';
 import Waterfall from './waterfall';
 
-const version = '0.10.9';
+const version = '0.11.0';
 const components = [
   Actionsheet,
   AddressEdit,
