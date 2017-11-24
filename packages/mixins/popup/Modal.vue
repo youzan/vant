0ac2585ca68897d207a665a99626c978f8ac21f5
陈嘉涵 <chenjiahan@youzan.com>
2017-11-24 11:48:48 +0800

    [bugfix] Popup overlay style update

---
+++ packages/mixins/popup/Modal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="van-modal"
+    :class="className"
+    :style="style"
+    @touchmove.prevent.stop
+    @click="$emit('click', $event)"
+  />
+</template>
+
+<script>
+export default {
+  name: 'van-modal',
+
+  props: {
+    zIndex: Number,
+    className: String,
+    customStyle: Object,
+  },
+
+  computed: {
+    style() {
+      return {
+        zIndex: this.zIndex,
+        ...this.customStyle
+      }
+    }
+  }
+};
+</script>
