232b97d6a94f36b2fc57a9fd94e3c8c9fa9c1aa5
niunai <niunai@youzan.com>
2017-11-24 16:57:46 +0800

    [build] 0.11.2

--- packages/index.js
+++ packages/index.js
@@ -62,7 +62,7 @@ import TreeSelect from './tree-select';
 import Uploader from './uploader';
 import Waterfall from './waterfall';
 
-const version = '0.11.1';
+const version = '0.11.2';
 const components = [
   Actionsheet,
   AddressEdit,
