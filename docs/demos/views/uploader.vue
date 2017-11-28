12ec70b30e656cc3ba7cc67a34ee26901659d8a2
陈嘉涵 <chenjiahan@youzan.com>
2017-11-28 10:32:36 +0800

    [new feature] Uploader support inherit attrs

--- docs/demos/views/uploader.vue
+++ docs/demos/views/uploader.vue
@@ -7,11 +7,28 @@
         </van-uploader>
       </div>
     </demo-block>
+
+    <demo-block :title="$t('title2')">
+      <div class="demo-uploader-container">
+        <van-uploader :after-read="logContent" accept="image/gif, image/jpeg">
+          <van-icon name="photograph" />
+        </van-uploader>
+      </div>
+    </demo-block>
   </demo-section>
 </template>
 
 <script>
 export default {
+  i18n: {
+    'zh-CN': {
+      title2: '设置 input 属性'
+    },
+    'en-US': {
+      title2: 'Set input attr'
+    }
+  },
+
   methods: {
     logContent(file) {
       console.log(file);
@@ -19,7 +36,6 @@ export default {
   }
 };
 </script>
-
 
 <style lang="postcss">
 .demo-uploader {
