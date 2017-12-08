e99b5e2b9eee93984d26b96567893712429090c7
陈嘉涵 <chenjiahan@youzan.com>
2017-12-08 16:07:40 +0800

    fix

--- packages/locale/index.js
+++ packages/locale/index.js
@@ -1,10 +1,29 @@
-import Vue from 'vue';
+mport Vue from 'vue';
+import { get, camelize } from '../utils';
 import deepAssign from '../utils/deep-assign';
 import defaultMessages from './lang/zh-CN';
+
+// component mixin
+const i18n = {
+  computed: {
+    $t() {
+      const { name } = this.$options;
+      const prefix = name ? camelize(name) + '.' : '';
+      const messages = this.$vantMessages[this.$vantLang];
+
+      return (path, ...args) => {
+        const message = get(messages, prefix + path) || get(messages, path);
+        return typeof message === 'function' ? message.apply(null, args) : message;
+      };
+    }
+  }
+};
 
 const proto = Vue.prototype;
 const defaultLang = 'zh-CN';
 const locale = {
+  i18n,
+
   init() {
     Vue.util.defineReactive(proto, '$vantLang', defaultLang);
     Vue.util.defineReactive(proto, '$vantMessages', { [defaultLang]: defaultMessages });
@@ -22,3 +41,4 @@ const locale = {
 
 locale.init();
 export default locale;
+export { i18n };
