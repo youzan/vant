1b2ffa8b8fe660d4f6646a349ea03fc5a515c2bc
陈嘉涵 <chenjiahan@youzan.com>
2017-12-14 16:13:21 +0800

    fix: pullRefresh test cases

--- test/unit/specs/pull-refresh.spec.js
+++ test/unit/specs/pull-refresh.spec.js
@@ -25,20 +25,21 @@ describe('PullRefresh', () => {
       }
     });
 
-    triggerTouch(wrapper, 'touchstart', 0, 0);
-    triggerTouch(wrapper, 'touchmove', 0, 10);
+    const track = wrapper.find('.van-pull-refresh__track')[0];
+    triggerTouch(track, 'touchstart', 0, 0);
+    triggerTouch(track, 'touchmove', 0, 10);
 
     wrapper.vm.$nextTick(() => {
       expect(wrapper.find('.van-pull-refresh__text')[0].text()).to.equal('下拉即可刷新...');
 
-      triggerTouch(wrapper, 'touchmove', 0, 30);
-      triggerTouch(wrapper, 'touchmove', 0, 60);
-      triggerTouch(wrapper, 'touchmove', 0, 100);
+      triggerTouch(track, 'touchmove', 0, 30);
+      triggerTouch(track, 'touchmove', 0, 60);
+      triggerTouch(track, 'touchmove', 0, 100);
 
       wrapper.vm.$nextTick(() => {
         expect(wrapper.find('.van-pull-refresh__text')[0].text()).to.equal('释放即可刷新...');
 
-        triggerTouch(wrapper, 'touchend', 0, 100);
+        triggerTouch(track, 'touchend', 0, 100);
 
         wrapper.vm.$nextTick(() => {
           expect(wrapper.find('.van-pull-refresh__loading span')[1].text()).to.equal('加载中...');
@@ -67,17 +68,18 @@ describe('PullRefresh', () => {
       }, 30);
     });
 
-    triggerTouch(wrapper, 'touchstart', 0, 0);
-    triggerTouch(wrapper, 'touchmove', 0, 100);
-    triggerTouch(wrapper, 'touchend', 0, 100);
+    const track = wrapper.find('.van-pull-refresh__track')[0];
+    triggerTouch(track, 'touchstart', 0, 0);
+    triggerTouch(track, 'touchmove', 0, 100);
+    triggerTouch(track, 'touchend', 0, 100);
 
     expect(wrapper.vm.value).to.be.true;
     expect(wrapper.vm.status).to.equal('loading');
 
     // ignore touch event when loading
-    triggerTouch(wrapper, 'touchstart', 0, 0);
-    triggerTouch(wrapper, 'touchmove', 0, 100);
-    triggerTouch(wrapper, 'touchend', 0, 100);
+    triggerTouch(track, 'touchstart', 0, 0);
+    triggerTouch(track, 'touchmove', 0, 100);
+    triggerTouch(track, 'touchend', 0, 100);
   });
 
   it('pull a short distance', () => {
@@ -87,9 +89,10 @@ describe('PullRefresh', () => {
       }
     });
 
-    triggerTouch(wrapper, 'touchstart', 0, 0);
-    triggerTouch(wrapper, 'touchmove', 0, 10);
-    triggerTouch(wrapper, 'touchend', 0, 10);
+    const track = wrapper.find('.van-pull-refresh__track')[0];
+    triggerTouch(track, 'touchstart', 0, 0);
+    triggerTouch(track, 'touchmove', 0, 10);
+    triggerTouch(track, 'touchend', 0, 10);
 
     expect(wrapper.vm.value).to.be.false;
     expect(wrapper.vm.status).to.equal('normal');
@@ -104,14 +107,15 @@ describe('PullRefresh', () => {
 
     window.scrollTop = 100;
 
+    const track = wrapper.find('.van-pull-refresh__track')[0];
     // ignore touch event when not at page top
-    triggerTouch(wrapper, 'touchstart', 0, 0);
-    triggerTouch(wrapper, 'touchmove', 0, 100);
-    triggerTouch(wrapper, 'touchend', 0, 100);
+    triggerTouch(track, 'touchstart', 0, 0);
+    triggerTouch(track, 'touchmove', 0, 100);
+    triggerTouch(track, 'touchend', 0, 100);
     expect(wrapper.vm.ceiling).to.be.false;
 
     window.scrollTop = 0;
-    triggerTouch(wrapper, 'touchmove', 0, 100);
+    triggerTouch(track, 'touchmove', 0, 100);
     expect(wrapper.vm.ceiling).to.be.true;
   });
 
@@ -121,9 +125,11 @@ describe('PullRefresh', () => {
         value: false
       }
     });
-    triggerTouch(wrapper, 'touchstart', 0, 0);
-    triggerTouch(wrapper, 'touchmove', 10, 0);
-    triggerTouch(wrapper, 'touchend', 10, 0);
+
+    const track = wrapper.find('.van-pull-refresh__track')[0];
+    triggerTouch(track, 'touchstart', 0, 0);
+    triggerTouch(track, 'touchmove', 10, 0);
+    triggerTouch(track, 'touchend', 10, 0);
     expect(wrapper.vm.direction).to.equal('horizontal');
   });
 });
