d845bb8730873ed51eeb0b593d876a71e07f80b4
neverland <chenjiahan@buaa.edu.cn>
2017-12-18 11:45:44 +0800

    [new feature] GoodsAction: support  routerLink & info (#448)

--- docs/demos/views/goods-action.vue
+++ docs/demos/views/goods-action.vue
@@ -2,18 +2,20 @@
   <demo-section>
     <demo-block :title="$t('basicUsage')">
       <van-goods-action>
-        <van-goods-action-mini-btn icon="chat" @click="onClickMiniBtn">
-          {{ $t('button1') }}
-        </van-goods-action-mini-btn>
-        <van-goods-action-mini-btn icon="cart" @click="onClickMiniBtn">
-          {{ $t('button2') }}
-        </van-goods-action-mini-btn>
-        <van-goods-action-big-btn @click="onClickBigBtn">
-          {{ $t('button3') }}
-        </van-goods-action-big-btn>
-        <van-goods-action-big-btn @click="onClickBigBtn" primary>
-          {{ $t('button4') }}
-        </van-goods-action-big-btn>
+        <van-goods-action-mini-btn icon="chat" :text="$t('icon1')" @click="onClickMiniBtn" />
+        <van-goods-action-mini-btn icon="cart" :text="$t('icon2')" @click="onClickMiniBtn" />
+        <van-goods-action-big-btn :text="$t('button1')" @click="onClickBigBtn" />
+        <van-goods-action-big-btn :text="$t('button2')" @click="onClickBigBtn" primary />
+      </van-goods-action>
+    </demo-block>
+
+    <demo-block :title="$t('title2')">
+      <van-goods-action>
+        <van-goods-action-mini-btn icon="chat" :text="$t('icon1')" @click="onClickMiniBtn" />
+        <van-goods-action-mini-btn icon="cart" info="5" :text="$t('icon2')" @click="onClickMiniBtn" />
+        <van-goods-action-mini-btn icon="shop" :text="$t('icon3')" @click="onClickMiniBtn" />
+        <van-goods-action-big-btn :text="$t('button1')" @click="onClickBigBtn" />
+        <van-goods-action-big-btn :text="$t('button2')" @click="onClickBigBtn" primary />
       </van-goods-action>
     </demo-block>
   </demo-section>
@@ -25,18 +27,22 @@ export default {
     'zh-CN': {
       clickIcon: '点击图标',
       clickButton: '点击按钮',
-      button1: '客服',
-      button2: '购物车',
-      button3: '加入购物车',
-      button4: '立即购买'
+      icon1: '客服',
+      icon2: '购物车',
+      icon3: '店铺',
+      button1: '加入购物车',
+      button2: '立即购买',
+      title2: '图标提示'
     },
     'en-US': {
       clickIcon: 'Click Icon',
       clickButton: 'Click Button',
-      button1: 'Icon1',
-      button2: 'Icon2',
-      button3: 'Button1',
-      button4: 'Button2'
+      icon1: 'Icon1',
+      icon2: 'Icon2',
+      icon3: 'Icon3',
+      button1: 'Button1',
+      button2: 'Button2',
+      title2: 'Icon info'
     }
   },
 
