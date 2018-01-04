bf64b570956d48a1249cff0e08a3857a86956b12
陈嘉涵 <chenjiahan@youzan.com>
2018-01-04 17:15:52 +0800

    fix

--- packages/cell/index.vue
+++ packages/cell/index.vue
@@ -8,7 +8,7 @@
     }"
     @click="onClick"
   >
-    <div class="van-cell__title" v-if="$slots.title || title || icon || $slots.icon">
+    <div class="van-cell__title" v-if="title || icon || $slots.title || $slots.icon">
       <slot name="icon">
         <icon v-if="icon" :name="icon" />
       </slot>
