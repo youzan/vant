4c2eec380d13d3e4b570d51e87378e5d836a3495
陈嘉涵 <chenjiahan@youzan.com>
2018-02-27 15:00:16 +0800

    [Improvement] update CDN domain

--- docs/src/mobile.js
+++ docs/src/mobile.js
@@ -5,7 +5,6 @@ import routes from './router';
 import Vant, { Lazyload } from 'packages';
 import VantDoc from 'vant-doc';
 import 'packages/vant-css/src/index.css';
-import 'packages/vant-css/src/icon-local.css';
 import 'vant-doc/src/helper/touch-simulator';
 import './components/nprogress.css';
 
