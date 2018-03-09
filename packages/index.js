6626fd91a2d6c8725457b494afa88a72a03fcf8c
陈嘉涵 <chenjiahan@youzan.com>
2018-03-09 16:22:58 +0800

    [build] 0.12.14

--- packages/index.js
+++ packages/index.js
@@ -63,7 +63,7 @@ import TreeSelect from './tree-select';
 import Uploader from './uploader';
 import Waterfall from './waterfall';
 
-const version = '0.12.13';
+const version = '0.12.14';
 const components = [
   Actionsheet,
   AddressEdit,
