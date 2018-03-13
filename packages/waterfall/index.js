e3c8f5b60de77e16d4b9718f97fd8e13db7b2cf6
陈嘉涵 <chenjiahan@youzan.com>
2018-03-13 16:10:52 +0800

    [Deprecate] add Waterfall deprecate tip

--- packages/waterfall/index.js
+++ packages/waterfall/index.js
@@ -1,6 +1,9 @@
 import Waterfall from './directive.js';
 
 const install = function(Vue) {
+  if (process.env.NODE_ENV !== 'production') {
+    console.warn('[Vant warn] Waterfall is deprecated, please use List component instread.');
+  }
   Vue.directive('WaterfallLower', Waterfall('lower'));
   Vue.directive('WaterfallUpper', Waterfall('upper'));
 };
