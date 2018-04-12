7d3b63eb15abd7dd5d028e191869e3262375be89
neverland <chenjiahan@buaa.edu.cn>
2018-04-12 10:40:40 +0800

    [bugfix] SubmitBar: inherit wrong font size (#857)

--- packages/submit-bar/index.vue
+++ packages/submit-bar/index.vue
@@ -7,7 +7,7 @@
       <slot />
       <div class="van-submit-bar__price">
         <template v-if="hasPrice">
-          <span class="van-submit-bar__price-text">{{ label || $t('label') }}</span>
+          <span>{{ label || $t('label') }}</span>
           <span class="van-submit-bar__price-interger">¥{{ priceInterger }}.</span>
           <span class="van-submit-bar__price-decimal">{{ priceDecimal }}</span>
         </template>
