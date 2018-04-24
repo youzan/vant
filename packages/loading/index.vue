871a5f222f5babb19ce3c9ff7cf88954d0fa5ffd
陈嘉涵 <chenjiahan@youzan.com>
2018-04-24 16:20:01 +0800

    fix

--- packages/loading/index.vue
+++ packages/loading/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="b([type, color])" :style="style">
-    <span :class="b('spinner', [type])">
+    <span :class="b('spinner', type)">
       <i v-for="item in (type === 'spinner' ? 12 : 0)" />
       <svg v-if="type === 'circular'" :class="b('circular')" viewBox="25 25 50 50">
         <circle cx="50" cy="50" r="20" fill="none"/>
