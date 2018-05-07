1c60cee6443cde4b2419afce8fedead0981de921
陈嘉涵 <chenjiahan@youzan.com>
2018-05-07 21:40:33 +0800

    [Doc] optimzie document site

--- docs/src/mobile.js
+++ docs/src/mobile.js
@@ -2,20 +2,10 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './WapApp';
 import routes from './router';
-import Vant, { Lazyload } from 'packages';
-import VantDoc from 'vant-doc';
-import 'packages/vant-css/src/index.css';
-import 'packages/vant-css/src/icon-local.css';
+import '../../packages/vant-css/src/index.css';
+import '../../packages/vant-css/src/icon-local.css';
 import 'vant-doc/src/helper/touch-simulator';
 import './components/nprogress.css';
-
-Vue
-  .use(Vant)
-  .use(VantDoc)
-  .use(VueRouter)
-  .use(Lazyload, {
-    lazyComponent: true
-  });
 
 const router = new VueRouter({
   mode: 'hash',
--- packages/index.js
+++ packages/index.js
@@ -144,6 +144,7 @@ const install = Vue => {
   });
 };
 
+/* istanbul ignore if */
 if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue);
 }
