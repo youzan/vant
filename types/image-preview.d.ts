ba10fc93d79eff5e5ce6306b54462dca690a22b1
陈嘉涵 <chenjiahan@youzan.com>
2018-09-07 15:32:33 +0800

    [bugfix] ImagePreview: fix ts definition

--- types/image-preview.d.ts
+++ types/image-preview.d.ts
@@ -1,12 +1,18 @@
 import { VanPopupMixin } from './mixins/popup';
 
+export type ImagePreviewOptions = string[] | {
+  images: string[];
+  startPosition?: number;
+  onClose?: () => any;
+};
+
 export class VanImagePreview extends VanPopupMixin {
   images: string[];
   startPosition: number;
 }
 
 export interface ImagePreview {
-  (images: string[], startPosition?: number): VanImagePreview;
+  (options: ImagePreviewOptions, startPosition?: number): VanImagePreview;
   install(): void;
 }
 
