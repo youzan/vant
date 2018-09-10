09463a70d265e4ca180094c863907d760b0bc8c9
陈嘉涵 <chenjiahan@youzan.com>
2018-09-10 14:37:19 +0800

    [improvement] Dialog: update style

--- packages/index.js
+++ packages/index.js
@@ -69,7 +69,7 @@ import TreeSelect from './tree-select';
 import Uploader from './uploader';
 import Waterfall from './waterfall';
 
-const version = '1.3.0';
+const version = '1.3.1';
 const components = [
   Actionsheet,
   AddressEdit,
