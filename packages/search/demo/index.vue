a995c203dcfc7550cba2aa1bae19333a0d905af5
neverland <chenjiahan@buaa.edu.cn>
2018-10-26 16:10:40 +0800

    [Doc] Search: add more api document (#1983)

--- packages/search/demo/index.vue
+++ packages/search/demo/index.vue
@@ -1,7 +1,10 @@
 <template>
   <demo-section>
     <demo-block :title="$t('basicUsage')">
-      <van-search :placeholder="$t('placeholder')" v-model="value" />
+      <van-search
+        v-model="value"
+        :placeholder="$t('placeholder')"
+      />
     </demo-block>
 
     <demo-block :title="$t('title2')">
