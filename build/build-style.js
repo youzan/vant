32e89460cb7d16fcfcb4f58e216574f0904290e2
陈嘉涵 <chenjiahan@youzan.com>
2018-11-03 20:41:35 +0800

    compile less

--- build/build-style.js
+++ build/build-style.js
@@ -1,14 +1,16 @@
 const gulp = require('gulp');
+const less = require('gulp-less');
 const postcss = require('gulp-postcss');
 const cssmin = require('gulp-clean-css');
 
 // compile component css
 gulp.task('compile', () => {
   return gulp
-    .src(['../es/**/*.css', '../lib/**/*.css'])
+    .src(['../es/**/*.less', '../lib/**/*.less'])
+    .pipe(less())
     .pipe(postcss())
     .pipe(cssmin())
-    .pipe(gulp.dest(file => file.base));
+    .pipe(gulp.dest(file => file.base.replace('.less', '.css')));
 });
 
 gulp.task('default', ['compile']);
