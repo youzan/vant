cafef4aadc49801dd144b01e729e8cfbe1330ff7
陈嘉涵 <chenjiahan@youzan.com>
2018-11-04 21:26:24 +0800

    [release] 1.4.1

--- packages/index.js
+++ packages/index.js
@@ -69,7 +69,7 @@ import TreeSelect from './tree-select';
 import Uploader from './uploader';
 import Waterfall from './waterfall';
 
-const version = '1.4.0';
+const version = '1.4.1';
 const components = [
   Actionsheet,
   AddressEdit,
