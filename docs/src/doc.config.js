d237a374feb6e105b7c4a657ddc380b4f808f4df
neverland <chenjiahan@buaa.edu.cn>
2018-11-06 09:32:58 +0800

    [Doc] reorganize components (#2032)

--- docs/src/doc.config.js
+++ docs/src/doc.config.js
@@ -75,14 +75,6 @@ module.exports = {
             groupName: '基础组件',
             list: [
               {
-                path: '/col',
-                title: 'Layout 布局'
-              },
-              {
-                path: '/badge',
-                title: 'Badge 徽章'
-              },
-              {
                 path: '/button',
                 title: 'Button 按钮'
               },
@@ -91,76 +83,16 @@ module.exports = {
                 title: 'Cell 单元格'
               },
               {
-                path: '/circle',
-                title: 'Circle 环形进度条'
-              },
-              {
-                path: '/collapse',
-                title: 'Collapse 折叠面板'
-              },
-              {
                 path: '/icon',
                 title: 'Icon 图标'
               },
               {
-                path: '/lazyload',
-                title: 'Lazyload 图片懒加载'
-              },
-              {
-                path: '/list',
-                title: 'List 列表'
-              },
-              {
-                path: '/loading',
-                title: 'Loading 加载'
-              },
-              {
-                path: '/nav-bar',
-                title: 'NavBar 导航栏'
-              },
-              {
-                path: '/notice-bar',
-                title: 'NoticeBar 通告栏'
-              },
-              {
-                path: '/pagination',
-                title: 'Pagination 分页'
-              },
-              {
-                path: '/panel',
-                title: 'Panel 面板'
+                path: '/col',
+                title: 'Layout 布局'
               },
               {
                 path: '/popup',
                 title: 'Popup 弹出层'
-              },
-              {
-                path: '/progress',
-                title: 'Progress 进度条'
-              },
-              {
-                path: '/steps',
-                title: 'Steps 步骤条'
-              },
-              {
-                path: '/swipe',
-                title: 'Swipe 轮播'
-              },
-              {
-                path: '/tab',
-                title: 'Tab 标签页'
-              },
-              {
-                path: '/tabbar',
-                title: 'Tabbar 标签栏'
-              },
-              {
-                path: '/tag',
-                title: 'Tag 标记'
-              },
-              {
-                path: '/waterfall',
-                title: 'Waterfall 瀑布流'
               }
             ]
           },
@@ -170,6 +102,10 @@ module.exports = {
               {
                 path: '/checkbox',
                 title: 'Checkbox 复选框'
+              },
+              {
+                path: '/datetime-picker',
+                title: 'DatetimePicker 时间选择'
               },
               {
                 path: '/field',
@@ -182,6 +118,10 @@ module.exports = {
               {
                 path: '/password-input',
                 title: 'PasswordInput 密码输入框'
+              },
+              {
+                path: '/picker',
+                title: 'Picker 选择器'
               },
               {
                 path: '/radio',
@@ -208,33 +148,37 @@ module.exports = {
                 title: 'Switch 开关'
               },
               {
+                path: '/switch-cell',
+                title: 'SwitchCell 开关单元格'
+              },
+              {
                 path: '/uploader',
                 title: 'Uploader 图片上传'
               }
             ]
           },
           {
-            groupName: '操作反馈',
+            groupName: '反馈组件',
             list: [
               {
                 path: '/actionsheet',
                 title: 'Actionsheet 上拉菜单'
               },
               {
-                path: '/datetime-picker',
-                title: 'DatetimePicker 时间选择'
-              },
-              {
                 path: '/dialog',
                 title: 'Dialog 弹出框'
               },
               {
-                path: '/picker',
-                title: 'Picker 选择器'
+                path: '/loading',
+                title: 'Loading 加载'
               },
               {
                 path: '/pull-refresh',
                 title: 'PullRefresh 下拉刷新'
+              },
+              {
+                path: '/swipe-cell',
+                title: 'SwipeCell 滑动单元格'
               },
               {
                 path: '/toast',
@@ -243,19 +187,76 @@ module.exports = {
             ]
           },
           {
-            groupName: '高阶组件',
+            groupName: '展示组件',
             list: [
+              {
+                path: '/circle',
+                title: 'Circle 环形进度条'
+              },
+              {
+                path: '/collapse',
+                title: 'Collapse 折叠面板'
+              },
               {
                 path: '/image-preview',
                 title: 'ImagePreview 图片预览'
               },
               {
-                path: '/swipe-cell',
-                title: 'SwipeCell 滑动单元格'
+                path: '/lazyload',
+                title: 'Lazyload 图片懒加载'
               },
               {
-                path: '/switch-cell',
-                title: 'SwitchCell 开关单元格'
+                path: '/list',
+                title: 'List 列表'
+              },
+              {
+                path: '/notice-bar',
+                title: 'NoticeBar 通告栏'
+              },
+              {
+                path: '/panel',
+                title: 'Panel 面板'
+              },
+              {
+                path: '/progress',
+                title: 'Progress 进度条'
+              },
+              {
+                path: '/steps',
+                title: 'Steps 步骤条'
+              },
+              {
+                path: '/swipe',
+                title: 'Swipe 轮播'
+              },
+              {
+                path: '/tag',
+                title: 'Tag 标记'
+              }
+            ]
+          },
+          {
+            groupName: '导航组件',
+            list: [
+              {
+                path: '/badge',
+                title: 'Badge 徽章'
+              },
+              {
+                path: '/nav-bar',
+                title: 'NavBar 导航栏'
+              },
+              {
+                path: '/pagination',
+                title: 'Pagination 分页'
+              },
+              {
+                path: '/tab',
+                title: 'Tab 标签页'
+              },
+              {
+                path: '/tabbar',
+                title: 'Tabbar 标签栏'
               },
               {
                 path: '/tree-select',
@@ -292,7 +293,7 @@ module.exports = {
               },
               {
                 path: '/goods-action',
-                title: 'GoodsAction 商品页行动点'
+                title: 'GoodsAction 商品导航'
               },
               {
                 path: '/submit-bar',
@@ -300,7 +301,7 @@ module.exports = {
               },
               {
                 path: '/sku',
-                title: 'Sku 商品规格弹层'
+                title: 'Sku 商品规格'
               }
             ]
           }
@@ -371,14 +372,6 @@ module.exports = {
             groupName: 'Basic Components',
             list: [
               {
-                path: '/col',
-                title: 'Layout'
-              },
-              {
-                path: '/badge',
-                title: 'Badge'
-              },
-              {
                 path: '/button',
                 title: 'Button'
               },
@@ -387,76 +380,16 @@ module.exports = {
                 title: 'Cell'
               },
               {
-                path: '/circle',
-                title: 'Circle'
-              },
-              {
-                path: '/collapse',
-                title: 'Collapse'
-              },
-              {
                 path: '/icon',
                 title: 'Icon'
               },
               {
-                path: '/lazyload',
-                title: 'Lazyload'
-              },
-              {
-                path: '/list',
-                title: 'List'
-              },
-              {
-                path: '/loading',
-                title: 'Loading'
-              },
-              {
-                path: '/nav-bar',
-                title: 'NavBar'
-              },
-              {
-                path: '/notice-bar',
-                title: 'NoticeBar'
-              },
-              {
-                path: '/pagination',
-                title: 'Pagination'
-              },
-              {
-                path: '/panel',
-                title: 'Panel'
+                path: '/col',
+                title: 'Layout'
               },
               {
                 path: '/popup',
                 title: 'Popup'
-              },
-              {
-                path: '/progress',
-                title: 'Progress'
-              },
-              {
-                path: '/steps',
-                title: 'Steps'
-              },
-              {
-                path: '/swipe',
-                title: 'Swipe'
-              },
-              {
-                path: '/tab',
-                title: 'Tab'
-              },
-              {
-                path: '/tabbar',
-                title: 'Tabbar'
-              },
-              {
-                path: '/tag',
-                title: 'Tag'
-              },
-              {
-                path: '/waterfall',
-                title: 'Waterfall'
               }
             ]
           },
@@ -466,6 +399,10 @@ module.exports = {
               {
                 path: '/checkbox',
                 title: 'Checkbox'
+              },
+              {
+                path: '/datetime-picker',
+                title: 'DatetimePicker'
               },
               {
                 path: '/field',
@@ -478,6 +415,10 @@ module.exports = {
               {
                 path: '/password-input',
                 title: 'PasswordInput'
+              },
+              {
+                path: '/picker',
+                title: 'Picker'
               },
               {
                 path: '/radio',
@@ -504,6 +445,10 @@ module.exports = {
                 title: 'Switch'
               },
               {
+                path: '/switch-cell',
+                title: 'SwitchCell'
+              },
+              {
                 path: '/uploader',
                 title: 'Uploader'
               }
@@ -517,20 +462,20 @@ module.exports = {
                 title: 'Actionsheet'
               },
               {
-                path: '/datetime-picker',
-                title: 'DatetimePicker'
-              },
-              {
                 path: '/dialog',
                 title: 'Dialog'
               },
               {
-                path: '/picker',
-                title: 'Picker'
+                path: '/loading',
+                title: 'Loading'
               },
               {
                 path: '/pull-refresh',
                 title: 'PullRefresh'
+              },
+              {
+                path: '/swipe-cell',
+                title: 'SwipeCell'
               },
               {
                 path: '/toast',
@@ -539,19 +484,76 @@ module.exports = {
             ]
           },
           {
-            groupName: 'Advanced Components',
+            groupName: 'Display Components',
             list: [
               {
-                path: '/swipe-cell',
-                title: 'SwipeCell'
+                path: '/circle',
+                title: 'Circle'
+              },
+              {
+                path: '/collapse',
+                title: 'Collapse'
               },
               {
                 path: '/image-preview',
                 title: 'ImagePreview'
               },
               {
-                path: '/switch-cell',
-                title: 'SwitchCell'
+                path: '/lazyload',
+                title: 'Lazyload'
+              },
+              {
+                path: '/list',
+                title: 'List'
+              },
+              {
+                path: '/notice-bar',
+                title: 'NoticeBar'
+              },
+              {
+                path: '/panel',
+                title: 'Panel'
+              },
+              {
+                path: '/progress',
+                title: 'Progress'
+              },
+              {
+                path: '/steps',
+                title: 'Steps'
+              },
+              {
+                path: '/swipe',
+                title: 'Swipe'
+              },
+              {
+                path: '/tag',
+                title: 'Tag'
+              }
+            ]
+          },
+          {
+            groupName: 'Navigation Components',
+            list: [
+              {
+                path: '/badge',
+                title: 'Badge'
+              },
+              {
+                path: '/nav-bar',
+                title: 'NavBar'
+              },
+              {
+                path: '/pagination',
+                title: 'Pagination'
+              },
+              {
+                path: '/tab',
+                title: 'Tab'
+              },
+              {
+                path: '/tabbar',
+                title: 'Tabbar'
               },
               {
                 path: '/tree-select',
