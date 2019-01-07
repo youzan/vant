aa8bcf1f08331c401ff79a88d51b75681971f61c
陈嘉涵 <chenjiahan@youzan.com>
2019-01-07 15:19:02 +0800

    [improvement] use i18n

--- packages/locale/index.js
+++ packages/locale/index.js
@@ -1,19 +1,23 @@
+/* eslint-disable import/no-mutable-exports */
 import Vue from 'vue';
 import deepAssign from '../utils/deep-assign';
 import defaultMessages from './lang/zh-CN';
 
+let lang = 'zh-CN';
 const proto = Vue.prototype;
-const defaultLang = 'zh-CN';
+const messages = { [lang]: defaultMessages };
+
 const locale = {
   install() {
     if (proto.$vantLang) {
       return;
     }
-    Vue.util.defineReactive(proto, '$vantLang', defaultLang);
-    Vue.util.defineReactive(proto, '$vantMessages', { [defaultLang]: defaultMessages });
+    Vue.util.defineReactive(proto, '$vantLang', lang);
+    Vue.util.defineReactive(proto, '$vantMessages', messages);
   },
 
-  use(lang, messages) {
+  use(newLang, messages) {
+    lang = newLang;
     proto.$vantLang = lang;
     this.add({ [lang]: messages });
   },
@@ -26,3 +30,4 @@ const locale = {
 locale.install();
 
 export default locale;
+export { lang, messages };
