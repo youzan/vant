9888bc4b53bdedfeb0f20510dc68f3fbc818a81b
陈嘉涵 <chenjiahan@youzan.com>
2019-01-09 22:59:37 +0800

    [release] 1.5.2-beta

--- packages/index.js
+++ packages/index.js
@@ -73,7 +73,7 @@ import TreeSelect from './tree-select';
 import Uploader from './uploader';
 import Waterfall from './waterfall';
 
-const version = '1.5.1';
+const version = '1.5.2-beta';
 const components = [
   Actionsheet,
   AddressEdit,
