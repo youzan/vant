3bb15e026b2abd55b10fc1d1ff24846096d61635
neverland <chenjiahan@buaa.edu.cn>
2019-01-11 22:11:41 +0800

    [improvement] CheckboxGroup: jsx (#2498)

---
+++ packages/checkbox-group/index.js
@@ -0,0 +1,21 @@
+import { use } from '../utils';
+
+const [sfc, bem] = use('checkbox-group');
+
+export default sfc({
+  props: {
+    max: Number,
+    value: Array,
+    disabled: Boolean
+  },
+
+  watch: {
+    value(val) {
+      this.$emit('change', val);
+    }
+  },
+
+  render(h) {
+    return <div class={bem()}>{this.$slots.default}</div>;
+  }
+});
