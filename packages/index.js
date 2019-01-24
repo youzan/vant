0f3ed4a831a73e69eac60c0fffd299c675379510
陈嘉涵 <chenjiahan@youzan.com>
2019-01-24 22:35:23 +0800

    [release] 1.5.7

--- packages/index.js
+++ packages/index.js
@@ -73,7 +73,7 @@ import TreeSelect from './tree-select';
 import Uploader from './uploader';
 import Waterfall from './waterfall';
 
-const version = '1.5.6';
+const version = '1.5.7';
 const components = [
   Actionsheet,
   AddressEdit,
