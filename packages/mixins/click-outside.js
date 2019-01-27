342202697b0394d441c79cc6dba28368c914bcea
陈嘉涵 <chenjiahan@youzan.com>
2019-01-27 08:49:33 +0800

    shorthand

--- packages/mixins/click-outside.js
+++ packages/mixins/click-outside.js
@@ -1,19 +1,17 @@
 import { on, off } from '../utils/event';
 
-export default function (config) {
-  return {
-    mounted() {
-      config.handler = event => {
-        if (!this.$el.contains(event.target)) {
-          this[config.method]();
-        }
-      };
+export default config => ({
+  mounted() {
+    config.handler = event => {
+      if (!this.$el.contains(event.target)) {
+        this[config.method]();
+      }
+    };
 
-      on(document, config.event, config.handler);
-    },
+    on(document, config.event, config.handler);
+  },
 
-    beforeDestroy() {
-      off(document, config.event, config.handler);
-    }
-  };
-}
+  beforeDestroy() {
+    off(document, config.event, config.handler);
+  }
+});
