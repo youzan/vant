4822717c72a921646407894e81bb4bcafcecf138
陈嘉涵 <chenjiahan@youzan.com>
2019-02-10 18:42:51 +0800

    [release] 1.6.2

--- packages/index.js
+++ packages/index.js
@@ -73,7 +73,7 @@ import TreeSelect from './tree-select';
 import Uploader from './uploader';
 import Waterfall from './waterfall';
 
-const version = '1.6.1';
+const version = '1.6.2';
 const components = [
   Actionsheet,
   AddressEdit,
