5524fb07ff28790b972c63e7b8d82c6acdb82ecb
陈嘉涵 <chenjiahan@youzan.com>
2019-02-14 13:49:56 +0800

    fix test

--- packages/switch/test/index.spec.js
+++ packages/switch/test/index.spec.js
@@ -2,12 +2,39 @@ import Switch from '..';
 import { mount } from '../../../test/utils';
 
 test('emit event', () => {
-  const wrapper = mount(Switch);
+  const input = jest.fn();
+  const change = jest.fn();
+  const wrapper = mount(Switch, {
+    context: {
+      on: {
+        input,
+        change
+      }
+    }
+  });
   wrapper.trigger('click');
-  wrapper.trigger('click');
-  wrapper.setProps({ disabled: true });
   wrapper.trigger('click');
 
-  expect(wrapper.emitted('input')).toEqual([[true], [true]]);
-  expect(wrapper.emitted('change')).toEqual([[true], [true]]);
+  expect(input.mock.calls).toEqual([[true], [true]]);
+  expect(change.mock.calls).toEqual([[true], [true]]);
+});
+
+test('disabled', () => {
+  const input = jest.fn();
+  const change = jest.fn();
+  const wrapper = mount(Switch, {
+    context: {
+      on: {
+        input,
+        change
+      }
+    },
+    propsData: {
+      disabled: true
+    }
+  });
+  wrapper.trigger('click');
+
+  expect(input.mock.calls.length).toBeFalsy();
+  expect(change.mock.calls.length).toBeFalsy();
 });
