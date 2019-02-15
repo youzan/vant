880f574b9aef3b4967a5f1a06ad2e19fa26b914d
neverland <chenjiahan@buaa.edu.cn>
2019-02-15 21:00:46 +0800

    [improvement] Sku: partial functional (#2756)

--- packages/sku/components/SkuHeader.js
+++ packages/sku/components/SkuHeader.js
@@ -1,61 +1,57 @@
 import { use } from '../../utils';
+import { inherit } from '../../utils/functional';
 import Icon from '../../icon';
 
 const [sfc, bem] = use('sku-header');
 
-export default sfc({
-  props: {
-    sku: Object,
-    goods: Object,
-    skuEventBus: Object,
-    selectedSku: Object
-  },
+function getSkuImg(sku, selectedSku) {
+  const id = selectedSku.s1;
 
-  computed: {
-    goodsImg() {
-      const s1Id = this.selectedSku.s1;
-      const skuImg = this.getSkuImg(s1Id);
-      // 优先使用选中 sku 的图片
-      return skuImg || this.goods.picture;
-    }
-  },
-
-  methods: {
-    getSkuImg(id) {
-      if (!id) return;
-
-      // skuImg 挂载在 skuTree 中 s1 上
-      const treeItem = this.sku.tree.filter(item => item.k_s === 's1')[0] || {};
-
-      if (!treeItem.v) return;
-
+  if (id) {
+    // skuImg 挂载在 skuTree 中 s1 上
+    const treeItem = sku.tree.filter(item => item.k_s === 's1')[0] || {};
+    if (treeItem.v) {
       const matchedSku = treeItem.v.filter(skuValue => skuValue.id === id)[0];
-      if (matchedSku) return matchedSku.imgUrl || matchedSku.img_url;
-    },
-
-    previewImage() {
-      this.skuEventBus.$emit('sku:previewImage', this.goodsImg);
+      if (matchedSku) {
+        return matchedSku.imgUrl || matchedSku.img_url;
+      }
     }
-  },
-
-  render(h) {
-    return (
-      <div class={[bem(), 'van-hairline--bottom']}>
-        <div class={bem('img-wrap')} onClick={this.previewImage}>
-          <img src={this.goodsImg} />
-        </div>
-        <div class={bem('goods-info')}>
-          <div class="van-sku__goods-name van-ellipsis">{this.goods.title}</div>
-          {this.slots()}
-          <Icon
-            name="close"
-            class="van-sku__close-icon"
-            onClick={() => {
-              this.skuEventBus.$emit('sku:close');
-            }}
-          />
-        </div>
-      </div>
-    );
   }
-});
+}
+
+function SkuHeader(h, props, slots, ctx) {
+  const { sku, goods, skuEventBus, selectedSku } = props;
+  const goodsImg = getSkuImg(sku, selectedSku) || goods.picture;
+
+  const previewImage = () => {
+    skuEventBus.$emit('sku:previewImage', goodsImg);
+  };
+
+  return (
+    <div class={[bem(), 'van-hairline--bottom']} {...inherit(ctx)}>
+      <div class={bem('img-wrap')} onClick={previewImage}>
+        <img src={goodsImg} />
+      </div>
+      <div class={bem('goods-info')}>
+        <div class="van-sku__goods-name van-ellipsis">{goods.title}</div>
+        {slots.default && slots.default()}
+        <Icon
+          name="close"
+          class="van-sku__close-icon"
+          onClick={() => {
+            skuEventBus.$emit('sku:close');
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+SkuHeader.props = {
+  sku: Object,
+  goods: Object,
+  skuEventBus: Object,
+  selectedSku: Object
+};
+
+export default sfc(SkuHeader);
