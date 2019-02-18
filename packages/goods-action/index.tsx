8f4f4ae299fa50a96c793b4e3cb5e1ca28e3ad3e
陈嘉涵 <chenjiahan@youzan.com>
2019-02-18 20:53:27 +0800

    [improvement] GoodsAction: tsx

--- packages/goods-action/index.tsx
+++ packages/goods-action/index.tsx
@@ -1,9 +1,18 @@
 import { use } from '../utils';
 import { inherit } from '../utils/functional';
 
+// Types
+import { CreateElement, RenderContext } from 'vue/types';
+import { DefaultProps, DefaultSlots } from '../utils/use/sfc';
+
 const [sfc, bem] = use('goods-action');
 
-function GoodsAction(h, props, slots, ctx) {
+function GoodsAction(
+  h: CreateElement,
+  props: DefaultProps,
+  slots: DefaultSlots,
+  ctx: RenderContext
+) {
   return (
     <div class={bem()} {...inherit(ctx, true)}>
       {slots.default && slots.default()}
