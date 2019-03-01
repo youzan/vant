bcd24fc70bf07e525d49d87e851899c94eb64608
陈嘉涵 <chenjiahan@youzan.com>
2019-03-01 11:23:00 +0800

    icon

---
+++ packages/icon/test/index.spec.js
@@ -0,0 +1,29 @@
+import Icon from '..';
+import { mount } from '../../../test/utils';
+
+test('render icon with builtin icon name', () => {
+  const wrapper = mount(Icon, {
+    propsData: {
+      name: 'success'
+    }
+  });
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('render icon with url name', () => {
+  const wrapper = mount(Icon, {
+    propsData: {
+      name: 'https://img.yzcdn.com/icon.jpg'
+    }
+  });
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('render icon default slot', () => {
+  const wrapper = mount({
+    render(h) {
+      return h(Icon, { props: { name: 'success' } }, ['Default slot']);
+    }
+  });
+  expect(wrapper).toMatchSnapshot();
+});
