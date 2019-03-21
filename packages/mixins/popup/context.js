a641a2b8747158ccafe49db540ffc9e0609787d7
陈嘉涵 <chenjiahan@youzan.com>
2019-03-21 10:03:03 +0800

    [improvement] popup mixin ts

--- packages/mixins/popup/context.js
+++
@@ -1,9 +0,0 @@
-export default {
-  zIndex: 2000,
-  stack: [],
-  lockCount: 0,
-
-  get top() {
-    return this.stack[this.stack.length - 1];
-  }
-};
---
+++ packages/mixins/popup/context.ts
@@ -0,0 +1,17 @@
+import { OverlayConfig } from './overlay';
+
+export type StackItem = {
+  vm: any;
+  target: HTMLElement;
+  config: OverlayConfig;
+};
+
+export const context = {
+  zIndex: 2000,
+  lockCount: 0,
+  stack: [] as StackItem[],
+
+  get top(): StackItem {
+    return this.stack[this.stack.length - 1];
+  }
+};
