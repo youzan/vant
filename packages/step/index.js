95f014cff4f22a0cb0b76577ccfcf5e437024015
akebe <akebe@163.com>
2019-04-13 09:10:30 +0800

    [bugfix] Step: step 销毁之前从父 steps 中移除 (#3140)

--- packages/step/index.js
+++ packages/step/index.js
@@ -8,6 +8,13 @@ export default sfc({
     this.$parent.steps.push(this);
   },
 
+  beforeDestroy() {
+    const index = this.$parent.steps.indexOf(this);
+    if (index > -1) {
+      this.$parent.steps.splice(index, 1);
+    }
+  },
+
   computed: {
     status() {
       const index = this.$parent.steps.indexOf(this);
