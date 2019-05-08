f0e336cb4fb8cfe431f182f48590979486786482
陈嘉涵 <chenjiahan@youzan.com>
2019-05-08 16:42:19 +0800

    fix swipe snapshot

--- packages/swipe/test/demo.spec.js
+++ packages/swipe/test/demo.spec.js
@@ -1,4 +1,12 @@
 import Demo from '../demo';
 import demoTest from '../../../test/demo-test';
 
+function mockGetBoundingClientRect(vertical) {
+  Element.prototype.getBoundingClientRect = jest.fn(() => ({
+    width: 100,
+    height: 100
+  }));
+}
+
+mockGetBoundingClientRect();
 demoTest(Demo);
