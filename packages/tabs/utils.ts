c6230b09b52900ed600ef0231175a51d89c1d7d3
陈嘉涵 <chenjiahan@youzan.com>
2019-06-14 17:20:12 +0800

    fix immediate

--- packages/tabs/utils.ts
+++ packages/tabs/utils.ts
@@ -3,7 +3,7 @@ import { raf } from '../utils/dom/raf';
 export function scrollLeftTo(el: HTMLElement, to: number, duration: number) {
   let count = 0;
   const from = el.scrollLeft;
-  const frames = Math.round((duration * 1000) / 16);
+  const frames = duration === 0 ? 1 : Math.round((duration * 1000) / 16);
 
   function animate() {
     el.scrollLeft += (to - from) / frames;
