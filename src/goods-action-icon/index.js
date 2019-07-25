a5e8e9af7af23ec5b420a0ee09466f56916e3c12
陈嘉涵 <chenjiahan@youzan.com>
2019-07-25 20:09:30 +0800

    [new feature] GoodsAction: update style

---
+++ src/goods-action-icon/index.js
@@ -0,0 +1,43 @@
+import { createNamespace } from '../utils';
+import { route, routeProps } from '../utils/router';
+import { ChildrenMixin } from '../mixins/relation';
+import Icon from '../icon';
+
+const [createComponent, bem] = createNamespace('goods-action-icon');
+
+export default createComponent({
+  mixins: [ChildrenMixin('vanGoodsAction')],
+
+  props: {
+    ...routeProps,
+    text: String,
+    icon: String,
+    info: [Number, String],
+    iconClass: null
+  },
+
+  methods: {
+    onClick(event) {
+      this.$emit('click', event);
+      route(this.$router, this);
+    }
+  },
+
+  render(h) {
+    return (
+      <div role="button" tabindex="0" class={bem()} onClick={this.onClick}>
+        {this.slots('icon') ? (
+          <div class={bem('icon')}>{this.slots('icon')}</div>
+        ) : (
+          <Icon
+            class={[bem('icon'), this.iconClass]}
+            tag="div"
+            info={this.info}
+            name={this.icon}
+          />
+        )}
+        {this.slots() || this.text}
+      </div>
+    );
+  }
+});
---
+++ src/goods-action/index.js
@@ -0,0 +1,20 @@
+import { createNamespace } from '../utils';
+import { ParentMixin } from '../mixins/relation';
+
+const [createComponent, bem] = createNamespace('goods-action');
+
+export default createComponent({
+  mixins: [ParentMixin('vanGoodsAction')],
+
+  props: {
+    safeAreaInsetBottom: Boolean
+  },
+
+  render(h) {
+    return (
+      <div class={bem({ 'safe-area-inset-bottom': this.safeAreaInsetBottom })}>
+        {this.slots()}
+      </div>
+    );
+  }
+});
