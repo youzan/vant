5158c7d6da75caa6455f40d786e90339af61efe3
ystarlognzi <zhangzilong.zzl@163.com>
2019-08-15 20:44:11 +0800

    [improvement]: IndexBar: update demo and snapshots

--- src/index-bar/demo/index.vue
+++ src/index-bar/demo/index.vue
@@ -15,6 +15,46 @@
         </van-index-bar>
       </van-tab>
 
+      <van-tab :title="$t('customScroller')">
+        <div class="custom-scroller">
+          <van-index-bar>
+            <div
+              v-for="index in indexList"
+              :key="index"
+            >
+              <van-index-anchor :index="index" />
+              <van-cell :title="$t('text')" />
+              <van-cell :title="$t('text')" />
+              <van-cell :title="$t('text')" />
+            </div>
+          </van-index-bar>
+        </div>
+      </van-tab>
+
+      <van-tab :title="$t('popupScroller')">
+        <div class="popup-scroller">
+          <van-button round type="primary" @click="showPopup=true">Click me!</van-button>
+          <van-popup
+            class="index-bar-popup"
+            v-model="showPopup"
+            position="right"
+            get-container="body"
+          >
+            <van-index-bar>
+              <div
+                v-for="index in indexList"
+                :key="index"
+              >
+                <van-index-anchor :index="index" />
+                <van-cell :title="$t('text')" />
+                <van-cell :title="$t('text')" />
+                <van-cell :title="$t('text')" />
+              </div>
+            </van-index-bar>
+          </van-popup>
+        </div>
+      </van-tab>
+
       <van-tab :title="$t('customIndexList')">
         <van-index-bar :index-list="customIndexList">
           <div
@@ -39,10 +79,14 @@ export default {
   i18n: {
     'zh-CN': {
       text: '文本',
+      customScroller: '自定义滚动容器',
+      popupScroller: 'Popup 内滚动',
       customIndexList: '自定义索引列表'
     },
     'en-US': {
       text: 'Text',
+      customScroller: 'Custom Scroller',
+      popupScroller: 'Popup Scroller',
       customIndexList: 'Custom Index List'
     }
   },
@@ -57,8 +101,34 @@ export default {
     return {
       activeTab: 0,
       indexList,
-      customIndexList: [1, 2, 3, 4, 5, 6, 8, 9, 10]
+      customIndexList: [1, 2, 3, 4, 5, 6, 8, 9, 10],
+      showPopup: false,
     };
   }
 };
 </script>
+
+<style lang="less">
+  .demo-index-bar {
+    .custom-scroller {
+      height: 50vh;
+      margin: 100px 0 0 100px;
+      overflow-y: scroll;
+    }
+
+    .popup-scroller {
+      padding: 50px 0;
+      text-align: center;
+    }
+  }
+
+  .index-bar-popup {
+    top: 0;
+    transform: none;
+
+    .van-index-bar {
+      width: 80vw;
+      background: #f8f8f8;
+    }
+  }
+</style>
