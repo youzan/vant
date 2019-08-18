b573ba9f6a6e9ec4da23c8bdb5d083f0a346ea29
neverland <chenjiahan@youzan.com>
2019-08-18 11:16:44 +0800

    [improvement] Uploader: support more image types (#4140)

--- src/uploader/test/utils.spec.js
+++ src/uploader/test/utils.spec.js
@@ -6,6 +6,10 @@ test('isImageFile', () => {
   expect(isImageFile({ url: 'https://a.png' })).toBeTruthy();
   expect(isImageFile({ url: 'https://a.svg' })).toBeTruthy();
   expect(isImageFile({ url: 'https://a.gif' })).toBeTruthy();
+  expect(isImageFile({ url: 'https://a.webp' })).toBeTruthy();
+  expect(isImageFile({ url: 'https://a.jfif' })).toBeTruthy();
+  expect(isImageFile({ url: 'https://a.bmp' })).toBeTruthy();
+  expect(isImageFile({ url: 'https://a.dpg' })).toBeTruthy();
   expect(isImageFile({ file: { type: 'image/jpg' } })).toBeTruthy();
   expect(isImageFile({ file: { type: 'application/pdf' } })).toBeFalsy();
   expect(isImageFile({ content: 'data:image/xxx' })).toBeTruthy();
