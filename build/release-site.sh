fcd26a867a14b39ef4465b2f772c775e899633fe
陈嘉涵 <chenjiahan@youzan.com>
2019-08-30 16:04:26 +0800

    build: split chunk

--- build/release-site.sh
+++ build/release-site.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 rm -rf docs/dist
 
-npx cross-env NODE_ENV=production webpack --config build/webpack.site.js
+npx cross-env NODE_ENV=production webpack --config build/webpack.site.prd.js
 
 superman-cdn /vant ./docs/dist/*.js
 
