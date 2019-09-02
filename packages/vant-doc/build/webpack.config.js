5539fbc376b5893c53cbad2fe89e71cf723e337a
陈嘉涵 <chenjiahan@youzan.com>
2019-09-02 22:18:56 +0800

    build: fix vant-doc webpack config

--- packages/vant-doc/build/webpack.config.js
+++ packages/vant-doc/build/webpack.config.js
@@ -1,8 +1,7 @@
 const path = require('path');
-const baseWebpackConfig = require('../../../build/webpack.dev');
+const baseWebpackConfig = require('../../../build/webpack.pkg');
 
 module.exports = Object.assign(baseWebpackConfig, {
-  mode: 'production',
   entry: {
     index: './src/index.js'
   },
@@ -10,13 +9,5 @@ module.exports = Object.assign(baseWebpackConfig, {
     path: path.resolve(__dirname, '../lib'),
     filename: '[name].js',
     libraryTarget: 'commonjs2'
-  },
-  externals: {
-    vue: {
-      root: 'Vue',
-      commonjs: 'vue',
-      commonjs2: 'vue',
-      amd: 'vue'
-    }
   }
 });
