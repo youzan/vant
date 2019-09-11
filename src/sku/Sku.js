913aef17398be9b1296a6c805dc2717fd1963a9c
codpoe <codpoe.me@gmail.com>
2019-09-11 20:08:12 +0800

    fix: update style

--- src/sku/Sku.js
+++ src/sku/Sku.js
@@ -545,7 +545,6 @@ export default createComponent({
         round
         closeable
         position="bottom"
-        closeIcon="clear"
         class="van-sku-container"
         getContainer={this.getContainer}
         closeOnClickOverlay={this.closeOnClickOverlay}
