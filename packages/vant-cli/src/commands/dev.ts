11faabd40801646cb708bac4a567a7969920ca56
陈嘉涵 <chenjiahan@youzan.com>
2019-12-30 10:40:35 +0800

    chore(cli): optimize clean dir

--- packages/vant-cli/src/commands/dev.ts
+++ packages/vant-cli/src/commands/dev.ts
@@ -1,9 +1,10 @@
-import { clean } from '../commands/clean';
+import { emptyDir } from 'fs-extra';
 import { setNodeEnv } from '../common';
 import { compileSite } from '../compiler/compile-site';
+import { DIST_DIR } from '../common/constant';
 
 export async function dev() {
   setNodeEnv('development');
-  await clean();
+  await emptyDir(DIST_DIR);
   await compileSite();
 }
