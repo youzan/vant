a8f73f5e1030803bbd7b73f9bc5fce51cea0659d
陈嘉涵 <chenjiahan@youzan.com>
2020-01-05 10:03:13 +0800

    chore(cli): remove unused type conversion

--- packages/vant-cli/src/compiler/gen-site-desktop-shared.ts
+++ packages/vant-cli/src/compiler/gen-site-desktop-shared.ts
@@ -66,14 +66,14 @@ function resolveDocuments(components: string[]): DocumentItem[] {
   }
 
   const staticDocs = glob.sync(join(DOCS_DIR, '**/*.md')).map(path => {
-    const pairs = parse(path.toString()).name.split('.');
+    const pairs = parse(path).name.split('.');
     return {
       name: formatName(pairs[0], pairs[1] || defaultLang),
       path
     };
   });
 
-  return [...staticDocs, ...docs.filter(item => existsSync(item.path))] as DocumentItem[];
+  return [...staticDocs, ...docs.filter(item => existsSync(item.path))];
 }
 
 function genImportDocuments(items: DocumentItem[]) {
