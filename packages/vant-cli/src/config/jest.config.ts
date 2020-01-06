7b15699512321be5986373c7fa367db489c72204
陈嘉涵 <chenjiahan@youzan.com>
2020-01-06 11:01:58 +0800

    feat(cli): adjust jest setup timing

--- packages/vant-cli/src/config/jest.config.ts
+++ packages/vant-cli/src/config/jest.config.ts
@@ -2,7 +2,7 @@ import { join } from 'path';
 import { existsSync } from 'fs-extra';
 import {
   ROOT,
-  JEST_INIT_FILE,
+  JEST_SEPUP_FILE,
   JEST_FILE_MOCK_FILE,
   JEST_STYLE_MOCK_FILE
 } from '../common/constant';
@@ -12,7 +12,7 @@ const DEFAULT_CONFIG = {
     '\\.(css|less|scss)$': JEST_STYLE_MOCK_FILE,
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': JEST_FILE_MOCK_FILE
   },
-  setupFiles: [JEST_INIT_FILE],
+  setupFilesAfterEnv: [JEST_SEPUP_FILE],
   moduleFileExtensions: ['js', 'jsx', 'vue', 'ts', 'tsx'],
   transform: {
     '\\.(vue)$': 'vue-jest',
