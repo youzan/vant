c89d5ee096c28e4355be946a6d40115dfa74bc60
neverland <chenjiahan@youzan.com>
2020-01-07 17:03:21 +0800

    fix(Tabs): can not render line when wrapper is fixed (#5496)

--- src/utils/dom/style.ts
+++ src/utils/dom/style.ts
@@ -1,5 +1,11 @@
 export function isHidden(element: HTMLElement) {
-  return (
-    window.getComputedStyle(element).display === 'none' || element.offsetParent === null
-  );
+  const style = window.getComputedStyle(element);
+  const isHidden = style.display === 'none';
+
+  // offsetParent returns null in the following situations:
+  // 1. The element or its parent element has the display property set to none.
+  // 2. The element has the position property set to fixed
+  const isParentHidden = element.offsetParent === null && style.position !== 'fixed';
+
+  return isHidden || isParentHidden;
 }
