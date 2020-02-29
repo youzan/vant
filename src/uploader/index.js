c6791841f4b06e699a684da0243526147438d852
chenjiahan <chenjiahan@youzan.com>
2020-02-29 10:49:52 +0800

    feat(Uploader): increase border-radius to 8px

--- src/uploader/index.js
+++ src/uploader/index.js
@@ -278,7 +278,6 @@ export default createComponent({
           class={bem('preview-image')}
           width={this.previewSize}
           height={this.previewSize}
-          radius={4}
           onClick={() => {
             this.onPreviewImage(item);
           }}
