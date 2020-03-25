3dd3a45cfd3a992fc2e27238810c622c907559fd
neverland <chenjiahan@youzan.com>
2020-03-25 10:18:15 +0800

    fix(cli): use remove instead of emptyDir in clean command (#5895) (#5904)

--- packages/vant-cli/src/compiler/gen-vetur-config.ts
+++ packages/vant-cli/src/compiler/gen-vetur-config.ts
@@ -1,26 +1,25 @@
 import markdownVetur from '@vant/markdown-vetur';
-import { join } from 'path';
 import { get } from 'lodash';
 import {
-  ROOT,
   SRC_DIR,
+  VETUR_DIR,
   getVantConfig,
   getPackageJson,
 } from '../common/constant';
 
 // generate vetur tags & attributes
 export function genVeturConfig() {
-  const vantConfig = getVantConfig();
   const pkgJson = getPackageJson();
+  const vantConfig = getVantConfig();
   const options = get(vantConfig, 'build.vetur');
 
   if (options) {
     markdownVetur.parseAndWrite({
       name: vantConfig.name,
-      version: pkgJson.version,
       path: SRC_DIR,
       test: /zh-CN\.md/,
-      outputDir: join(ROOT, 'vetur'),
+      version: pkgJson.version,
+      outputDir: VETUR_DIR,
       ...options,
     });
   }
