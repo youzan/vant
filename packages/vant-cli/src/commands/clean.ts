7afd68a4b4db83ee74feffdd86cf34930226a6f9
chenjiahan <chenjiahan@youzan.com>
2020-03-25 10:13:35 +0800

    fix(cli): use remove instead of emptyDir in clean command (#5895)

--- packages/vant-cli/src/commands/clean.ts
+++ packages/vant-cli/src/commands/clean.ts
@@ -1,11 +1,18 @@
-import { emptyDir } from 'fs-extra';
-import { ES_DIR, LIB_DIR, DIST_DIR, SITE_DIST_DIR } from '../common/constant';
+import { remove } from 'fs-extra';
+import {
+  ES_DIR,
+  LIB_DIR,
+  DIST_DIR,
+  VETUR_DIR,
+  SITE_DIST_DIR,
+} from '../common/constant';
 
 export async function clean() {
   await Promise.all([
-    emptyDir(ES_DIR),
-    emptyDir(LIB_DIR),
-    emptyDir(DIST_DIR),
-    emptyDir(SITE_DIST_DIR),
+    remove(ES_DIR),
+    remove(LIB_DIR),
+    remove(DIST_DIR),
+    remove(VETUR_DIR),
+    remove(SITE_DIST_DIR),
   ]);
 }
