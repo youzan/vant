76106f016005a8376c0c78a4a2dc58c90273c9fc
chenjiahan <chenjiahan@youzan.com>
2020-03-25 10:04:59 +0800

    feat(cli): support generate web-types (#5893)

--- packages/vant-cli/src/compiler/gen-vetur-config.ts
+++ packages/vant-cli/src/compiler/gen-vetur-config.ts
@@ -1,15 +1,23 @@
 import markdownVetur from '@vant/markdown-vetur';
 import { join } from 'path';
 import { get } from 'lodash';
-import { SRC_DIR, getVantConfig, ROOT } from '../common/constant';
+import {
+  ROOT,
+  SRC_DIR,
+  getVantConfig,
+  getPackageJson,
+} from '../common/constant';
 
 // generate vetur tags & attributes
 export function genVeturConfig() {
   const vantConfig = getVantConfig();
+  const pkgJson = getPackageJson();
   const options = get(vantConfig, 'build.vetur');
 
   if (options) {
     markdownVetur.parseAndWrite({
+      name: vantConfig.name,
+      version: pkgJson.version,
       path: SRC_DIR,
       test: /zh-CN\.md/,
       outputDir: join(ROOT, 'vetur'),
