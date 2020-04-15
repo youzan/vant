93fbdb7a05afd08984d5560b4dcf76c88d2441b9
chenjiahan <chenjiahan@youzan.com>
2020-04-15 14:03:31 +0800

    chore(Empty): improve svg

--- src/empty/Network.tsx
+++ src/empty/Network.tsx
@@ -1,83 +1,60 @@
 export default {
   render() {
+    const genStop = (color: string, offset: number, opacity?: number) => {
+      return (
+        <stop stop-color={color} offset={`${offset}%`} stop-opacity={opacity} />
+      );
+    };
+
     return (
-      <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+      <svg viewBox="0 0 160 160" xmlns="http://www.w3.org/2000/svg">
         <defs>
-          <linearGradient x1="64.022%" y1="100%" x2="64.022%" y2="0%" id="c">
-            <stop stop-color="#FFF" stop-opacity=".5" offset="0%" />
-            <stop stop-color="#F2F3F5" offset="100%" />
+          <linearGradient id="c" x1="64.022%" y1="100%" x2="64.022%" y2="0%">
+            {genStop('#FFF', 0, 0.5)}
+            {genStop('#F2F3F5', 100)}
           </linearGradient>
-          <linearGradient x1="64.022%" y1="96.956%" x2="64.022%" y2="0%" id="d">
-            <stop stop-color="#F2F3F5" stop-opacity=".3" offset="0%" />
-            <stop stop-color="#F2F3F5" offset="100%" />
+          <linearGradient id="d" x1="64.022%" y1="96.956%" x2="64.022%" y2="0%">
+            {genStop('#F2F3F5', 0, 0.3)}
+            {genStop('#F2F3F5', 100)}
           </linearGradient>
-          <linearGradient x1="64.022%" y1="96.956%" x2="64.022%" y2="0%" id="e">
-            <stop stop-color="#F2F3F5" stop-opacity=".3" offset="0%" />
-            <stop stop-color="#F2F3F5" offset="100%" />
+          <linearGradient id="h" x1="50%" y1="0%" x2="50%" y2="84.459%">
+            {genStop('#EBEDF0', 0)}
+            {genStop('#DCDEE0', 100, 0)}
           </linearGradient>
-          <linearGradient x1="64.022%" y1="100%" x2="64.022%" y2="0%" id="f">
-            <stop stop-color="#FFF" stop-opacity=".5" offset="0%" />
-            <stop stop-color="#F2F3F5" offset="100%" />
+          <linearGradient id="i" x1="100%" y1="0%" x2="100%" y2="100%">
+            {genStop('#EAEDF0', 0)}
+            {genStop('#DCDEE0', 100)}
           </linearGradient>
-          <linearGradient x1="50%" y1="0%" x2="50%" y2="84.459%" id="h">
-            <stop stop-color="#EBEDF0" offset="0%" />
-            <stop stop-color="#DCDEE0" stop-opacity="0" offset="100%" />
+          <linearGradient id="k" x1="100%" y1="100%" x2="100%" y2="0%">
+            {genStop('#EAEDF0', 0)}
+            {genStop('#DCDEE0', 100)}
           </linearGradient>
-          <linearGradient x1="100%" y1="0%" x2="100%" y2="100%" id="i">
-            <stop stop-color="#EAEDF1" offset="0%" />
-            <stop stop-color="#DCDEE0" offset="100%" />
-          </linearGradient>
-          <linearGradient x1="100%" y1="0%" x2="100%" y2="100%" id="j">
-            <stop stop-color="#EAEDF1" offset="0%" />
-            <stop stop-color="#DCDEE0" offset="100%" />
-          </linearGradient>
-          <linearGradient x1="100%" y1="100%" x2="100%" y2="0%" id="k">
-            <stop stop-color="#EAEDF1" offset="0%" />
-            <stop stop-color="#DCDEE0" offset="100%" />
-          </linearGradient>
-          <linearGradient x1="100%" y1="100%" x2="100%" y2="0%" id="l">
-            <stop stop-color="#EAEDF1" offset="0%" />
-            <stop stop-color="#DCDEE0" offset="100%" />
-          </linearGradient>
-          <linearGradient x1="0%" y1="43.982%" x2="100%" y2="54.703%" id="m">
-            <stop stop-color="#EAEDF1" offset="0%" />
-            <stop stop-color="#DCDEE0" offset="100%" />
+          <linearGradient id="m" x1="0%" y1="43.982%" x2="100%" y2="54.703%">
+            {genStop('#EAEDF0', 0)}
+            {genStop('#DCDEE0', 100)}
           </linearGradient>
           <linearGradient
+            id="n"
             x1="94.535%"
             y1="43.837%"
             x2="5.465%"
             y2="54.948%"
-            id="n"
           >
-            <stop stop-color="#EAEDF1" offset="0%" />
-            <stop stop-color="#DCDEE0" offset="100%" />
+            {genStop('#EAEDF0', 0)}
+            {genStop('#DCDEE0', 100)}
           </linearGradient>
           <radialGradient
+            id="g"
             cx="50%"
             cy="0%"
             fx="50%"
             fy="0%"
             r="100%"
             gradientTransform="matrix(0 1 -.54835 0 .5 -.5)"
-            id="g"
           >
-            <stop stop-color="#EBEDF0" offset="0%" />
-            <stop stop-color="#FFF" stop-opacity="0" offset="100%" />
+            {genStop('#EBEDF0', 0)}
+            {genStop('#FFF', 100, 0)}
           </radialGradient>
-          <path id="a" d="M0 0h160v160H0z" />
-          <mask
-            id="b"
-            maskContentUnits="userSpaceOnUse"
-            maskUnits="objectBoundingBox"
-            x="0"
-            y="0"
-            width="160"
-            height="160"
-            fill="#fff"
-          >
-            <use xlinkHref="#a" />
-          </mask>
         </defs>
         <g fill="none" fill-rule="evenodd">
           <g opacity=".8">
@@ -93,12 +70,12 @@ export default {
             />
             <path
               d="M96.016 0a4.108 4.108 0 013.934 2.868l.179-.004c2.138 0 3.871 1.71 3.871 3.818 0 2.109-1.733 3.818-3.871 3.818-.164 0-.325-.01-.484-.03v.03h-6.774v-.083a3.196 3.196 0 01-.726.083C90.408 10.5 89 9.111 89 7.398c0-1.636 1.284-2.976 2.911-3.094a3.555 3.555 0 01-.008-.247c0-2.24 1.842-4.057 4.113-4.057z"
-              fill="url(#e)"
+              fill="url(#d)"
               transform="translate(2 7)"
             />
             <path
               d="M121 8h22.231v14H152v77.37h-31V8z"
-              fill="url(#f)"
+              fill="url(#c)"
               transform="translate(2 7)"
             />
           </g>
@@ -117,7 +94,7 @@ export default {
             />
             <path
               d="M9.849 0C3.756 6.225 0 14.747 0 24.146c0 9.398 3.756 17.92 9.849 24.145"
-              stroke="url(#j)"
+              stroke="url(#i)"
               transform="translate(43 36)"
             />
             <path
@@ -127,7 +104,7 @@ export default {
             />
             <path
               d="M73.216 0c-6.093 6.225-9.849 14.747-9.849 24.146 0 9.398 3.756 17.92 9.849 24.145"
-              stroke="url(#l)"
+              stroke="url(#k)"
               transform="rotate(-180 89.791 42.146)"
             />
           </g>
