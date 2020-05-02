9a4927ac0518255360afd944fba4d98495542ffd
neverland <chenjiahan@youzan.com>
2020-05-02 16:43:52 +0800

    fix(Layout): gutter calculation (#6197)
    * Revert "fix(Layout): outside of the screen after setting gutter (#6143)"
    
    This reverts commit a460425b265e9b83a8e835466be60c87b157e73e.
    
    * fix(Layout): gutter calculation
    
    * test: fix snapshot

--- src/col/index.js
+++ src/col/index.js
@@ -16,28 +16,15 @@ export default createComponent({
   },
 
   computed: {
-    gutter() {
-      return (this.parent && Number(this.parent.gutter)) || 0;
-    },
-
     style() {
-      const { index, gutter } = this;
+      const { index } = this;
+      const { spaces } = this.parent || {};
 
-      if (gutter) {
-        const count = this.parent.children.length;
-        const padding = (gutter * (count - 1)) / count;
-
-        if (index === 0) {
-          return { paddingRight: `${padding}px` };
-        }
-
-        if (index === count - 1) {
-          return { paddingLeft: `${padding}px` };
-        }
-
+      if (spaces && spaces[index]) {
+        const { left, right } = spaces[index];
         return {
-          paddingLeft: `${padding / 2}px`,
-          paddingRight: `${padding / 2}px`,
+          paddingLeft: left ? `${left}px` : null,
+          paddingRight: right ? `${right}px` : null,
         };
       }
     },
--- src/row/index.js
+++ src/row/index.js
@@ -20,6 +20,47 @@ export default createComponent({
     },
   },
 
+  computed: {
+    spaces() {
+      const gutter = Number(this.gutter);
+
+      if (!gutter) {
+        return;
+      }
+
+      const spaces = [];
+      const groups = [[]];
+
+      let totalSpan = 0;
+      this.children.forEach((item, index) => {
+        totalSpan += Number(item.span);
+
+        if (totalSpan > 24) {
+          groups.push([index]);
+          totalSpan -= 24;
+        } else {
+          groups[groups.length - 1].push(index);
+        }
+      });
+
+      groups.forEach((group) => {
+        const averagePadding = (gutter * (group.length - 1)) / group.length;
+
+        group.forEach((item, index) => {
+          if (index === 0) {
+            spaces.push({ right: group.length > 1 ? averagePadding : 0 });
+          } else {
+            const left = gutter - spaces[item - 1].right;
+            const right = averagePadding - left;
+            spaces.push({ left, right });
+          }
+        });
+      });
+
+      return spaces;
+    },
+  },
+
   methods: {
     onClick(event) {
       this.$emit('click', event);
