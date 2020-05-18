392c52b81a24c5e1a80c5d2327d189cdc67bc76c
desperado <yuwei_149@163.com>
2020-05-18 19:06:23 +0800

    Update number.ts
    '-' can only appear first

--- src/utils/format/number.ts
+++ src/utils/format/number.ts
@@ -4,12 +4,16 @@ export function range(num: number, min: number, max: number): number {
 
 function trimExtraChar(value: string, char: string, regExp: RegExp) {
   const index = value.indexOf(char);
-
-  if (index > -1) {
-    return value.slice(0, index + 1) + value.slice(index).replace(regExp, '');
+  
+  if (index === -1) {
+  	return value;
   }
 
-  return value;
+  if (char === '-' && index !== 0) {
+  	return value.slice(0, index);
+  }
+
+  return value.slice(0, index + 1) + value.slice(index).replace(regExp, '');
 }
 
 export function formatNumber(value: string, allowDot?: boolean) {
