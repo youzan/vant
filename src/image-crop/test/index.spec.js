a5e98318135a63768cbce64ec96f28b16f32cefa
jczzq <1195262383@qq.com>
2020-07-12 21:22:20 +0800

    test(ImageCrop): deletable prop

--- src/image-crop/test/index.spec.js
+++ src/image-crop/test/index.spec.js
@@ -2,9 +2,23 @@ import { mount } from '../../../test';
 import ImageCrop from '..';
 
 // const mockFile = new File([], 'test.jpg');
+// const file = { target: { files: [mockFile] } };
 
-it('input event', () => {
+test('input event', () => {
   const wrapper = mount(ImageCrop);
   wrapper.vm.currentValue = '1';
   expect(wrapper.emitted('input')[0][0]).toEqual('1');
+});
+
+test('deletable prop', () => {
+  const wrapper = mount(ImageCrop, {
+    propsData: {
+      deletable: true,
+    },
+  });
+
+  expect(wrapper.contains('.van-image-crop__remove')).toBeTruthy();
+
+  wrapper.setProps({ deletable: false });
+  expect(wrapper.find('.van-image-crop__remove').element).toBeFalsy();
 });
