67f418ab076f69089eef8bbb7248af774e3cdfe4
chenjiahan <chenjiahan@youzan.com>
2020-08-27 14:20:37 +0800

    refactor(Rate): refactor with composition api

--- src/composition/use-parent-field.ts
+++ src/composition/use-parent-field.ts
@@ -2,7 +2,7 @@ import { watch, inject, WatchSource, getCurrentInstance } from 'vue';
 import { FIELD_KEY } from '../field';
 
 export function useParentField(watchSource: WatchSource) {
-  const field = inject(FIELD_KEY) as any;
+  const field = inject(FIELD_KEY, null) as any;
 
   if (field && !field.children) {
     field.children = getCurrentInstance()!.proxy;
