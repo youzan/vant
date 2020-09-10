dbc6b92868aee8e7ab1c3fefc5a8fb15dc574805
chenjiahan <chenjiahan@youzan.com>
2020-09-10 21:03:53 +0800

    feat: add @vant/use

---
+++ packages/vant-use/src/index.ts
@@ -0,0 +1,1 @@
+export { useToggle } from './useToggle';
---
+++ packages/vant-use/src/useToggle/index.ts
@@ -0,0 +1,10 @@
+import { ref } from 'vue';
+
+export function useToggle(defaultValue = false) {
+  const state = ref(defaultValue);
+  const toggle = (value = !state.value) => {
+    state.value = value;
+  };
+
+  return [state, toggle];
+}
