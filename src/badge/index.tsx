40085b72fe96ea4b1c389d64f4a4fd32c7226759
chenjiahan <chenjiahan@youzan.com>
2020-09-19 23:20:25 +0800

    types(Badge): remove unused BadgeProps

--- src/badge/index.tsx
+++ src/badge/index.tsx
@@ -2,18 +2,13 @@ import { isDef, createNamespace } from '../utils';
 
 const [createComponent, bem] = createNamespace('badge');
 
-export type BadgeProps = {
-  dot?: boolean;
-  badge?: number | string;
-};
-
 export default createComponent({
   props: {
     dot: Boolean,
     badge: [Number, String],
   },
 
-  setup(props: BadgeProps) {
+  setup(props) {
     return () => {
       const { dot, badge } = props;
       const hasBadge = isDef(badge) && badge !== '';
