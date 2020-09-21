63f15094f33d0a1784c74d9ced449ec479459dff
chenjiahan <chenjiahan@youzan.com>
2020-09-21 18:48:11 +0800

    types: use-placeholder

---
+++ src/composition/use-placeholder.tsx
@@ -0,0 +1,16 @@
+import { useHeight } from './use-rect';
+import type { Ref, VNode } from 'vue';
+import type { BEM } from '../utils/create/bem';
+
+export function usePlaceholder(contentRef: Ref<Element>, bem: BEM) {
+  const height = useHeight(contentRef);
+
+  return (renderContent: () => VNode) => (
+    <div
+      class={bem('placeholder')}
+      style={{ height: height.value ? `${height.value}px` : undefined }}
+    >
+      {renderContent()}
+    </div>
+  );
+}
