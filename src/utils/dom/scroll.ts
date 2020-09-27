a3a8f2961cea27e47da4e72cebc81fb63399665e
chenjiahan <chenjiahan@youzan.com>
2020-09-27 16:37:11 +0800

    chore: remove unused getScroller

--- src/utils/dom/scroll.ts
+++ src/utils/dom/scroll.ts
@@ -4,41 +4,6 @@ function isWindow(val: unknown): val is Window {
   return val === window;
 }
 
-// get nearest scroll element
-// http://w3help.org/zh-cn/causes/SD9013
-// http://stackoverflow.com/questions/17016740/onscroll-function-is-not-working-for-chrome
-const overflowScrollReg = /scroll|auto/i;
-export function getScroller(el: HTMLElement, root: ScrollElement = window) {
-  let node = el;
-
-  while (
-    node &&
-    node.tagName !== 'HTML' &&
-    node.nodeType === 1 &&
-    node !== root
-  ) {
-    const { overflowY } = window.getComputedStyle(node);
-
-    if (overflowScrollReg.test(overflowY)) {
-      if (node.tagName !== 'BODY') {
-        return node;
-      }
-
-      // see: https://github.com/youzan/vant/issues/3823
-      const { overflowY: htmlOverflowY } = window.getComputedStyle(
-        node.parentNode as Element
-      );
-
-      if (overflowScrollReg.test(htmlOverflowY)) {
-        return node;
-      }
-    }
-    node = node.parentNode as HTMLElement;
-  }
-
-  return root;
-}
-
 export function getScrollTop(el: ScrollElement): number {
   const top = 'scrollTop' in el ? el.scrollTop : el.pageYOffset;
 
