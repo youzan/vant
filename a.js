56fc7f771225fb697bd0bae7561f07807d6b9698
chenjiahan <chenjiahan@youzan.com>
2020-11-08 20:31:08 +0800

    chore: rename temp file

--- a.js
+++
@@ -1,8 +0,0 @@
-const glob = require('glob');
-const fs = require('fs');
-
-const legacy = glob.sync('src/**/demo.legacy.js');
-
-legacy.forEach((item) => {
-  fs.renameSync(item, item.replace('demo.legacy.js', 'demo.spec.js'));
-});
