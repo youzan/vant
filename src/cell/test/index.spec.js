0f9a5a87649e52d5180514f15b1e1fc6444d6c1d
chenjiahan <chenjiahan@youzan.com>
2020-11-13 23:43:16 +0800

    test(Cell): update test cases

---
+++ src/cell/test/index.spec.js
@@ -0,0 +1,85 @@
+import Cell from '..';
+import { mount } from '@vue/test-utils';
+
+test('should render default slot and match snapshot', () => {
+  const wrapper = mount(Cell, {
+    slots: {
+      default: () => 'Custom Default',
+    },
+  });
+  expect(wrapper.html()).toMatchSnapshot();
+});
+
+test('should render title slot and match snapshot', () => {
+  const wrapper = mount(Cell, {
+    slots: {
+      title: () => 'Custom Title',
+    },
+  });
+  expect(wrapper.html()).toMatchSnapshot();
+});
+
+test('should render label slot and match snapshot', () => {
+  const wrapper = mount(Cell, {
+    props: {
+      title: 'Title',
+    },
+    slots: {
+      label: () => 'Custom Label',
+    },
+  });
+  expect(wrapper.html()).toMatchSnapshot();
+});
+
+test('should render icon slot and match snapshot', () => {
+  const wrapper = mount(Cell, {
+    slots: {
+      icon: () => 'Custom Icon',
+    },
+  });
+  expect(wrapper.html()).toMatchSnapshot();
+});
+
+test('should render extra slot and match snapshot', () => {
+  const wrapper = mount(Cell, {
+    slots: {
+      extra: () => 'Custom Extra',
+    },
+  });
+  expect(wrapper.html()).toMatchSnapshot();
+});
+
+test('should change arrow direction when using arrow-direction prop', () => {
+  const wrapper = mount(Cell, {
+    props: {
+      isLink: true,
+      arrowDirection: 'down',
+    },
+  });
+
+  expect(wrapper.find('.van-cell__right-icon').html()).toMatchSnapshot();
+});
+
+test('should change title style when using title-style prop', () => {
+  const wrapper = mount(Cell, {
+    props: {
+      title: 'title',
+      titleStyle: {
+        color: 'red',
+      },
+    },
+  });
+
+  expect(wrapper.find('.van-cell__title').element.style.color).toEqual('red');
+});
+
+test('should change icon class prefix when using icon-prefix prop', () => {
+  const wrapper = mount(Cell, {
+    props: {
+      icon: 'success',
+      iconPrefix: 'my-icon',
+    },
+  });
+
+  expect(wrapper.html()).toMatchSnapshot();
+});
