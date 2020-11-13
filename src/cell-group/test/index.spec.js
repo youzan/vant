d212883c5e50cf70e213c7e4c6e251995a5d1469
chenjiahan <chenjiahan@youzan.com>
2020-11-13 23:43:27 +0800

    test(CellGroup): update test cases

---
+++ src/cell-group/test/index.spec.js
@@ -0,0 +1,12 @@
+import CellGroup from '..';
+import { mount } from '@vue/test-utils';
+
+test('should render title slot and match snapshot', () => {
+  const wrapper = mount(CellGroup, {
+    slots: {
+      title: () => 'Custom Title',
+    },
+  });
+
+  expect(wrapper.html()).toMatchSnapshot();
+});
