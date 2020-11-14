d366f7b4f755cb04c8ba70427110e7cecf092559
chenjiahan <chenjiahan@youzan.com>
2020-11-14 08:10:59 +0800

    chore: should not collect coverage from lang config files

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/packages/'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx,vue}',
+    '!**/demo/**',
+    '!**/test/**',
+    '!**/lang/**',
+  ],
 };
