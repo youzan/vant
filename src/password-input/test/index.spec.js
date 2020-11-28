b317ac46ce138f371d7569b00aabc071ce15bc71
chenjiahan <chenjiahan@youzan.com>
2020-11-28 19:22:43 +0800

    test(PasswordInput): update test cases

---
+++ src/password-input/test/index.spec.js
@@ -0,0 +1,19 @@
+import PasswordInput from '..';
+import { mount } from '@vue/test-utils';
+
+test('should emit focus event when security is touched', () => {
+  const wrapper = mount(PasswordInput);
+  wrapper.find('.van-password-input__security').trigger('touchstart');
+  expect(wrapper.emitted('focus').length).toEqual(1);
+});
+
+test('should render error info correctly', () => {
+  const wrapper = mount(PasswordInput, {
+    props: {
+      errorInfo: 'error!',
+    },
+  });
+  expect(
+    wrapper.find('.van-password-input__error-info').html()
+  ).toMatchSnapshot();
+});
