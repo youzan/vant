3f0f3e4474e88f6066bb1eff58f9c7b2b1c2bfca
chenjiahan <chenjiahan@youzan.com>
2020-12-02 15:49:34 +0800

    feat(cli): support sass tilde importer

--- packages/vant-cli/src/compiler/compile-sass.ts
+++ packages/vant-cli/src/compiler/compile-sass.ts
@@ -1,6 +1,21 @@
 import { renderSync } from 'sass';
 
+// allow to import from node_modules
+// @import "~package-name/var.scss"
+const tildeImporter = (url: string) => {
+  if (url.includes('~')) {
+    url = url.replace('~', '');
+
+    if (!url.includes('.scss')) {
+      url += '.scss';
+    }
+
+    url = require.resolve(url);
+  }
+  return { file: url };
+};
+
 export async function compileSass(filePath: string) {
-  const { css } = renderSync({ file: filePath });
+  const { css } = renderSync({ file: filePath, importer: tildeImporter });
   return css;
 }
