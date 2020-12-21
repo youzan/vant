8563648635ac6ba82738dddd0be7287a9422e007
Jake <Jake.Laoyu@gmail.com>
2020-12-21 16:16:16 +0800

    fix: 修复eslint 组件名称配置冲突

--- packages/vant-eslint-config/index.js
+++ packages/vant-eslint-config/index.js
@@ -53,8 +53,7 @@ module.exports = {
     'vue/require-v-for-key': 'off',
     'vue/require-default-prop': 'off',
     'vue/no-unused-components': 'off',
-    'vue/component-definition-name-casing': 'off',
-    'vue/name-property-casing': ['error', 'kebab-case'],
+    'vue/component-definition-name-casing': ['error', 'PascalCase'],
     'vue/component-name-in-template-casing': ['error', 'kebab-case'],
     // typescript-eslint
     '@typescript-eslint/camelcase': 'off',
