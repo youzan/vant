1e92cde37711e054784884a19259938b3ea295f2
chenjiahan <chenjiahan@youzan.com>
2021-01-23 11:27:35 +0800

    perf(ImagePreview): adjust default swipeDuration to 300ms

--- src/image-preview/index.js
+++ src/image-preview/index.js
@@ -19,7 +19,7 @@ const defaultConfig = {
   closeIcon: 'clear',
   beforeClose: null,
   startPosition: 0,
-  swipeDuration: 500,
+  swipeDuration: 300,
   showIndicators: false,
   closeOnPopstate: true,
   closeIconPosition: 'top-right',
