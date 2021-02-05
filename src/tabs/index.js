7b94ac91996d8803062f8123cf959d4862f2b877
gogHeroDream <31148295+gogHeroDream@users.noreply.github.com>
2021-02-05 10:31:13 +0800

    fix(Tabs): incorrect active tab when active prop is zero (#8074)
    Co-authored-by: songpeng01 <songpeng01@mail.open.com.cn>

--- src/tabs/index.js
+++ src/tabs/index.js
@@ -372,7 +372,7 @@ export default createComponent({
       () => children.length,
       () => {
         if (state.inited) {
-          setCurrentIndexByName(props.active || currentName.value);
+          setCurrentIndexByName((props.active || props.active === 0) ? props.active : currentName.value);
           setLine();
           nextTick(() => {
             scrollIntoView(true);
@@ -397,7 +397,7 @@ export default createComponent({
     );
 
     const init = () => {
-      setCurrentIndexByName(props.active || currentName.value);
+      setCurrentIndexByName((props.active || props.active === 0) ? props.active : currentName.value);
       nextTick(() => {
         state.inited = true;
         tabHeight = getVisibleHeight(wrapRef.value);
