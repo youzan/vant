0d0310d34e230115a0cd3389835a0fdd2109c66d
neverland <chenjiahan@youzan.com>
2021-02-13 20:57:06 +0800

    fix(@vant/use): useRect should always return DOMRect (#8147)

--- packages/vant-use/src/useRect/index.ts
+++ packages/vant-use/src/useRect/index.ts
@@ -12,27 +12,12 @@ export const useRect = (
   if (isWindow(element)) {
     const width = element.innerWidth;
     const height = element.innerHeight;
-
-    return {
-      top: 0,
-      left: 0,
-      right: width,
-      bottom: height,
-      width,
-      height,
-    };
+    return new DOMRect(0, 0, width, height);
   }
 
   if (element && element.getBoundingClientRect) {
     return element.getBoundingClientRect();
   }
 
-  return {
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    width: 0,
-    height: 0,
-  };
+  return new DOMRect(0, 0, 0, 0);
 };
