a0a6bcd0af141f10dac9a6b6d813fc55b14b2416
chenjiahan <chenjiahan@youzan.com>
2021-03-07 16:20:11 +0800

    types: createComponent missing install

--- src/utils/create/component.ts
+++ src/utils/create/component.ts
@@ -1,17 +1,95 @@
 /**
  * Create a basic component with common options
  */
-import { App, defineComponent, ComponentOptionsWithObjectProps } from 'vue';
+/* eslint-disable @typescript-eslint/ban-types */
+import {
+  App,
+  EmitsOptions,
+  MethodOptions,
+  defineComponent,
+  DefineComponent,
+  ComputedOptions,
+  ComponentPropsOptions,
+  ComponentOptionsMixin,
+  ComponentOptionsWithoutProps,
+  ComponentOptionsWithObjectProps,
+} from 'vue';
 import { camelize } from '../format/string';
 
+type ComponentWithInstall = {
+  install(app: App): void;
+};
+
 export function createComponent(name: string) {
-  return function (sfc: ComponentOptionsWithObjectProps) {
+  function defineComponentWithInstall<
+    PropsOptions extends Readonly<ComponentPropsOptions>,
+    RawBindings,
+    D,
+    C extends ComputedOptions = {},
+    M extends MethodOptions = {},
+    Mixin extends ComponentOptionsMixin = ComponentOptionsMixin,
+    Extends extends ComponentOptionsMixin = ComponentOptionsMixin,
+    E extends EmitsOptions = Record<string, any>,
+    EE extends string = string
+  >(
+    options: ComponentOptionsWithObjectProps<
+      PropsOptions,
+      RawBindings,
+      D,
+      C,
+      M,
+      Mixin,
+      Extends,
+      E,
+      EE
+    >
+  ): DefineComponent<
+    PropsOptions,
+    RawBindings,
+    D,
+    C,
+    M,
+    Mixin,
+    Extends,
+    E,
+    EE
+  > &
+    ComponentWithInstall;
+
+  function defineComponentWithInstall<
+    Props = {},
+    RawBindings = {},
+    D = {},
+    C extends ComputedOptions = {},
+    M extends MethodOptions = {},
+    Mixin extends ComponentOptionsMixin = ComponentOptionsMixin,
+    Extends extends ComponentOptionsMixin = ComponentOptionsMixin,
+    E extends EmitsOptions = EmitsOptions,
+    EE extends string = string
+  >(
+    options: ComponentOptionsWithoutProps<
+      Props,
+      RawBindings,
+      D,
+      C,
+      M,
+      Mixin,
+      Extends,
+      E,
+      EE
+    >
+  ): DefineComponent<Props, RawBindings, D, C, M, Mixin, Extends, E, EE> &
+    ComponentWithInstall;
+
+  function defineComponentWithInstall(sfc: any) {
     sfc.name = name;
     sfc.install = (app: App) => {
       app.component(name as string, sfc);
       app.component(camelize(`-${name}`), sfc);
     };
 
-    return defineComponent(sfc);
-  } as typeof defineComponent;
+    return defineComponent(sfc) as any;
+  }
+
+  return defineComponentWithInstall;
 }
