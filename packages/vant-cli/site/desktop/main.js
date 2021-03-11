c0b10e43740f8ca7bd3ac9f726595abc16601fe0
HaoChuan9421 <hc199421@gmail.com>
2021-03-11 19:49:46 +0800

    feat(vant-cli): 支持在 vant-cli 的 desktop 页中使用组件库中的组件

--- packages/vant-cli/site/desktop/main.js
+++ packages/vant-cli/site/desktop/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
+import { packageEntry } from 'site-desktop-shared';
 import App from './App';
 import { router } from './router';
 
-window.app = createApp(App).use(router).mount('#app');
+window.app = createApp(App).use(router).use(packageEntry).mount('#app');
