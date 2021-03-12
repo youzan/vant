c2ac641ec1f20080dc8e71033f2dddd205ae91d9
HaoChuan9421 <hc199421@gmail.com>
2021-03-12 17:58:53 +0800

    feat(vant-cli): 增加用于 demo 组件展示的 playground 组件

--- packages/vant-cli/site/desktop/main.js
+++ packages/vant-cli/site/desktop/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue';
 import { packageEntry } from 'site-desktop-shared';
 import App from './App';
+import DemoPlayground from './components/DemoPlayground';
 import { router } from './router';
 
-window.app = createApp(App).use(router).use(packageEntry).mount('#app');
+window.app = createApp(App)
+  .use(router)
+  .use(packageEntry)
+  .component(DemoPlayground.name, DemoPlayground)
+  .mount('#app');
