6e026e18c3a71f6509fba9e0ddd83906a5674ff1
neverland <chenjiahan@youzan.com>
2021-03-13 18:19:42 +0800

    fix(Swipe): incorrect item width after scaled (#8330)

--- src/swipe/test/demo.spec.ts
+++ src/swipe/test/demo.spec.ts
@@ -1,17 +1,4 @@
 import Demo from '../demo/index.vue';
 import { snapshotDemo } from '../../../test/demo';
-import { mockGetBoundingClientRect } from '../../../test';
 
-let restore: () => void;
-
-snapshotDemo(Demo, {
-  beforeTest: () => {
-    restore = mockGetBoundingClientRect({
-      width: 100,
-      height: 100,
-    });
-  },
-  afterTest: () => {
-    restore();
-  },
-});
+snapshotDemo(Demo);
