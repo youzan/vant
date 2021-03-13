c5ac2b62aa49317139a7a2530fe186470f025a77
nemo-shen <kienshin626@gmail.com>
2021-03-13 17:44:12 +0800

    test(Toast): add test cases (#8324)
    * test(Toast): add test cases
    
    * test(Toast): add test cases
    
    Co-authored-by: nemo-shen <1034131477@qq.com>

--- src/toast/test/index.spec.ts
+++ src/toast/test/index.spec.ts
@@ -40,3 +40,24 @@ test('should close Toast when using closeOnClickOverlay prop and overlay is clic
   wrapper.find('.van-overlay').trigger('click');
   expect(wrapper.emitted('update:show')![0]).toEqual([false]);
 });
+
+test('create a forbidClick toast', async () => {
+  const wrapper = mount(Toast, {
+    props: {
+      show: true,
+      forbidClick: true,
+      type: 'success',
+    },
+  });
+
+  await later();
+  expect(wrapper.html()).toMatchSnapshot();
+  expect(
+    document.body.classList.contains('van-toast--unclickable')
+  ).toBeTruthy();
+
+  await wrapper.setProps({ forbidClick: false });
+  expect(
+    document.body.classList.contains('van-toast--unclickable')
+  ).toBeFalsy();
+});
