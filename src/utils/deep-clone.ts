ece09432539316bd8d64a43b93bdd6f0ed43d05d
nemo-shen <1034131477@qq.com>
2021-03-14 23:03:21 +0800

    fix(utils): 调整deepClone
    解决deepClone没有对数组进行处理的问题

--- src/utils/deep-clone.ts
+++ src/utils/deep-clone.ts
@@ -1,12 +1,21 @@
-import { deepAssign } from './deep-assign';
+import {isDef} from "./base";
 
 export function deepClone<T extends Record<string, any>>(obj: T): T {
+  if (!isDef(obj)) {
+    return obj;
+  }
+
   if (Array.isArray(obj)) {
     return (obj.map((item) => deepClone(item)) as unknown) as T;
   }
 
   if (typeof obj === 'object') {
-    return deepAssign({}, obj) as T;
+    const to = {} as Record<any, any>;
+    Object.keys(obj).forEach((key: string) => {
+      to[key] = deepClone(obj[key]);
+    });
+
+    return to;
   }
 
   return obj;
