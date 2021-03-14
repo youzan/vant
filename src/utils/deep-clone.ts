bf1652e938c9c31cceb00a74ffefbdc7ece50169
nemo-shen <kienshin626@gmail.com>
2021-03-14 10:17:16 +0800

    fix(Cascader): assignKey function deep clone Array (#8326)
    Co-authored-by: nemo-shen <1034131477@qq.com>

--- src/utils/deep-clone.ts
+++ src/utils/deep-clone.ts
@@ -1,12 +1,22 @@
-import { deepAssign } from './deep-assign';
+import { isDef } from './index';
+import { ObjectIndex } from './types';
 
-export function deepClone(obj: object): object {
+export function deepClone(obj: ObjectIndex): object {
+  if (!isDef(obj)) {
+    return obj;
+  }
+
   if (Array.isArray(obj)) {
     return obj.map((item) => deepClone(item));
   }
 
   if (typeof obj === 'object') {
-    return deepAssign({}, obj);
+    const to = {} as ObjectIndex;
+    Object.keys(obj).forEach((key) => {
+      to[key] = deepClone(obj[key]);
+    });
+
+    return to;
   }
 
   return obj;
