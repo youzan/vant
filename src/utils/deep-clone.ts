7145764c00ed175fed357fdafcfb873072de7294
nemo-shen <1034131477@qq.com>
2021-03-15 18:42:35 +0800

    fix(utils): add isDef
    1. 考虑到可能会有参数同时存在oject和null两种可能性，重新增加isDef判断
    2. 补全测试用例

--- src/utils/deep-clone.ts
+++ src/utils/deep-clone.ts
@@ -1,15 +1,21 @@
-export function deepClone<T extends Record<string, any>>(obj: T): T {
+import {isDef} from "./base";
+
+export function deepClone<T extends Record<string, any> | null | undefined>(obj: T): T {
+  if (!isDef(obj)) {
+    return obj;
+  }
+
   if (Array.isArray(obj)) {
     return (obj.map((item) => deepClone(item)) as unknown) as T;
   }
 
   if (typeof obj === 'object') {
-    const to = {} as Record<any, any>;
-    Object.keys(obj).forEach((key: string) => {
+    const to = {} as Record<string, any>;
+    Object.keys(obj).forEach((key) => {
       to[key] = deepClone(obj[key]);
     });
 
-    return to;
+    return to as T;
   }
 
   return obj;
