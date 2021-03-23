24e2f25b7d211bb9c29f93396cd5ff1d6a976d1a
段佳良 <>
2021-03-23 20:09:02 +0800

    test: simulate drag use relative coordinates
    Closes #8386

--- test/event.ts
+++ test/event.ts
@@ -57,10 +57,22 @@ export function triggerDrag(
     | VueWrapper<ComponentPublicInstance<any, any, any>>
     | DOMWrapper<Element>
     | HTMLElement,
-  x = 0,
-  y = 0
-) {
-  trigger(el, 'touchstart', 0, 0);
+  relativeX = 0,
+  relativeY = 0
+): void {
+  let x = relativeX;
+  let y = relativeY;
+  let startX = 0;
+  let startY = 0;
+  if (relativeX < 0) {
+    startX = Math.abs(relativeX);
+    x = 0;
+  }
+  if (relativeY < 0) {
+    startY = Math.abs(relativeY);
+    y = 0;
+  }
+  trigger(el, 'touchstart', startX, startY);
   trigger(el, 'touchmove', x / 4, y / 4);
   trigger(el, 'touchmove', x / 3, y / 3);
   trigger(el, 'touchmove', x / 2, y / 2);
