84332ae68f444576092f122f957560104774fb9f
neverland <chenjiahan@youzan.com>
2021-04-03 13:24:17 +0800

    perf(cli): make jest tests faster (#8453)

--- packages/vant-cli/src/commands/jest.ts
+++ packages/vant-cli/src/commands/jest.ts
@@ -15,15 +15,18 @@ export function test(command: any) {
     watch: command.watch,
     config: JEST_CONFIG_FILE,
     clearCache: command.clearCache,
+    // make jest tests faster
+    // see: https://ivantanev.com/make-jest-faster/
+    maxWorkers: '50%',
   } as any;
 
   runCLI(config, [ROOT])
-    .then(response => {
+    .then((response) => {
       if (!response.results.success && !command.watch) {
         process.exit(1);
       }
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
 
       if (!command.watch) {
