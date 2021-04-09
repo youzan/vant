7fecbf26f77d3e089f01d70876875595cbc1ef4e
chenjiahan <chenjiahan@youzan.com>
2021-04-09 14:39:22 +0800

    types(Stepper): improve theme prop typing

--- src/stepper/index.ts
+++ src/stepper/index.ts
@@ -1,7 +1,8 @@
 import { withInstall } from '../utils';
-import _Stepper from './Stepper';
+import _Stepper, { StepperTheme } from './Stepper';
 
 const Stepper = withInstall<typeof _Stepper>(_Stepper);
 
 export default Stepper;
 export { Stepper };
+export type { StepperTheme };
