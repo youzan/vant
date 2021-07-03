aff77bd9e75c2b3a29c8033bef3abc70aebf9c0c
chenjiahan <chenjiahan@youzan.com>
2021-07-03 19:47:28 +0800

    types: improve inject typing

--- src/composables/use-link-field.ts
+++ src/composables/use-link-field.ts
@@ -1,11 +1,11 @@
 import { ComponentInstance } from '../utils';
-import { watch, inject } from 'vue';
+import { watch, inject, InjectionKey } from 'vue';
 
 export const FORM_KEY = Symbol('van-form');
-export const FIELD_KEY = Symbol('van-field');
+export const FIELD_KEY: InjectionKey<ComponentInstance> = Symbol('van-field');
 
 export function useLinkField(getValue: () => unknown) {
-  const field = inject(FIELD_KEY, null) as ComponentInstance | null;
+  const field = inject(FIELD_KEY, null);
 
   if (field && !field.childFieldValue.value) {
     field.childFieldValue.value = getValue;
