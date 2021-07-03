6e6f80440bb80660310483811314a8f9cbb972e1
chenjiahan <chenjiahan@youzan.com>
2021-07-03 20:08:56 +0800

    types: using InjectionKey in composables

--- src/composables/use-tab-status.ts
+++ src/composables/use-tab-status.ts
@@ -1,8 +1,8 @@
-import { inject, ComputedRef } from 'vue';
+import { inject, ComputedRef, InjectionKey } from 'vue';
 
 // eslint-disable-next-line
-export const TAB_STATUS_KEY = Symbol();
+export const TAB_STATUS_KEY: InjectionKey<ComputedRef<boolean>> = Symbol();
 
 export function useTabStatus() {
-  return inject<ComputedRef<boolean> | null>(TAB_STATUS_KEY, null);
+  return inject(TAB_STATUS_KEY, null);
 }
