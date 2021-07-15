fc3a276f542e3cf77cf035d2e6747dc2035eece7
Jake <Jake.Laoyu@gmail.com>
2021-07-15 15:39:38 +0800

    feat(vant-cli): support jest options

--- packages/vant-cli/src/index.ts
+++ packages/vant-cli/src/index.ts
@@ -37,6 +37,22 @@ command('test')
     '--clearCache',
     'Clears the configured Jest cache directory and then exits'
   )
+  .option(
+    '--changedSince <changedSince>',
+    'Runs tests related to the changes since the provided branch or commit hash'
+  )
+  .option(
+    '--logHeapUsage',
+    'Logs the heap usage after every test. Useful to debug memory leaks'
+  )
+  .option(
+    '--runInBand',
+    'Run all tests serially in the current process, rather than creating a worker pool of child processes that run tests'
+  )
+  .option(
+    '--debug',
+    'Print debugging info about your Jest config'
+  )
   .action(test);
 
 command('clean')
