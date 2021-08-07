3c6ed83277cd079e0197824377bc7ec59dd7d755
chenjiahan <chenjiahan@youzan.com>
2021-08-07 21:34:35 +0800

    docs: remove deprecated vant-waterfall

--- packages/vant-waterfall/src/index.js
+++
@@ -1,8 +0,0 @@
-import Waterfall from './directive';
-
-Waterfall.install = function(Vue) {
-  Vue.directive('WaterfallLower', Waterfall('lower'));
-  Vue.directive('WaterfallUpper', Waterfall('upper'));
-};
-
-export default Waterfall;
