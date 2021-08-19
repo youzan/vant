c67127541e16a3a9c7e5badc06d79d109f6d42c6
neverland <chenjiahan@youzan.com>
2021-08-19 20:18:37 +0800

    feat(cli): bump webpack-dev-server v4 (#9292)
    * feat(cli): bump webpack-dev-server v4
    
    * fix: deps

--- packages/vant-cli/src/common/types.ts
+++ packages/vant-cli/src/common/types.ts
@@ -1,6 +1,5 @@
 import type Webpack from 'webpack';
-import type WebpackDevServer from 'webpack-dev-server';
 
 export type WebpackConfig = Webpack.Configuration & {
-  devServer?: WebpackDevServer.Configuration;
+  devServer?: any;
 };
