816790a875fe5bc4015cf68239cff50126c4f9da
neverland <chenjiahan@youzan.com>
2021-08-20 10:15:17 +0800

    chore(@vant/cli): bump jest v27 (#9293)

--- packages/vant-cli/src/config/jest.config.ts
+++ packages/vant-cli/src/config/jest.config.ts
@@ -8,6 +8,7 @@ import {
 } from '../common/constant';
 
 const DEFAULT_CONFIG = {
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.(css|less|scss)$': JEST_STYLE_MOCK_FILE,
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': JEST_FILE_MOCK_FILE,
@@ -15,7 +16,7 @@ const DEFAULT_CONFIG = {
   setupFilesAfterEnv: [JEST_SETUP_FILE],
   moduleFileExtensions: ['js', 'jsx', 'vue', 'ts', 'tsx'],
   transform: {
-    '\\.(vue)$': 'vue-jest',
+    '\\.(vue)$': 'vue3-jest',
     '\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   transformIgnorePatterns: ['/node_modules/(?!(@vant/cli))/'],
