20d3f41d6df26d87da2eae3279ad0ac66dba7260
qhbin <925078922@qq.com>
2021-08-26 11:51:39 +0800

    fix(Popup): memory leak issue (#9336)
    * fix: 修复弹框组件内存泄漏问题
    
    * fix: es5兼容
    
    Co-authored-by: qiuhuibin <qiuhuibin@bigo.sg>

--- src/mixins/popup/context.ts
+++ src/mixins/popup/context.ts
@@ -13,4 +13,12 @@ export const context = {
   find(vm: any): StackItem | undefined {
     return this.stack.filter((item) => item.vm === vm)[0];
   },
+  remove: function remove(vm: any) {
+    const item = this.find(vm);
+    if(!item) return;
+    item.vm = null;
+    item.overlay = null;
+    const index = this.stack.indexOf(item);
+    this.stack.splice(index, 1);
+  },
 };
