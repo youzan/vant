36bb34432d2df7426e103065165b666930e348b3
qiuhuibin <qiuhuibin@bigo.sg>
2021-08-26 10:41:48 +0800

    fix: es5兼容

--- src/mixins/popup/context.ts
+++ src/mixins/popup/context.ts
@@ -13,11 +13,12 @@ export const context = {
   find(vm: any): StackItem | undefined {
     return this.stack.filter((item) => item.vm === vm)[0];
   },
-  remove: function remove(vm) {
-    const index = this.stack.findIndex(item  => item.vm === vm);
-    if(index > -1) {
-      this.stack[index].vm = null;
-      this.stack.splice(index, 1);
-    }
+  remove: function remove(vm: any) {
+    const item = this.find(vm);
+    if(!item) return;
+    item.vm = null;
+    item.overlay = null;
+    const index = this.stack.indexOf(item);
+    this.stack.splice(index, 1);
   },
 };
