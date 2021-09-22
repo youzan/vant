ba7f45745db96900bb5c53712e346cf451575419
chenjiahan <chenjiahan@youzan.com>
2021-09-22 16:14:02 +0800

    perf(@vant/use): remove raf polyfill

--- packages/vant-use/src/utils.ts
+++ packages/vant-use/src/utils.ts
@@ -1,25 +1,13 @@
 export const inBrowser = typeof window !== 'undefined';
 
-const root = (inBrowser ? window : global) as Window;
-
-let prev = Date.now();
-
-function rafPolyfill(fn: FrameRequestCallback): number {
-  const curr = Date.now();
-  const ms = Math.max(0, 16 - (curr - prev));
-  const id = setTimeout(fn, ms);
-  prev = curr + ms;
-  return id;
-}
-
 export function raf(fn: FrameRequestCallback): number {
-  const requestAnimationFrame = root.requestAnimationFrame || rafPolyfill;
-  return requestAnimationFrame.call(root, fn);
+  return inBrowser ? requestAnimationFrame(fn) : -1;
 }
 
 export function cancelRaf(id: number) {
-  const cancelAnimationFrame = root.cancelAnimationFrame || root.clearTimeout;
-  cancelAnimationFrame.call(root, id);
+  if (inBrowser) {
+    cancelAnimationFrame(id);
+  }
 }
 
 // double raf for animation
