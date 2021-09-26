986ba9bbc2bee03d62d9973cad3f4c1dd8484e9b
Forrest <rainforest92@126.com>
2021-09-26 22:37:01 +0800

    数据列表组件下拉刷新等

--- src-vusion/components/list-view/index.vue
+++ src-vusion/components/list-view/index.vue
@@ -1,15 +1,86 @@
+<template>
+<div :class="$style.root" :readonly="readonly" :readonly-mode="readonlyMode" :disabled="disabled"
+    :tabindex="readonly || disabled ? '' : 0"
+    @keydown.prevent.up="shift(-1)"
+    @keydown.prevent.down="shift(+1)">
+    <div v-show="showHead" :class="$style.head">
+        <slot name="head">
+            <u-checkbox v-if="multiple" :value="allChecked" @check="checkAll($event.value)"></u-checkbox>
+            <span :class="$style.title" vusion-slot-name="title">{{ title }}</span>
+            <div :class="$style.extra">
+                <span v-if="multiple">{{ selectedVMs.length }}{{ currentDataSource && currentDataSource.originTotal !== Infinity ? ' / ' + currentDataSource.originTotal : '' }}</span>
+            </div>
+        </slot>
+    </div>
+    <u-input v-if="filterable" :class="$style.filter" :disabled="disabled" :placeholder="placeholder" size="small" suffix="search" :clearable="clearable"
+        :value="filterText" @input="onInput">
+    </u-input>
+    <van-pull-refresh :value="currentLoading" :disabled="!pullRefresh"
+        :pullingText="pullingText" :loosingText="loosingText" :loadingText="loadingText" :successText="successText" :successDuration="successDuration" :pullDistance="pullDistance"
+        @refresh="reload">
+        <div ref="body" :class="$style.body" @scroll.stop="onScroll">
+            <slot></slot>
+            <div ref="virtual" v-if="(!currentLoading && !currentError || pageable === 'auto-more' || pageable === 'load-more') && currentData && currentData.length"
+                :style="{ paddingTop: virtualTop + 'px', paddingBottom: virtualBottom + 'px' }">
+                <component :is="ChildComponent"
+                    v-for="item in virtualList"
+                    v-if="item"
+                    :key="$at(item, valueField)"
+                    :value="$at(item, valueField)"
+                    :disabled="item.disabled || disabled"
+                    :item="item"
+                ><slot name="item" :item="item" :text="$at(item, field || textField)" :value="$at(item, valueField)" :disabled="item.disabled || disabled">{{ $at(item, field || textField) }}</slot>
+                </component>
+            </div>
+            <div :class="$style.status" status="loading" v-if="currentLoading">
+                <slot name="loading"><u-spinner></u-spinner> {{ loadingText }}</slot>
+            </div>
+            <div :class="$style.status" status="error" v-else-if="currentData === null || currentError">
+                <slot name="error">{{ errorText }}</slot>
+            </div>
+            <div :class="$style.status" v-else-if="pageable === 'load-more' && currentDataSource.hasMore()">
+                <u-link @click="load(true)">{{ $t('loadMore') }}</u-link>
+            </div>
+            <div :class="$style.status" v-else-if="(pageable === 'auto-more' || pageable === 'load-more') && !currentDataSource.hasMore()">
+                {{ $t('noMore') }}
+            </div>
+            <div :class="$style.status" v-else-if="currentData && !currentData.length">
+                <slot name="empty">{{ emptyText }}</slot>
+            </div>
+        </div>
+    </van-pull-refresh>
+    <div v-show="showFoot || (pageable === true || pageable === 'pagination') && currentDataSource.total > currentDataSource.paging.size" :class="$style.foot">
+        <slot name="foot"></slot>
+        <u-pagination :class="$style.pagination" v-if="pageable === true || pageable === 'pagination'"
+            :total-items="currentDataSource.total" :page="currentDataSource.paging.number"
+            :page-size="currentDataSource.paging.size" :side="1" :around="3"
+            @change="page($event.page)">
+        </u-pagination>
+    </div>
+</div>
+</template>
+
 <script>
 import UListView from 'cloud-ui.vusion/src/components/u-list-view.vue/index.vue';
+import VanPullRefresh from '../../../src/pull-refresh';
 
 export default {
     name: 'van-list-view',
     groupName: 'van-list-view-group',
     childName: 'van-list-view-item',
     extends: UListView,
+    components: { VanPullRefresh },
     props: {
         border: { type: Boolean, default: false },
         readonly: { type: Boolean, default: true },
         readonlyMode: { type: String, default: 'initial' },
+        pageSize: { type: Number, default: 20 },
+        pullRefresh: { type: Boolean, default: true },
+        pullingText: { type: String, default: '下拉刷新' },
+        loosingText: { type: String, default: '释放刷新' },
+        successText: { type: String, default: '已刷新' },
+        successDuration: 500,
+        pullDistance: 50,
     },
     getDataSourceOptions() {
         return {
