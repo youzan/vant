e27e6256c76beca4cf57378350e3ad1138265093
hzhuangjianqiang <hzhuangjianqiang@corp.netease.com>
2021-09-26 17:31:14 +0800

    feat: add calendar pop

--- src/cascader/docs/blocks.md
+++ src/cascader/docs/blocks.md
@@ -1,5 +1,5 @@
 ### 基础用法
 
 ``` html
-  <van-cascader title="请选择" converter="json"></van-cascader>
+  <van-cascader title="请选择" converter="json" label-field="级联选择"></van-cascader>
 ```
--- src/datetime-picker/shared.js
+++ src/datetime-picker/shared.js
@@ -81,7 +81,7 @@ export const TimePickerMixin = {
 
     onConfirm() {
       this.$emit('input', this.innerValue)
-      this.$emit('update:value', this.type==="datetime" ? this.innerValue.formath("yyyy-MM-dd HH:mm:ss") : this.innerValue);
+      this.$emit('update:value', this.type==="datetime" ? this.innerValue.formath("yyyy/MM/dd HH:mm:ss") : this.innerValue);
       this.$emit('confirm', this.innerValue);
     },
 
