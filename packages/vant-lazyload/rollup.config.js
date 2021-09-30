091b5c7ff852f51fa4e92a5a80d54a238f599d3a
neverland <chenjiahan@youzan.com>
2021-09-30 11:19:38 +0800

    build(@vant/lazyload): using rollup to bundle (#9598)

--- packages/vant-lazyload/rollup.config.js
+++ packages/vant-lazyload/rollup.config.js
@@ -3,7 +3,7 @@ import babel from '@rollup/plugin-babel';
 import nodeResolve from '@rollup/plugin-node-resolve';
 
 export default {
-  input: path.join(__dirname, 'src', 'index.ts'),
+  input: path.join(__dirname, 'src', 'index.js'),
   output: [
     {
       dir: 'dist/cjs',
@@ -14,6 +14,7 @@ export default {
       format: 'esm',
     },
   ],
+  external: ['vue', '@vant/use'],
   plugins: [
     babel({ babelHelpers: 'bundled', extensions: ['.js', '.ts'] }),
     nodeResolve(),
