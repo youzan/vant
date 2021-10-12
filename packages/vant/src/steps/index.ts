4bc2557c5ab19622db097ee0d9343aa28692a3f6
neverland <chenjiahan@youzan.com>
2021-10-12 17:28:28 +0800

    types(Steps): export StepsDirection type (#9664)
    * types(Steps): export StepsDirection type
    
    * fix: evnt
    
    * chore: upd
    
    * Revert "chore: upd"
    
    This reverts commit df01337207ced0ad54e6e4a6172dac255bd292ac.

--- packages/vant/src/steps/index.ts
+++ packages/vant/src/steps/index.ts
@@ -1,5 +1,6 @@
 import { withInstall } from '../utils';
-import _Steps from './Steps';
+import _Steps, { StepsDirection } from './Steps';
 
 export const Steps = withInstall(_Steps);
 export default Steps;
+export type { StepsDirection };
