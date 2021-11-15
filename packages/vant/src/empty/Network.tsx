d2d16042763e66b96a004190d942e2da91dcd931
neverland <chenjiahan@youzan.com>
2021-11-15 19:32:29 +0800

    perf: minify svg icons (#9869)

--- packages/vant/src/empty/Network.tsx
+++ packages/vant/src/empty/Network.tsx
@@ -6,11 +6,11 @@ const renderStop = (color: string, offset: number, opacity?: number) => (
 export const Network = (
   <svg viewBox="0 0 160 160">
     <defs>
-      <linearGradient id={`${prefix}1`} x1="64.022%" y1="100%" x2="64.022%">
+      <linearGradient id={`${prefix}1`} x1="64%" y1="100%" x2="64%">
         {renderStop('#FFF', 0, 0.5)}
         {renderStop('#F2F3F5', 100)}
       </linearGradient>
-      <linearGradient id={`${prefix}2`} x1="50%" x2="50%" y2="84.459%">
+      <linearGradient id={`${prefix}2`} x1="50%" x2="50%" y2="84%">
         {renderStop('#EBEDF0', 0)}
         {renderStop('#DCDEE0', 100, 0)}
       </linearGradient>
@@ -18,18 +18,14 @@ export const Network = (
         {renderStop('#EAEDF0', 0)}
         {renderStop('#DCDEE0', 100)}
       </linearGradient>
-      <linearGradient id={`${prefix}4`} x1="100%" y1="100%" x2="100%">
-        {renderStop('#EAEDF0', 0)}
-        {renderStop('#DCDEE0', 100)}
-      </linearGradient>
       <radialGradient
-        id={`${prefix}5`}
+        id={`${prefix}4`}
         cx="50%"
         cy="0%"
         fx="50%"
         fy="0%"
         r="100%"
-        gradientTransform="matrix(0 1 -.54835 0 .5 -.5)"
+        gradientTransform="matrix(0 1 -.54 0 .5 -.5)"
       >
         {renderStop('#EBEDF0', 0)}
         {renderStop('#FFF', 100, 0)}
@@ -37,49 +33,30 @@ export const Network = (
     </defs>
     <g fill="none">
       <g opacity=".8">
-        <path
-          d="M0 124V46h20v20h14v58H0z"
-          fill={`url(#${prefix}1)`}
-          transform="matrix(-1 0 0 1 36 7)"
-        />
-        <path
-          d="M121 8h22.231v14H152v77.37h-31V8z"
-          fill={`url(#${prefix}1)`}
-          transform="translate(2 7)"
-        />
+        <path d="M36 131V53H16v20H2v58h34z" fill={`url(#${prefix}1)`} />
+        <path d="M123 15h22v14h9v77h-31V15z" fill={`url(#${prefix}1)`} />
       </g>
-      <path fill={`url(#${prefix}5)`} d="M0 139h160v21H0z" />
+      <path fill={`url(#${prefix}4)`} d="M0 139h160v21H0z" />
       <path
-        d="M37 18a7 7 0 013 13.326v26.742c0 1.23-.997 2.227-2.227 2.227h-1.546A2.227 2.227 0 0134 58.068V31.326A7 7 0 0137 18z"
+        d="M80 54a7 7 0 0 1 3 13v27l-2 2h-2a2 2 0 0 1-2-2V67a7 7 0 0 1 3-13z"
         fill={`url(#${prefix}2)`}
-        transform="translate(43 36)"
       />
       <g opacity=".6" stroke-linecap="round" stroke-width="7">
         <path
-          d="M20.875 11.136a18.868 18.868 0 00-5.284 13.121c0 5.094 2.012 9.718 5.284 13.12"
+          d="M64 47a19 19 0 0 0-5 13c0 5 2 10 5 13"
           stroke={`url(#${prefix}3)`}
-          transform="translate(43 36)"
         />
+        <path d="M53 36a34 34 0 0 0 0 48" stroke={`url(#${prefix}3)`} />
         <path
-          d="M9.849 0C3.756 6.225 0 14.747 0 24.146c0 9.398 3.756 17.92 9.849 24.145"
+          d="M95 73a19 19 0 0 0 6-13c0-5-2-9-6-13"
           stroke={`url(#${prefix}3)`}
-          transform="translate(43 36)"
         />
-        <path
-          d="M57.625 11.136a18.868 18.868 0 00-5.284 13.121c0 5.094 2.012 9.718 5.284 13.12"
-          stroke={`url(#${prefix}4)`}
-          transform="rotate(-180 76.483 42.257)"
-        />
-        <path
-          d="M73.216 0c-6.093 6.225-9.849 14.747-9.849 24.146 0 9.398 3.756 17.92 9.849 24.145"
-          stroke={`url(#${prefix}4)`}
-          transform="rotate(-180 89.791 42.146)"
-        />
+        <path d="M106 84a34 34 0 0 0 0-48" stroke={`url(#${prefix}3)`} />
       </g>
       <g transform="translate(31 105)">
         <rect fill="#EBEDF0" width="98" height="34" rx="2" />
-        <rect fill="#FFF" x="9" y="8" width="80" height="18" rx="1.114" />
-        <rect fill="#EBEDF0" x="15" y="12" width="18" height="6" rx="1.114" />
+        <rect fill="#FFF" x="9" y="8" width="80" height="18" rx="1.1" />
+        <rect fill="#EBEDF0" x="15" y="12" width="18" height="6" rx="1.1" />
       </g>
     </g>
   </svg>
