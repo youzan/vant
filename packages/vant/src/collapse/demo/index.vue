1d3735280e738738f36b5e7adbd7e4b5a643860d
neverland <chenjiahan@youzan.com>
2021-11-30 20:42:07 +0800

    docs: add poetry, haiku and words (#9971)
    * docs: add poetry, haiku and words
    
    * chore: update snapshot

--- packages/vant/src/collapse/demo/index.vue
+++ packages/vant/src/collapse/demo/index.vue
@@ -7,14 +7,18 @@ import { useTranslate } from '../../../docs/site/use-translate';
 
 const t = useTranslate({
   'zh-CN': {
+    text1: '代码是写出来给人看的，附带能在机器上运行。',
+    text2: '技术无非就是那些开发它的人的共同灵魂。',
+    text3: '在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。',
     accordion: '手风琴',
     titleSlot: '自定义标题内容',
-    text: '代码是写出来给人看的，附带能在机器上运行',
   },
   'en-US': {
+    text1: 'Content 1',
+    text2: 'Content 2',
+    text3: 'Content 3',
     accordion: 'Accordion',
     titleSlot: 'Custom title',
-    text: 'Content',
   },
 });
 
@@ -28,13 +32,13 @@ const active4 = ref([]);
   <demo-block :title="t('basicUsage')">
     <van-collapse v-model="active1">
       <van-collapse-item :title="t('title') + 1">
-        {{ t('text') }}
+        {{ t('text1') }}
       </van-collapse-item>
       <van-collapse-item :title="t('title') + 2">
-        {{ t('text') }}
+        {{ t('text2') }}
       </van-collapse-item>
       <van-collapse-item :title="t('title') + 3">
-        {{ t('text') }}
+        {{ t('text3') }}
       </van-collapse-item>
     </van-collapse>
   </demo-block>
@@ -42,13 +46,13 @@ const active4 = ref([]);
   <demo-block :title="t('accordion')">
     <van-collapse v-model="active2" accordion>
       <van-collapse-item :title="t('title') + 1">
-        {{ t('text') }}
+        {{ t('text1') }}
       </van-collapse-item>
       <van-collapse-item :title="t('title') + 2">
-        {{ t('text') }}
+        {{ t('text2') }}
       </van-collapse-item>
       <van-collapse-item :title="t('title') + 3">
-        {{ t('text') }}
+        {{ t('text3') }}
       </van-collapse-item>
     </van-collapse>
   </demo-block>
@@ -56,13 +60,13 @@ const active4 = ref([]);
   <demo-block :title="t('disabled')">
     <van-collapse v-model="active3">
       <van-collapse-item :title="t('title') + 1">
-        {{ t('text') }}
+        {{ t('text1') }}
       </van-collapse-item>
       <van-collapse-item :title="t('title') + 2" disabled>
-        {{ t('text') }}
+        {{ t('text2') }}
       </van-collapse-item>
       <van-collapse-item :title="t('title') + 3" disabled>
-        {{ t('text') }}
+        {{ t('text3') }}
       </van-collapse-item>
     </van-collapse>
   </demo-block>
@@ -73,14 +77,14 @@ const active4 = ref([]);
         <template #title>
           {{ t('title') + 1 }}<van-icon name="question-o" />
         </template>
-        {{ t('text') }}
+        {{ t('text1') }}
       </van-collapse-item>
       <van-collapse-item
         :title="t('title') + 2"
         :value="t('content')"
         icon="shop-o"
       >
-        {{ t('text') }}
+        {{ t('text2') }}
       </van-collapse-item>
     </van-collapse>
   </demo-block>
