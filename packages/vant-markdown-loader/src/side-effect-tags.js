3dccdf3b96d295e790371d68a60b5edb55f55284
chenjiahan <chenjiahan@youzan.com>
2021-12-09 21:31:02 +0800

    chore: fix CodeQL for RegExp

--- packages/vant-markdown-loader/src/side-effect-tags.js
+++ packages/vant-markdown-loader/src/side-effect-tags.js
@@ -2,13 +2,16 @@ module.exports = function sideEffectTags(content) {
   const styles = [];
 
   // 从模版中移除 script 标签
-  content = content.replace(/<script[\s\S]*?>[\s\S]*?<\/script>/g, '');
+  content = content.replace(/<script[\s\S]*?>[\s\S]*?<\/script>/gi, '');
 
   // 从模版中移除 style 标签，并收集到 styles 数组中，以转移为 .vue 文件 的 style 标签
-  content = content.replace(/<style[\s\S]*?>([\s\S]*?)<\/style>/g, (_, css) => {
-    styles.push(`<style scoped>${css}</style>`);
-    return '';
-  });
+  content = content.replace(
+    /<style[\s\S]*?>([\s\S]*?)<\/style>/gi,
+    (_, css) => {
+      styles.push(`<style scoped>${css}</style>`);
+      return '';
+    }
+  );
 
   return [content, styles];
 };
