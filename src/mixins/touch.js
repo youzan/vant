9c16a6ad2bef05201e2e0136143470072906fc02
neverland <chenjiahan@youzan.com>
2021-12-23 17:24:20 +0800

    fix(PullRefresh): may trigger browser bounce in some cases (#10090)

--- src/mixins/touch.js
+++ src/mixins/touch.js
@@ -1,13 +1,11 @@
 import { on } from '../utils/dom/event';
 
-const MIN_DISTANCE = 10;
-
 function getDirection(x, y) {
-  if (x > y && x > MIN_DISTANCE) {
+  if (x > y) {
     return 'horizontal';
   }
 
-  if (y > x && y > MIN_DISTANCE) {
+  if (y > x) {
     return 'vertical';
   }
 
@@ -28,13 +26,21 @@ export const TouchMixin = {
 
     touchMove(event) {
       const touch = event.touches[0];
-      // Fix: Safari back will set clientX to negative number
+      // safari back will set clientX to negative number
       this.deltaX = touch.clientX < 0 ? 0 : touch.clientX - this.startX;
       this.deltaY = touch.clientY - this.startY;
       this.offsetX = Math.abs(this.deltaX);
       this.offsetY = Math.abs(this.deltaY);
-      this.direction =
-        this.direction || getDirection(this.offsetX, this.offsetY);
+
+      // lock direction when distance is greater than a certain value
+      const LOCK_DIRECTION_DISTANCE = 10;
+      if (
+        !this.direction ||
+        (this.offsetX < LOCK_DIRECTION_DISTANCE &&
+          this.offsetY < LOCK_DIRECTION_DISTANCE)
+      ) {
+        this.direction = getDirection(this.offsetX, this.offsetY);
+      }
     },
 
     resetTouchStatus() {
