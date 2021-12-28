2f00bd78538c9032bb741e7abb8c8e53a3e103b4
chenjiahan <chenjiahan@youzan.com>
2021-12-28 11:42:52 +0800

    perf: caching handlers of useRefs

--- packages/vant/src/composables/use-refs.ts
+++ packages/vant/src/composables/use-refs.ts
@@ -2,13 +2,19 @@ import { ref, Ref, onBeforeUpdate } from 'vue';
 
 export function useRefs<T = Element>() {
   const refs = ref([]) as Ref<T[]>;
+  const cache: Array<(el: unknown) => void> = [];
 
   onBeforeUpdate(() => {
     refs.value = [];
   });
 
-  const setRefs = (index: number) => (el: unknown) => {
-    refs.value[index] = el as T;
+  const setRefs = (index: number) => {
+    if (!cache[index]) {
+      cache[index] = (el: unknown) => {
+        refs.value[index] = el as T;
+      };
+    }
+    return cache[index];
   };
 
   return [refs, setRefs] as const;
