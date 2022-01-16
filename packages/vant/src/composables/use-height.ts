47e75d8f06870e9f7313b8cb7b6c22b8dac71e31
hengshenyu <yuhengshen@qq.com>
2022-01-16 14:35:56 +0800

    fix: useHeight may get the wrong height

--- packages/vant/src/composables/use-height.ts
+++ packages/vant/src/composables/use-height.ts
@@ -4,11 +4,15 @@ import { Ref, ref, onMounted, nextTick } from 'vue';
 export const useHeight = (element: Element | Ref<Element | undefined>) => {
   const height = ref<number>();
 
-  onMounted(() =>
+  onMounted(() => {
     nextTick(() => {
       height.value = useRect(element).height;
-    })
-  );
+    });
+    // https://github.com/youzan/vant/issues/10131
+    setTimeout(() => {
+      height.value = useRect(element).height;
+    }, 100);
+  });
 
   return height;
 };
