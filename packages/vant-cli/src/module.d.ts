71a2e262593326d19ed40ee66e555bfd4ef80845
neverland <chenjiahan@youzan.com>
2022-01-17 15:14:13 +0800

    feat(@vant/cli): using esbuild to minify css (#10204)

--- packages/vant-cli/src/module.d.ts
+++ packages/vant-cli/src/module.d.ts
@@ -1,7 +1,6 @@
 // some modules with missing type definitions
 declare module 'execa';
 declare module 'hash-sum';
-declare module 'clean-css';
 declare module 'release-it';
 declare module 'conventional-changelog';
 declare module '@vant/markdown-vetur';
