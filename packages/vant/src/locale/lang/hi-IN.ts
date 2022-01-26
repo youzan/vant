c27944f735ce115c2d6d1b173750193cf2c285c8
chenjiahan <chenjiahan@youzan.com>
2022-01-26 11:53:01 +0800

    feat(Locale): add Hindi translation

---
+++ packages/vant/src/locale/lang/hi-IN.ts
@@ -0,0 +1,71 @@
+export default {
+  name: 'नाम',
+  tel: 'फ़ोन',
+  save: 'सहेजें',
+  confirm: 'पुष्टि करना',
+  cancel: 'रद्द करें',
+  delete: 'हटाएं',
+  loading: 'लोड हो रहा है...',
+  noCoupon: 'कोई कूपन नहीं',
+  nameEmpty: 'कृपया नाम भरें',
+  addContact: 'संपर्क जोड़ें',
+  telInvalid: 'विकृत फ़ोन नंबर',
+  vanCalendar: {
+    end: 'समाप्त',
+    start: 'शुरू',
+    title: 'पंचांग',
+    weekdays: [
+      'रविवार',
+      'सोमवार',
+      'मंगलवार',
+      'बुधवार',
+      'गुरूवार',
+      'शुक्रवार',
+      'शनिवार',
+    ],
+    monthTitle: (year: number, month: number) => `${year}/${month}`,
+    rangePrompt: (maxRange: number) => `${maxRange} दिन से अधिक नहीं चुनें`,
+  },
+  vanCascader: {
+    select: 'चुनते हैं',
+  },
+  vanPagination: {
+    prev: 'पहले का',
+    next: 'अगला',
+  },
+  vanPullRefresh: {
+    pulling: 'रीफ़्रेश करने के लिए खींचें...',
+    loosing: 'ताज़ा करने के लिए ढीला...',
+  },
+  vanSubmitBar: {
+    label: 'कुल：',
+  },
+  vanCoupon: {
+    unlimited: 'असीमित',
+    discount: (discount: number) => `${discount * 10}% छूट`,
+    condition: (condition: number) => `कम से कम ${condition}`,
+  },
+  vanCouponCell: {
+    title: 'कूपन',
+    count: (count: number) => `आपके पास ${count} कूपन हैं`,
+  },
+  vanCouponList: {
+    exchange: 'अदला बदली',
+    close: 'बंद करे',
+    enable: 'उपलब्ध',
+    disabled: 'अनुपलब्ध',
+    placeholder: 'कूपन कोड',
+  },
+  vanAddressEdit: {
+    area: 'क्षेत्र',
+    postal: 'डाक का',
+    areaEmpty: 'कृपया एक क्षेत्र चुनें',
+    addressEmpty: 'पता खाली नहीं हो सकता',
+    postalEmpty: 'गलत पोस्टल कोड',
+    addressDetail: 'पता',
+    defaultAddress: 'डिफ़ॉल्ट पते के रूप में सेट करें',
+  },
+  vanAddressList: {
+    add: 'नया पता जोड़ें',
+  },
+};
