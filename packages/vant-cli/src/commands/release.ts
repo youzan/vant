60d6b416450ce288373bacb8efecc7e87ad479dc
chenjiahan <chenjiahan@youzan.com>
2022-02-07 17:10:57 +0800

    feat(@vant/cli): support specify tag

--- packages/vant-cli/src/commands/release.ts
+++ packages/vant-cli/src/commands/release.ts
@@ -4,10 +4,13 @@ import { join } from 'path';
 
 const PLUGIN_PATH = join(__dirname, '../compiler/vant-cli-release-plugin.js');
 
-export async function release() {
+export async function release(command: { tag?: string }) {
   await releaseIt({
     plugins: {
       [PLUGIN_PATH]: {},
+    },
+    npm: {
+      tag: command.tag,
     },
     git: {
       tagName: 'v${version}',
