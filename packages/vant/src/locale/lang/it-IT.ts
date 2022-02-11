5b7c8883a355ff805f8ca93b434e6b333952e4af
neverland <chenjiahan@youzan.com>
2022-02-11 16:29:48 +0800

    feat(Locale): add Italain translation (#10288)

---
+++ packages/vant/src/locale/lang/it-IT.ts
@@ -0,0 +1,71 @@
+export default {
+  name: 'Nome',
+  tel: 'Telefono',
+  save: 'Salva',
+  confirm: 'Conferma',
+  cancel: 'Annulla',
+  delete: 'Elimina',
+  loading: 'Caricamento in corso...',
+  noCoupon: 'Nessun coupon',
+  nameEmpty: 'Inserisci il nome',
+  addContact: 'Aggiungi contatto',
+  telInvalid: 'Numero di telefono errato',
+  vanCalendar: {
+    end: 'Fine',
+    start: 'Inizio',
+    title: 'Calendario',
+    weekdays: [
+      'domenica',
+      'Lunedi',
+      'Martedì',
+      'mercoledì',
+      'giovedì',
+      'venerdì',
+      'Sabato',
+    ],
+    monthTitle: (year: number, month: number) => `${year}/${month}`,
+    rangePrompt: (maxRange: number) => `Scegli non più di ${maxRange} giorni`,
+  },
+  vanCascader: {
+    select: 'Seleziona',
+  },
+  vanPagination: {
+    prev: 'Precedente',
+    next: 'Avanti',
+  },
+  vanPullRefresh: {
+    pulling: 'Tiri per aggiornare...',
+    loosing: 'Largo per rinfrescare...',
+  },
+  vanSubmitBar: {
+    label: 'Totale:',
+  },
+  vanCoupon: {
+    unlimited: 'Illimitato',
+    discount: (discount: number) => `${discount * 10}% di sconto`,
+    condition: (condition: number) => `Almeno ${condition}`,
+  },
+  vanCouponCell: {
+    title: 'Buono',
+    count: (count: number) => `Hai ${count} coupon`,
+  },
+  vanCouponList: {
+    exchange: 'Scambio',
+    close: 'Chiudi',
+    enable: 'Disponibile',
+    disabled: 'Non disponibile',
+    placeholder: 'Codice coupon',
+  },
+  vanAddressEdit: {
+    area: 'Area',
+    postal: 'Postale',
+    areaEmpty: "Seleziona un'area di ricezione",
+    addressEmpty: "L'indirizzo non può essere vuoto",
+    postalEmpty: 'Codice postale errato',
+    addressDetail: 'Indirizzo',
+    defaultAddress: 'Imposta come indirizzo predefinito',
+  },
+  vanAddressList: {
+    add: 'Aggiungi nuovo indirizzo',
+  },
+};
