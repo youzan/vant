bc4e1cd17e932e52aec7adca56981e3b28cfb5a8
ready-research <72916209+ready-research@users.noreply.github.com>
2022-03-03 13:39:07 +0530

    fix(vant-cli): decamelize ReDoS (#10351)
    * [Security] Fix ReDoS
    
    Fix ReDoS
    
    * Fix ReDoS
    
    Fix ReDoS

--- packages/vant-cli/site/common/index.js
+++ packages/vant-cli/site/common/index.js
@@ -4,7 +4,7 @@ const isMobile = /ios|iphone|ipod|ipad|android/.test(ua);
 export function decamelize(str, sep = '-') {
   return str
     .replace(/([a-z\d])([A-Z])/g, '$1' + sep + '$2')
-    .replace(/([A-Z]+)([A-Z][a-z\d]+)/g, '$1' + sep + '$2')
+    .replace(/([A-Z])([A-Z][a-z\d]+)/g, '$1' + sep + '$2')
     .toLowerCase();
 }
 
--- packages/vant-cli/src/common/index.ts
+++ packages/vant-cli/src/common/index.ts
@@ -72,7 +72,7 @@ export function pascalize(str: string): string {
 export function decamelize(str: string, sep = '-') {
   return str
     .replace(/([a-z\d])([A-Z])/g, '$1' + sep + '$2')
-    .replace(/([A-Z]+)([A-Z][a-z\d]+)/g, '$1' + sep + '$2')
+    .replace(/([A-Z])([A-Z][a-z\d]+)/g, '$1' + sep + '$2')
     .toLowerCase();
 }
 
