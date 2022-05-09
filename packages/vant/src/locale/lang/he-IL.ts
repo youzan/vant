68a7b17f3679674ac3ee8e8bca63d2c9b71cbe7a
Maor Nissan <mr.nisan@gmail.com>
2022-05-09 08:04:12 +0300

    feat(Locale): Added he-IL support. (#10591)

---
+++ packages/vant/src/locale/lang/he-IL.ts
@@ -0,0 +1,63 @@
+export default {
+  name: 'שם',
+  tel: 'טלפון',
+  save: 'שמור',
+  confirm: 'אישור',
+  cancel: 'ביטול',
+  delete: 'מחיקה',
+  loading: 'טוען...',
+  noCoupon: 'אין קופונים',
+  nameEmpty: 'אנא מלא את השדה',
+  addContact: 'הוסף איש-קשר',
+  telInvalid: 'מספר טלפון שגוי',
+  vanCalendar: {
+    end: 'סוף',
+    start: 'התחלה',
+    title: 'לוח שנה',
+    weekdays: ['ראשון', 'שני', 'שלישי', 'רביעי', 'חמישי', 'שישי', 'שבת'],
+    monthTitle: (year: number, month: number) => `${year}/${month}`,
+    rangePrompt: (maxRange: number) => ` בחר לא יותר מ ${maxRange} ימים `,
+  },
+  vanCascader: {
+    select: 'בחר',
+  },
+  vanPagination: {
+    prev: 'הקודם',
+    next: 'הבא',
+  },
+  vanPullRefresh: {
+    pulling: 'גרור כדי לרענן',
+    loosing: 'שחרר כדי לרענן',
+  },
+  vanSubmitBar: {
+    label: 'סך הכל:',
+  },
+  vanCoupon: {
+    unlimited: 'ללא הגבלה',
+    discount: (discount: number) => `${discount * 10}% הנחה`,
+    condition: (condition: number) => ` לפחות ${condition}`,
+  },
+  vanCouponCell: {
+    title: 'קופון',
+    count: (count: number) => ` יש לך ${count} קופונים `,
+  },
+  vanCouponList: {
+    exchange: 'החלפה',
+    close: 'סגירה',
+    enable: 'זמינים',
+    disabled: 'לא זמינים',
+    placeholder: 'קוד קופון',
+  },
+  vanAddressEdit: {
+    area: 'איזור',
+    postal: 'מיקוד',
+    areaEmpty: 'אנא בחר איזור קבלה',
+    addressEmpty: 'יש למלא כתובת',
+    postalEmpty: 'טעות במיקוד',
+    addressDetail: 'כתובת',
+    defaultAddress: 'הגדר ככתובת ברירת מחדש',
+  },
+  vanAddressList: {
+    add: 'הוספת כתובת חדשה',
+  },
+};
