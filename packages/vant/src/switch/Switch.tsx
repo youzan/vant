586bc21dd311ac81c59e3c167a27e54c6dd1b4f5
neverland <chenjiahan@youzan.com>
2022-06-04 20:29:28 +0800

    feat(Switch): add background slot (#10666)
    * feat(Switch): add background slot
    
    * chore: revert lock file
    
    * chore: new line

--- packages/vant/src/switch/Switch.tsx
+++ packages/vant/src/switch/Switch.tsx
@@ -76,6 +76,7 @@ export default defineComponent({
           onClick={onClick}
         >
           <div class={bem('node')}>{renderLoading()}</div>
+          {slots.background?.()}
         </div>
       );
     };
