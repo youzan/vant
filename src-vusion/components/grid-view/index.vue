428ccc26e628d0fb20e81d15f583c829f1d03308
qingniao99 <huangjianqiang126@163.com>
2022-06-28 19:14:11 +0800

    fix: cardu样式优化 2 0628

--- src-vusion/components/grid-view/index.vue
+++ src-vusion/components/grid-view/index.vue
@@ -6,6 +6,7 @@
     :disabled="disabled"
     :tabindex="readonly || disabled ? '' : 0"
     :vusion-designer="$env.VUE_APP_DESIGNER"
+    style="padding: 10px;"
     ref="root"
   >
     <div v-show="showHead" :class="$style.head">
@@ -405,7 +406,7 @@ export default {
       this.$forceUpdate();
     },
     getWidth() {
-      return ((this.$refs.root.getBoundingClientRect() || {}).width-6) || 0;
+      return ((this.$refs.root.getBoundingClientRect() || {}).width-26) || 0;
     },
     async resize(start = 0) {
       if (!this.$refs.body) return;
