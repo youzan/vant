43f19f374cb454bc51f46dd806f17808ece5ca7c
chenjiahan <chenjiahan.jait@bytedance.com>
2022-07-02 22:33:06 +0800

    chore: bump typescript v4.7

--- packages/vant/src/composables/use-expose.ts
+++ packages/vant/src/composables/use-expose.ts
@@ -5,6 +5,6 @@ import { extend } from '../utils';
 export function useExpose<T = Record<string, any>>(apis: T) {
   const instance = getCurrentInstance();
   if (instance) {
-    extend(instance.proxy, apis);
+    extend(instance.proxy as object, apis);
   }
 }
