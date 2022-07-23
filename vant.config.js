7f71f2a62d9621dd6cc4edc6341cecced76f793e
chenjiahan <chenjiahan.jait@bytedance.com>
2022-07-23 22:28:48 +0800

    chore: update baidu analytics seed

--- vant.config.js
+++ vant.config.js
@@ -17,7 +17,7 @@ module.exports = {
       { label: 'v4', link: '/vant/v4/' },
     ],
     baiduAnalytics: {
-      seed: 'ad6b5732c36321f2dafed737ac2da92f',
+      seed: 'af5d41bc4e446e76665dbe3ec18d55c3',
     },
     htmlPluginOptions: {
       meta: {
