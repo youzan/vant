9957509be64c4dbbb45c9b9166d96ab353026b1a
qingniao99 <huangjianqiang126@163.com>
2022-08-04 17:33:21 +0800

    feat: add cellson

---
+++ src/cellson/README.md
@@ -0,0 +1,216 @@
+# Cell
+
+### Install
+
+```js
+import Vue from 'vue';
+import { Cell, CellGroup } from 'vant';
+
+Vue.use(Cell);
+Vue.use(CellGroup);
+```
+
+## Usage
+
+### Basic Usage
+
+```html
+<van-cell-group>
+  <van-cell title="Cell title" value="Content" />
+  <van-cell title="Cell title" value="Content" label="Description" />
+</van-cell-group>
+```
+
+### Inset Grouped
+
+```html
+<van-cell-group inset>
+  <van-cell title="Cell title" value="Content" />
+  <van-cell title="Cell title" value="Content" label="Description" />
+</van-cell-group>
+```
+
+### Size
+
+```html
+<van-cell-group>
+  <van-cell title="Cell title" value="Content" size="large" />
+  <van-cell
+    title="Cell title"
+    value="Content"
+    size="large"
+    label="Description"
+  />
+</van-cell-group>
+```
+
+### Left Icon
+
+```html
+<van-cell-group>
+  <van-cell title="Cell title" icon="location-o" />
+</van-cell-group>
+```
+
+### Value only
+
+```html
+<van-cell-group>
+  <van-cell value="Content" />
+</van-cell-group>
+```
+
+### Link
+
+```html
+<van-cell-group>
+  <van-cell title="Cell title" is-link />
+  <van-cell title="Cell title" is-link value="Content" />
+  <van-cell title="Cell title" is-link arrow-direction="down" value="Content" />
+</van-cell-group>
+```
+
+### Router
+
+```html
+<van-cell-group>
+  <van-cell title="URL" is-link url="/vant/mobile.html" />
+  <van-cell title="Vue Router" is-link to="index" />
+</van-cell-group>
+```
+
+### Group Title
+
+```html
+<van-cell-group title="Group 1">
+  <van-cell title="Cell title" value="Content" />
+</van-cell-group>
+<van-cell-group title="Group 2">
+  <van-cell title="Cell title" value="Content" />
+</van-cell-group>
+```
+
+### Use Slots
+
+```html
+<van-cell value="Content" is-link>
+  <!-- Use the title slot to customize the title -->
+  <template #title>
+    <span class="custom-title">Title</span>
+    <van-tag type="danger">Tag</van-tag>
+  </template>
+</van-cell>
+
+<van-cell title="Title" icon="shop-o">
+  <!-- Use the right-icon slot to customize the right icon -->
+  <template #right-icon>
+    <van-icon name="search" class="search-icon" />
+  </template>
+</van-cell>
+
+<style>
+  .custom-title {
+    margin-right: 4px;
+    vertical-align: middle;
+  }
+
+  .search-icon {
+    font-size: 16px;
+    line-height: inherit;
+  }
+</style>
+```
+
+### Vertical Center
+
+```html
+<van-cell center title="Cell title" value="Content" label="Description" />
+```
+
+## API
+
+### CellGroup Props
+
+| Attribute | Description                  | Type      | Default |
+| --------- | ---------------------------- | --------- | ------- |
+| title     | Group title                  | _string_  | -       |
+| border    | Whether to show outer border | _boolean_ | `true`  |
+
+### Cell Props
+
+| Attribute            | Description                                             | Type               | Default    |
+| -------------------- | ------------------------------------------------------- | ------------------ | ---------- |
+| title                | Title                                                   | _number \| string_ | -          |
+| value                | Right text                                              | _number \| string_ | -          |
+| label                | Description below the title                             | _string_           | -          |
+| size                 | Size，can be set to `large`                             | _string_           | -          |
+| icon                 | Left Icon                                               | _string_           | -          |
+| icon-prefix `v2.5.3` | Icon className prefix                                   | _string_           | `van-icon` |
+| border               | Whether to show inner border                            | _boolean_          | `true`     |
+| center               | Whether to center content vertically                    | _boolean_          | `true`     |
+| url                  | Link URL                                                | _string_           | -          |
+| to                   | Target route of the link, same as to of vue-router      | _string \| object_ | -          |
+| replace              | If true, the navigation will not leave a history record | _boolean_          | `false`    |
+| clickable            | Whether to show click feedback when clicked             | _boolean_          | `null`     |
+| is-link              | Whether to show link icon                               | _boolean_          | `false`    |
+| required             | Whether to show required mark                           | _boolean_          | `false`    |
+| arrow-direction      | Can be set to `left` `up` `down`                        | _string_           | `right`    |
+| title-style          | Title style                                             | _any_              | -          |
+| title-class          | Title className                                         | _any_              | -          |
+| value-class          | Value className                                         | _any_              | -          |
+| label-class          | Label className                                         | _any_              | -          |
+
+### Cell Events
+
+| Event | Description                  | Arguments      |
+| ----- | ---------------------------- | -------------- |
+| click | Emitted when cell is clicked | _event: Event_ |
+
+### CellGroup Slots
+
+| Name    | Description  |
+| ------- | ------------ |
+| default | Default slot |
+| title   | Custom title |
+
+### Cell Slots
+
+| Name       | Description                       |
+| ---------- | --------------------------------- |
+| default    | Custom value                      |
+| icon       | Custom icon                       |
+| title      | Custom title                      |
+| label      | Custom label                      |
+| right-icon | Custom right icon                 |
+| extra      | Custom extra content on the right |
+
+### Less Variables
+
+How to use: [Custom Theme](#/en-US/theme).
+
+| Name                          | Default Value                         | Description |
+| ----------------------------- | ------------------------------------- | ----------- |
+| @cell-font-size               | `@font-size-md`                       | -           |
+| @cell-line-height             | `24px`                                | -           |
+| @cell-vertical-padding        | `10px`                                | -           |
+| @cell-horizontal-padding      | `@padding-md`                         | -           |
+| @cell-text-color              | `@text-color`                         | -           |
+| @cell-background-color        | `@white`                              | -           |
+| @cell-border-color            | `@border-color`                       | -           |
+| @cell-active-color            | `@active-color`                       | -           |
+| @cell-required-color          | `@red`                                | -           |
+| @cell-label-color             | `@gray-6`                             | -           |
+| @cell-label-font-size         | `@font-size-sm`                       | -           |
+| @cell-label-line-height       | `@line-height-sm`                     | -           |
+| @cell-label-margin-top        | `@padding-base`                       | -           |
+| @cell-value-color             | `@gray-6`                             | -           |
+| @cell-icon-size               | `16px`                                | -           |
+| @cell-right-icon-color        | `@gray-6`                             | -           |
+| @cell-large-vertical-padding  | `@padding-sm`                         | -           |
+| @cell-large-title-font-size   | `@font-size-lg`                       | -           |
+| @cell-large-label-font-size   | `@font-size-md`                       | -           |
+| @cell-group-background-color  | `@white`                              | -           |
+| @cell-group-title-color       | `@gray-6`                             | -           |
+| @cell-group-title-padding     | `@padding-md @padding-md @padding-xs` | -           |
+| @cell-group-title-font-size   | `@font-size-md`                       | -           |
+| @cell-group-title-line-height | `16px`                                | -           |
---
+++ src/cellson/index.js
@@ -0,0 +1,360 @@
+// Utils
+import { createNamespace, isDef, isDefB, isObject } from '../utils';
+import { emit } from '../utils/functional';
+import { routeProps, functionalRoute } from '../utils/router';
+import { cellProps } from './shared';
+import { ChildrenMixin } from '../mixins/relation';
+
+// Components
+import Iconv from '../iconv';
+
+
+import VanEmptyCol from '../emptycol/index';
+
+
+const [createComponent, bem] = createNamespace('cellson');
+
+
+export default createComponent({
+  mixins: [ChildrenMixin('vanDropdownMenuItem')],
+  inheritAttrs: false,
+  components: {
+    VanEmptyCol,
+    Iconv
+  },
+  props: {
+    ...cellProps,
+    infield: {
+      type: Boolean,
+      default: false,
+    },
+    novalue: {
+      type: Boolean,
+      default: false,
+    },
+    ...routeProps,
+    rtitle: {
+      type: String,
+      default: '',
+    },
+    notitle: {
+      type: Boolean,
+      default: false,
+    },
+    notitleblock: {
+      type: Boolean,
+      default: false,
+    },
+    singleslot: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+    };
+  },
+
+  watch: {
+  },
+  created() {
+  },
+  mounted() {
+  },
+
+  beforeDestroy() {
+  },
+
+  computed: {
+    routeMode() {
+      return !!this.parent?.parent?.route;
+    },
+    activeDropdownItem() {
+      const inVanDropdownItem = this.vanDropdownMenuItem;
+      if (!inVanDropdownItem) return false;
+      const routeMode = this.routeMode;
+      if (routeMode && '$route' in this) {
+        const { to, destination, $route } = this;
+        // const config = isObject(to) ? to : { path: to };
+        const config = isObject(destination) ? destination : { path: this.newdest(destination) };
+
+        return !!$route.matched.find((r) => {
+          // vue-router 3.x $route.matched[0].path is empty in / and its children paths
+          const path = r.path === '' ? '/' : r.path;
+          if (config.path === '/') return false;
+          const pathMatched = config.path === path;
+          const nameMatched = isDef(config.name) && config.name === r.name;
+          return pathMatched || nameMatched;
+        });
+      }
+
+      return false;
+    }
+  },
+
+  methods: {
+    newdest(destination) {
+      return destination ? '/' + destination.split('/').slice(2).join('/') : destination;
+    },
+  },
+
+  render() {
+    const { slots } = this;
+    // const slots = this.$slots;
+    const props = this._props;
+    const parent = this.$parent;
+    const that = this;
+    const { icon, size, title, label, value, isLink, infield, novalue, rtitle, notitle, notitleblock, singleslot } = this._props;
+    const showTitle = true || slots('title') || isDefB(title);
+    const ifDesigner = (parent.$env && parent.$env.VUE_APP_DESIGNER);
+    function Labelb() {
+      const showLabel = slots('lable') || isDef(label);
+
+      if (showLabel) {
+        return (
+          <div class={[bem('label'), props.labelClass]} vusion-slot-name="label" vusion-slot-name-edit="label">
+            {slots('lable') ? slots('lable') : label}
+          </div>
+        );
+      }
+    }
+
+    function Title() {
+      const ifDesigner = (parent.$env && parent.$env.VUE_APP_DESIGNER);
+      if (showTitle) {
+        if (!ifDesigner && (!isDef(title) || title === '') && !slots('title')) return null;
+        if (notitleblock && !title) return null;
+        if (notitle) {
+          if ((!isDef(title) || title === '') && !slots('title')) return null;
+          return (
+            <div class={[bem('title'), props.titleClass]} style={props.titleStyle} vusion-slot-name="title" vusion-slot-name-edit="title">
+              {slots('title') ? slots('title') : title}
+            </div>
+          );
+        }
+        if(ifDesigner && singleslot) return null;
+        return (
+          <div class={[bem('title'), props.titleClass]} style={props.titleStyle} vusion-scope-id={that.$vnode.context.$options._scopeId} vusion-slot-name="title" vusion-slot-name-edit="title">
+            {slots('title') ? slots('title') : title}
+            {(ifDesigner && (!isDef(title) || title === '') && !slots('title')) ? <van-empty-col></van-empty-col> : null}
+          </div>
+        );
+      }
+    }
+
+    function Value() {
+      const showValue = true || slots() || (isDef(rtitle) && value !== '');
+      //@ts-ignore
+      const ifDesigner = (parent.$env && parent.$env.VUE_APP_DESIGNER);
+      if (!ifDesigner && (!isDef(rtitle) || rtitle === '') && !slots()) return null;
+      if (novalue) return null;
+      if (ifDesigner) {
+        return (
+          <div class={[bem('value', { alone: singleslot }), props.valueClass]} vusion-slot-name="default" vusion-scope-id={that.$vnode.context.$options._scopeId}>
+            {slots() ? slots()  : (isDef(rtitle) && rtitle !== '' ? <span>{rtitle}</span>  : null)}
+            {((!isDef(rtitle) || rtitle === '') && !slots()) ? <van-empty-col></van-empty-col> : null}
+          </div>
+        );
+      } else {
+        if (showValue) {
+          return (
+            <div class={[bem('value', { alone: singleslot }), props.valueClass]}>
+              {slots() ? slots() : <span>{rtitle}</span>}
+            </div>
+          );
+        }
+      }
+    }
+
+    function LeftIcon() {
+      if (slots('icon')) {
+        return slots('icon');
+      }
+
+      if (icon) {
+        return (
+          <van-iconv
+            class={bem('left-icon')}
+            name={icon}
+            classPrefix={props.iconPrefix}
+            notext
+          />
+        );
+      }
+    }
+
+    function RightIcon() {
+      const rightIconSlot = slots('right-icon');
+      if (rightIconSlot) {
+        return rightIconSlot;
+      }
+
+      if (isLink) {
+        const { arrowDirection } = props;
+
+        return (
+          <van-icon
+            class={bem('right-icon')}
+            name={(arrowDirection && arrowDirection !== 'right') ? `arrow-${arrowDirection}` : 'arrow'}
+          />
+        );
+      }
+    }
+
+    function currentHref() {
+      if (props.href !== undefined)
+          return props.href;
+      if (props.destination !== undefined && props.destination !== "")
+          return props.destination;
+      else if (parent?.$router && props.to !== undefined)
+          return parent?.$router.resolve(props.to, parent?.$route, props.append).href;
+      else
+          return undefined;
+    }
+
+    function onClick(event) {
+      if (that.vanDropdownMenuItem) {
+        that.vanDropdownMenuItem.showPopup = false;
+        if ((that.value ?? that.index) !== that.vanDropdownMenuItem.value) {
+          that.vanDropdownMenuItem.value = that.value ?? that.index;
+          that.vanDropdownMenuItem.$emit('input', that.value);
+          that.vanDropdownMenuItem.$emit('update:valueprop', that.value);
+        }
+      }
+
+
+      // emit(that, 'click', event);
+      const hrefR = currentHref();
+      if (!hrefR && !that.$listeners.click) {
+        // event.preventDefault();
+        return
+      }
+      that.$listeners?.click?.(event);
+      // @ts-ignore：没办法
+      // if (props.target !== '_self')
+      //   return;
+
+      if (hrefR === undefined) {
+        let to;
+        if (props.destination) {
+            // 只处理/a/b形式的链接
+            const origin = window.location.origin;
+            const path = window.location.href.replace(origin, '').split('/');
+            const destination = props.destination.replace(origin, '').split('/');
+            if (path[1] === destination[1]) {
+                to = '/' + destination.slice(2).join('/');
+            } else {
+                return;
+            }
+        }
+
+
+        const currentTo = to || props.to;
+        if (currentTo === undefined)
+            return;
+        let cancel = false;
+        emit(that, 'before-navigate',  {
+          to: currentTo,
+          replace: props.replace,
+          append: props.append,
+          preventDefault: () => (cancel = true),
+        });
+        if (cancel)
+            return;
+        const $router = parent?.$router;
+        const $route = parent?.$route;
+        const { location } = $router.resolve(
+            currentTo,
+            $route,
+            props.append,
+        );
+        props.replace ? $router.replace(location) : $router.push(location);
+
+        emit(that, 'navigate',  { to: currentTo, replace: props.replace, append: props.append });
+      } else {
+        function downloadClick() {
+          const a = document.createElement("a");
+          a.setAttribute("href", hrefR);
+          document.body.appendChild(a);
+          a.click();
+          setTimeout(() => {
+            document.body.removeChild(a);
+          }, 500);
+        }
+        downloadClick();
+      }
+
+        // functionalRoute(ctx);
+    }
+
+    const clickable = props.clickable ?? isLink;
+    const classes = {
+      clickable,
+      center: props.center,
+      required: props.required,
+      borderless: !props.border,
+    };
+
+    if (size) {
+      classes[size] = size;
+    }
+    const nocontentforlink = !ifDesigner && ((!isDef(rtitle) || rtitle === '') && !slots()) && ((!isDef(title) || title === '') && !slots('title'));
+
+    if (nocontentforlink) {
+      classes['nocontentforlink'] = nocontentforlink;
+    }
+
+    let classesnew = bem(classes);
+    const inVanDropdownItem = that.vanDropdownMenuItem;
+    if (inVanDropdownItem) {
+      if (that.routeMode) {
+        if (that.activeDropdownItem) {
+          classesnew += ' ' + that.vanDropdownMenuItem.bem('option', { active: true });
+        } else {
+          classesnew += ' ' + that.vanDropdownMenuItem.bem('option', { active: false });
+        }
+      } else if (isDef(that.vanDropdownMenuItem.value)) {
+        if (that.vanDropdownMenuItem.value === (that.value ?? that.index)) {
+          classesnew += ' ' + that.vanDropdownMenuItem.bem('option', { active: true });
+        } else {
+          classesnew += ' ' + that.vanDropdownMenuItem.bem('option', { active: false });
+        }
+      }
+    }
+
+    const ado = {
+      ...this.$attrs,
+      [infield ? 'is-sub': 'noallow']: ''
+    }
+
+    const canActivateItem = () => {
+      if (that.routeMode) {
+        // console.log(that.activeDropdownItem, 99999);
+        return that.activeDropdownItem;
+      }
+      return (inVanDropdownItem && (that.vanDropdownMenuItem?.value === (that.value ?? that.index)));
+    }
+
+    return (
+      <div
+        {...{attrs: {...ado}}}
+        class={classesnew}
+        role={clickable ? 'button' : null}
+        tabindex={clickable ? 0 : null}
+        onClick={onClick}
+        vusion-cut={that.vusionCut}
+        vusion-move={that.vusionMove}
+        vusion-node-path={that.vusionNodePath}
+        vusion-node-tag={that.vusionNodeTag}
+      >
+        {LeftIcon()}
+        {Title()}
+        {Value()}
+        {RightIcon()}
+        {slots('extra')}
+        {canActivateItem() ? <Iconv class={that.vanDropdownMenuItem.bem('icon')} color={that.vanDropdownMenuItem.parent.activeColor} name="success" /> : null}
+      </div>
+    );
+  },
+});
