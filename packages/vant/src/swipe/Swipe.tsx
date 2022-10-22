859e5ee32cddd7876e50084614eb24085e86a982
neverland <jait.chen@foxmail.com>
2022-10-22 22:58:42 +0800

    fix(Swipe): failed to render when inside popup (#11162)
    * fix(Swipe): failed to render when inside popup
    
    * chore: upd

--- packages/vant/src/swipe/Swipe.tsx
+++ packages/vant/src/swipe/Swipe.tsx
@@ -101,7 +101,9 @@ export default defineComponent({
     });
 
     const maxCount = computed(() =>
-      Math.ceil(Math.abs(minOffset.value) / size.value)
+      size.value
+        ? Math.ceil(Math.abs(minOffset.value) / size.value)
+        : count.value
     );
 
     const trackSize = computed(() => count.value * size.value);
