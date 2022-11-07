e1aa63720f446b0a87c3763840d75f216008d582
若川 <lxchuan12@163.com>
2022-11-07 22:44:34 +0800

    fix(lazyload): lazy-image h is not a function
    in vue 3.x h is not function. this.$slots.default is  a function.
    
    [vue 3.x refs render-function-api-change](https://github.com/vuejs/rfcs/blob/master/active-rfcs/0008-render-function-api-change.md)
    [vue 3.x 文档 h 渲染函数](https://cn.vuejs.org/api/render-function.html#h)

--- packages/vant/src/lazyload/vue-lazyload/lazy-image.js
+++ packages/vant/src/lazyload/vue-lazyload/lazy-image.js
@@ -6,6 +6,7 @@
 import { useRect } from '@vant/use';
 import { loadImageAsync } from './util';
 import { noop } from '../../utils';
+import { h } from 'vue';
 
 export default (lazyManager) => ({
   props: {
@@ -15,15 +16,13 @@ export default (lazyManager) => ({
       default: 'img',
     },
   },
-  render(h) {
+  render() {
     return h(
       this.tag,
       {
-        attrs: {
-          src: this.renderSrc,
-        },
+        src: this.renderSrc,
       },
-      this.$slots.default
+      this.$slots.default?.()
     );
   },
   data() {
