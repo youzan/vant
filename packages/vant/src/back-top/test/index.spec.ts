ef2e5d250aa26c5e5e2d284f9e438f4f15d2b99d
wangjunwei <gavinwjw@163.com>
2022-11-13 17:03:01 +0800

    perf: improve type

--- packages/vant/src/back-top/test/index.spec.ts
+++ packages/vant/src/back-top/test/index.spec.ts
@@ -8,9 +8,9 @@ test('test position prop', async () => {
       bottom: 100,
     },
   });
-  const backTopEl = document.querySelector<HTMLElement>('.van-back-top');
-  expect(backTopEl?.style.right).toBe('30px');
-  expect(backTopEl?.style.bottom).toBe('100px');
+  const backTopEl = document.querySelector('.van-back-top') as HTMLDivElement;
+  expect(backTopEl.style.right).toBe('30px');
+  expect(backTopEl.style.bottom).toBe('100px');
 });
 
 test('test backTop event', async () => {
