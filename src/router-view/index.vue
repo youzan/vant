848f9c3d03f80fa37d97344c9234a7d10157448b
Bener <benjavan@163.com>
2022-11-14 18:23:03 +0800

    feat: van-router-view style

--- src/router-view/index.vue
+++ src/router-view/index.vue
@@ -1,23 +1,35 @@
 <template>
-<div :class="$style.fake" v-if="$env.VUE_APP_DESIGNER" router-view-symbol>
+  <div :class="$style.fake" v-if="$env.VUE_APP_DESIGNER" router-view-symbol>
     <router-view></router-view>
-</div>
-<router-view v-else></router-view>
+  </div>
+  <router-view v-else></router-view>
 </template>
 
 <script>
 export default {
-    name: 'van-router-view',
+  name: 'VanRouterView',
 };
 </script>
 
 <style module>
 .fake:empty {
-    background: linear-gradient(-45deg, white 25%,
-    rgba(138, 205, 78, 0.25) 25%, rgba(138, 205, 78, 0.25) 50%,
-    white 50%, white 75%,
-    rgba(138, 205, 78, 0.25) 75%);
-    background-size: 20px 20px;
-    min-height: 20px;
+  background: linear-gradient(
+    -45deg,
+    white 25%,
+    rgba(129, 153, 202, 0.102) 25%,
+    rgba(129, 153, 202, 0.102) 50%,
+    white 50%,
+    white 75%,
+    rgba(129, 153, 202, 0.102) 75%
+  );
+  background-size: 45px 45px;
+  min-height: 32px;
+  text-align: center;
+}
+.fake:empty::after {
+  content: '此容器为子页面呈现占位，可在子页面编辑内容';
+  font-size: 12px;
+  line-height: 32px;
+  color: var(--font-second-color);
 }
 </style>
