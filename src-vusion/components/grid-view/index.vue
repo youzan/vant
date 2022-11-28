fb7bb335e2a16cc27417c7b2b84157b97ae0b3fb
qingniao99 <huangjianqiang126@163.com>
2022-11-28 11:07:05 +0800

    fix: 去掉cloudui变量

--- src-vusion/components/grid-view/index.vue
+++ src-vusion/components/grid-view/index.vue
@@ -651,40 +651,39 @@ export default {
 .root {
   /* display: flex; */
   flex-direction: column;
-  background: var(--list-view-body-background);
+  background: #fff;
   /* border: 1px solid var(--list-view-border-color); */
-  border-radius: var(--border-radius-base);
-  height: var(--list-view-height);
-  min-width: var(--list-view-width);
+  border-radius: 4px;
+  height: 400px;
+  min-width: 280px;
   max-width: 100%;
 
   box-sizing: border-box;
 }
 
 .root:focus {
-  border-color: var(--list-view-border-color-focus);
-  outline: var(--focus-outline);
+  border-color: #e5e5e5;
+  outline: none;
 }
 
 .status {
-  color: var(--list-view-status-color);
+  color: #999999;
   text-align: center;
-  padding: var(--list-view-item-padding);
+  padding: 5px 12px;
 }
 
 .root[disabled] {
-  border: 1px solid var(--list-view-border-color-disabled);
+  border: 1px solid #ebebeb;
 }
 
 .head {
-  background: var(--list-view-head-background);
-  padding: var(--list-view-head-padding);
-  border-bottom: 1px solid var(--list-view-border-color);
+ padding: 8px 12px;
+  border-bottom: 1px solid e5e5e5;
 }
 
 .extra {
   float: right;
-  color: var(--color-light);
+  color: #999;
 }
 
 .body {
@@ -748,18 +747,18 @@ export default {
 }
 
 .root[disabled] .body {
-  background: var(--list-view-body-background-disabled);
+  background: #fff;
 }
 
 .foot {
-  background: var(--list-view-foot-background);
+  /* background: var(--list-view-foot-background);
   padding: var(--list-view-foot-padding);
-  border-top: 1px solid var(--list-view-border-color);
+  border-top: 1px solid var(--list-view-border-color); */
 }
 
 .filter[class][class] {
-  margin: var(--list-view-filter-margin);
-  width: calc(100% - var(--list-view-filter-margin) * 2);
+  margin: 12px;
+    width: calc(100% - 24px);
 }
 
 .pagination {
@@ -767,7 +766,7 @@ export default {
   margin: 0 -12px;
 }
 
-.root[size^='normal'] {
+/* .root[size^='normal'] {
   height: var(--list-view-height);
 }
 .root[size^='large'] {
@@ -796,7 +795,7 @@ export default {
 }
 .root[size$='auto'] {
   width: auto;
-}
+} */
 
 /* .root {
     height: 100vh;
