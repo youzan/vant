8b5d32106840d79a3d0f876de022a8c7e2a30e86
空蝉 <GRAMMAC@163.com>
2023-01-09 16:01:27 +0800

    fix(Skeleton): can not register skeleton image component by default #11469

--- packages/vant/src/skeleton/index.ts
+++ packages/vant/src/skeleton/index.ts
@@ -1,17 +1,37 @@
+import type { App } from 'vue';
+
 import _Skeleton from './Skeleton';
 import _SkeletonImage from './SkeletonImage';
 import _SkeletonTitle from './SkeletonTitle';
 import _SkeletonAvatar from './SkeletonAvatar';
 import _SkeletonParagraph from './SkeletonParagraph';
 
-import { withInstall } from '../utils';
+import { withInstall, camelize } from '../utils';
 
 export const SkeletonImage = withInstall(_SkeletonImage);
 export const SkeletonTitle = withInstall(_SkeletonTitle);
 export const SkeletonAvatar = withInstall(_SkeletonAvatar);
 export const SkeletonParagraph = withInstall(_SkeletonParagraph);
-export const Skeleton = withInstall(_Skeleton);
 
+// fix https://github.com/youzan/vant/issues/11469
+// register all Skeleton components
+_Skeleton.install = (app: App) => {
+  [
+    _Skeleton,
+    _SkeletonImage,
+    _SkeletonTitle,
+    _SkeletonAvatar,
+    _SkeletonParagraph,
+  ].forEach((component) => {
+    const { name } = component;
+    if (name) {
+      app.component(name, component);
+      app.component(camelize(`-${name}`), component);
+    }
+  });
+};
+
+export const Skeleton = _Skeleton;
 export default Skeleton;
 
 // Skeleton
