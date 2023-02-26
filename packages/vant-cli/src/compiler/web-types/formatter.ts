5954997314c0255cb3080227361f65b49efe4c20
neverland <jait.chen@foxmail.com>
2023-02-26 10:07:31 +0800

    fix(cli): failed to match arguments in some cases (#11618)
    * fix(cli): failed to match arguments in some cases
    
    * chore: upd

--- packages/vant-cli/src/compiler/web-types/formatter.ts
+++ packages/vant-cli/src/compiler/web-types/formatter.ts
@@ -29,9 +29,15 @@ function formatArguments(input: string): VueEventArgument[] {
     } else if ([':', ',', '_', ' '].includes(input[0])) {
       input = input.substring(1);
     } else {
-      const val = input.match(/( |'|\||\w)+/)![0] || '';
-      input = input.substring(val.length);
-      items.push(val);
+      const matched = input.match(/( |'|\||\w)+/);
+
+      if (matched?.length && matched[0]) {
+        const val = matched[0];
+        input = input.substring(val.length);
+        items.push(val);
+      } else {
+        input = '';
+      }
     }
   }
 
