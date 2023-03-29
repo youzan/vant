e55780b3a024870f0deb8baece711e81d6021e26
wangjunwei <gavinwjw@163.com>
2023-03-29 14:27:39 +0800

    fix(ActionBar): set correct height when toggling diaplay

--- packages/vant/src/composables/use-placeholder.tsx
+++ packages/vant/src/composables/use-placeholder.tsx
@@ -1,12 +1,20 @@
-import { Ref } from 'vue';
-import { useHeight } from './use-height';
+import { type Ref, ref } from 'vue';
 import type { BEM } from '../utils/create';
+import { useRect } from '@vant/use';
+import { useHeight } from './use-height';
+import { useVisibilityChange } from './use-visibility-change';
 
 export function usePlaceholder(contentRef: Ref<Element | undefined>, bem: BEM) {
+  const root = ref<HTMLElement>();
   const height = useHeight(contentRef, true);
+
+  useVisibilityChange(root, () => {
+    height.value = useRect(contentRef).height;
+  });
 
   return (renderContent: () => JSX.Element) => (
     <div
+      ref={root}
       class={bem('placeholder')}
       style={{ height: height.value ? `${height.value}px` : undefined }}
     >
