eea41f3fbc4dcb32afb9594d3b440842dc16f4a4
chenjiahan <jait.chen@foxmail.com>
2023-05-01 16:34:57 +0800

    fix(Signature): failed to sign in mobile devices

--- packages/vant/src/signature/Signature.tsx
+++ packages/vant/src/signature/Signature.tsx
@@ -12,6 +12,7 @@ import {
   createNamespace,
   preventDefault,
 } from '../utils';
+import { useRect } from '@vant/use';
 import { Button } from '../button';
 
 const [name, bem, t] = createNamespace('signature');
@@ -45,42 +46,10 @@ export default defineComponent({
       width: 0,
       height: 0,
       ctx: null as CanvasRenderingContext2D | null | undefined,
-      isSupportTouch: 'ontouchstart' in window,
     });
 
+    let canvasRect: DOMRect;
     const isRenderCanvas = inBrowser ? hasCanvasSupport() : true;
-
-    const touchMove = (event: TouchEvent) => {
-      if (!state.ctx) {
-        return false;
-      }
-
-      preventDefault(event);
-
-      const evt = event.changedTouches
-        ? event.changedTouches[0]
-        : event.targetTouches[0];
-
-      emit('signing', evt);
-      let mouseX = evt.clientX;
-      let mouseY = evt.clientY;
-
-      if (!state.isSupportTouch) {
-        const coverPos = canvasRef.value?.getBoundingClientRect();
-        mouseX = evt.clientX - (coverPos?.left || 0);
-        mouseY = evt.clientY - (coverPos?.top || 0);
-      }
-
-      state.ctx.lineCap = 'round';
-      state.ctx.lineJoin = 'round';
-      state.ctx?.lineTo(mouseX, mouseY);
-      state.ctx?.stroke();
-    };
-
-    const touchEnd = (event: TouchEvent) => {
-      preventDefault(event);
-      emit('end');
-    };
 
     const touchStart = () => {
       if (!state.ctx) {
@@ -90,7 +59,33 @@ export default defineComponent({
       state.ctx.beginPath();
       state.ctx.lineWidth = props.lineWidth;
       state.ctx.strokeStyle = props.penColor;
+      canvasRect = useRect(canvasRef);
+
       emit('start');
+    };
+
+    const touchMove = (event: TouchEvent) => {
+      if (!state.ctx) {
+        return false;
+      }
+
+      preventDefault(event);
+
+      const touch = event.touches[0];
+      const mouseX = touch.clientX - (canvasRect?.left || 0);
+      const mouseY = touch.clientY - (canvasRect?.top || 0);
+
+      state.ctx.lineCap = 'round';
+      state.ctx.lineJoin = 'round';
+      state.ctx?.lineTo(mouseX, mouseY);
+      state.ctx?.stroke();
+
+      emit('signing', event);
+    };
+
+    const touchEnd = (event: TouchEvent) => {
+      preventDefault(event);
+      emit('end');
     };
 
     const isCanvasEmpty = (canvas: HTMLCanvasElement) => {
