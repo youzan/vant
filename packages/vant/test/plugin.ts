53fc8a7d6c55660e50e5801d3c75a3d01d42a6f3
chenjiahan <jait.chen@foxmail.com>
2023-05-02 11:12:35 +0800

    chore: add some SSR test cases

--- packages/vant/test/plugin.ts
+++ packages/vant/test/plugin.ts
@@ -1,23 +1,19 @@
 /* eslint-disable max-classes-per-file */
-import { ComponentPublicInstance } from 'vue';
-import { config, VueWrapper, DOMWrapper } from '@vue/test-utils';
+import { config } from '@vue/test-utils';
 
 declare module '@vue/test-utils' {
   // eslint-disable-next-line
-  export class DOMWrapper<ElementType extends Element> {
+  export class DOMWrapper<NodeType extends Node> {
     style: CSSStyleDeclaration;
   }
 
-  // eslint-disable-next-line
-  class VueWrapper<T extends ComponentPublicInstance> {
+  export class VueWrapper {
     style: CSSStyleDeclaration;
   }
 }
 
-const stylePlugin = (
-  wrapper: VueWrapper<ComponentPublicInstance> | DOMWrapper<Element>
-) => ({
-  style: (wrapper.element as HTMLElement).style,
+const stylePlugin = (wrapper: any) => ({
+  style: wrapper.element.style,
 });
 
 config.plugins.DOMWrapper.install(stylePlugin);
