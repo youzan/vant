759a16b3be504d1d40f24f78d4e68eee09f3cf84
inottn <inottn@outlook.com>
2023-05-05 17:59:48 +0800

    fix(Tab): consider the case where multiple components exist at the same time

--- packages/vant/src/tabs/utils.ts
+++ packages/vant/src/tabs/utils.ts
@@ -1,29 +1,31 @@
 import { raf, cancelRaf } from '@vant/use';
 import { ScrollElement, getScrollTop, setScrollTop } from '../utils';
 
-let scrollLeftToRafId: number;
-let scrollTopToRafId: number;
-
 export function scrollLeftTo(
   scroller: HTMLElement,
   to: number,
   duration: number
 ) {
-  cancelRaf(scrollLeftToRafId);
-
+  let rafId: number;
   let count = 0;
   const from = scroller.scrollLeft;
   const frames = duration === 0 ? 1 : Math.round((duration * 1000) / 16);
+
+  function cancel() {
+    cancelRaf(rafId);
+  }
 
   function animate() {
     scroller.scrollLeft += (to - from) / frames;
 
     if (++count < frames) {
-      scrollLeftToRafId = raf(animate);
+      rafId = raf(animate);
     }
   }
 
   animate();
+
+  return cancel;
 }
 
 export function scrollTopTo(
@@ -32,12 +34,15 @@ export function scrollTopTo(
   duration: number,
   callback: () => void
 ) {
-  cancelRaf(scrollTopToRafId);
-
+  let rafId: number;
   let current = getScrollTop(scroller);
   const isDown = current < to;
   const frames = duration === 0 ? 1 : Math.round((duration * 1000) / 16);
   const step = (to - current) / frames;
+
+  function cancel() {
+    cancelRaf(rafId);
+  }
 
   function animate() {
     current += step;
@@ -49,11 +54,13 @@ export function scrollTopTo(
     setScrollTop(scroller, current);
 
     if ((isDown && current < to) || (!isDown && current > to)) {
-      scrollTopToRafId = raf(animate);
+      rafId = raf(animate);
     } else if (callback) {
-      scrollTopToRafId = raf(callback as FrameRequestCallback);
+      rafId = raf(callback as FrameRequestCallback);
     }
   }
 
   animate();
+
+  return cancel;
 }
