ba6a0e93eba9ac106d2bfda3e4d21f3fcc7d8f65
inottn <inottn@outlook.com>
2023-06-18 09:49:35 +0800

    feat(Rate): add clearable prop (#11969)
    * feat(Rate): add clearable prop
    
    * test(Rate): update test snapshots
    
    * fix(Rate): solve problem
    
    * feat: extract constant
    
    * test: update test case

--- packages/vant/src/rate/demo/index.vue
+++ packages/vant/src/rate/demo/index.vue
@@ -11,6 +11,7 @@ const t = useTranslate({
     customIcon: '自定义图标',
     customStyle: '自定义样式',
     customCount: '自定义数量',
+    clearable: '可清空',
     readonly: '只读状态',
     readonlyHalfStar: '只读状态小数显示',
     changeEvent: '监听 change 事件',
@@ -22,6 +23,7 @@ const t = useTranslate({
     customIcon: 'Custom Icon',
     customStyle: 'Custom Style',
     customCount: 'Custom Count',
+    clearable: 'Clearable',
     readonly: 'Readonly',
     readonlyHalfStar: 'Readonly Half Star',
     changeEvent: 'Change Event',
@@ -37,6 +39,7 @@ const value5 = ref(4);
 const value6 = ref(3);
 const value7 = ref(3.3);
 const value8 = ref(2);
+const value9 = ref(3);
 
 const onChange = (value: number) => showToast(t('toastContent', value));
 </script>
@@ -66,6 +69,10 @@ const onChange = (value: number) => showToast(t('toastContent', value));
 
   <demo-block :title="t('customCount')">
     <van-rate v-model="value5" :count="6" />
+  </demo-block>
+
+  <demo-block :title="t('clearable')">
+    <van-rate v-model="value9" clearable />
   </demo-block>
 
   <demo-block :title="t('disabled')">
--- packages/vant/src/rate/test/index.spec.ts
+++ packages/vant/src/rate/test/index.spec.ts
@@ -1,5 +1,6 @@
 import { Rate } from '..';
-import { mount, triggerDrag } from '../../../test';
+import { mount, trigger, triggerDrag } from '../../../test';
+import { TAP_OFFSET } from '../../utils';
 import type { DOMWrapper } from '@vue/test-utils';
 
 function mockGetBoundingClientRect(items: DOMWrapper<Element>[]) {
@@ -140,4 +141,28 @@ test('should render correct count when using string prop', () => {
   const icons = wrapper.findAll('.van-rate__item');
 
   expect(icons).toHaveLength(4);
+});
+
+test('should reset value to 0 when using clearable prop', () => {
+  const wrapper = mount(Rate, {
+    props: {
+      modelValue: 4,
+      clearable: true,
+    },
+  });
+  const icons = wrapper.findAll('.van-rate__item');
+  const item4 = wrapper.findAll('.van-rate__icon')[3];
+  mockGetBoundingClientRect(icons);
+
+  trigger(wrapper, 'touchstart', 80, 0);
+  trigger(wrapper, 'touchmove', 80 + TAP_OFFSET, 0);
+  item4.trigger('click');
+  expect(wrapper.emitted('change')![0]).toEqual([0]);
+  expect(wrapper.emitted('update:modelValue')![0]).toEqual([0]);
+
+  trigger(wrapper, 'touchstart', 80, 0);
+  trigger(wrapper, 'touchmove', 80 + TAP_OFFSET + 1, 0);
+  item4.trigger('click');
+  expect(wrapper.emitted('change')).toHaveLength(1);
+  expect(wrapper.emitted('update:modelValue')).toHaveLength(1);
 });
