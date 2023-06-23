14d1211b32d7171e816e6cc3afea46cd1311d8f4
zhousg <zhousg1007@qq.com>
2023-06-23 21:11:57 +0800

    feat(FloatingPanel): add theme vars type

--- packages/vant/src/floating-panel/index.ts
+++ packages/vant/src/floating-panel/index.ts
@@ -5,6 +5,7 @@ export const FloatingPanel = withInstall(_FloatingPanel);
 export default FloatingPanel;
 export { floatingPanelProps } from './FloatingPanel';
 export type { FloatingPanelProps } from './FloatingPanel';
+export type { FloatingPanelThemeVars } from './types';
 
 declare module 'vue' {
   export interface GlobalComponents {
---
+++ packages/vant/src/floating-panel/types.ts
@@ -0,0 +1,9 @@
+export type FloatingPanelThemeVars = {
+  FloatingPanelBorderRadius: string;
+  FloatingPanelHeaderHeight: string;
+  FloatingPanelZIndex: number | string;
+  FloatingPanelBackground: string;
+  FloatingPanelBarWidth: string;
+  FloatingPanelBarHeight: string;
+  FloatingPanelBarColor: string;
+};
