01d70f1d0f0d522848114ac4f77b3e020e7d310d
chenjiahan <jait.chen@foxmail.com>
2023-07-02 12:09:50 +0800

    chore(eslint-config): remove eslint-plugin-import

--- packages/vant-eslint-config/index.js
+++ packages/vant-eslint-config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: [
+    'eslint:recommended',
     'plugin:vue/vue3-recommended',
-    'airbnb-base',
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
@@ -23,28 +23,7 @@ module.exports = {
   },
 
   rules: {
-    'no-new': 'off',
-    'no-shadow': 'off',
-    'no-bitwise': 'off',
-    'func-names': 'off',
-    'no-console': 'off',
-    'no-plusplus': 'off',
-    'default-case': 'off',
-    'prefer-template': 'off',
-    'consistent-return': 'off',
-    'no-param-reassign': 'off',
-    'no-nested-ternary': 'off',
-    'no-underscore-dangle': 'off',
-    'no-unused-expressions': 'off',
-    'no-restricted-globals': 'off',
-    'class-methods-use-this': 'off',
     'prefer-destructuring': ['error', { object: true, array: false }],
-    // eslint-plugin-import
-    'import/order': 'off',
-    'import/extensions': 'off',
-    'import/no-unresolved': 'off',
-    'import/prefer-default-export': 'off',
-    'import/no-extraneous-dependencies': 'off',
     // eslint-plugin-vue
     'vue/no-v-html': 'off',
     'vue/attributes-order': 'off',
