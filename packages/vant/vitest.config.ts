c23a3481024926905715fcc79779e78046e94cb5
chenjiahan <jait.chen@foxmail.com>
2023-08-20 16:17:11 +0800

    chore: try experimentalVmThreads in CI

--- packages/vant/vitest.config.ts
+++ packages/vant/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 import vitePluginVue from '@vitejs/plugin-vue';
 import vitePluginJsx from '@vitejs/plugin-vue-jsx';
 
-const isCI = Boolean(process.env.GITHUB_ACTIONS);
+// const isCI = Boolean(process.env.GITHUB_ACTIONS);
 
 export default defineConfig({
   test: {
@@ -24,7 +24,7 @@ export default defineConfig({
     // enable single thread in CI because it is faster
     // singleThread: isCI,
     // disable experimentalVmThreads on CI because it causes OOM
-    experimentalVmThreads: !isCI,
+    experimentalVmThreads: true,
   },
   plugins: [vitePluginVue(), vitePluginJsx()],
 });
