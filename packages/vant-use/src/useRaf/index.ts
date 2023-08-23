0db9269b478a939a6299c69ad818d1a30ec05f46
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-08-23 22:44:48 +0800

    chore: update

--- packages/vant-use/src/useRaf/index.ts
+++ packages/vant-use/src/useRaf/index.ts
@@ -1,19 +1,25 @@
 import { inBrowser } from '..';
+
 export function useRaf(fn: FrameRequestCallback, interval = 0): () => void {
   if (inBrowser) {
     let start: number;
-    const id = requestAnimationFrame(function wrappedFn(
-      timestamp = Date.now(),
-    ) {
+    const disposesId: number[] = [];
+
+    const myFrame = (timestamp: number) => {
       if (start === undefined) {
         start = timestamp;
-      } else if (timestamp - start === interval) {
+      } else if (timestamp - start > interval) {
         fn(timestamp);
         start = timestamp;
       }
-      requestAnimationFrame(wrappedFn);
-    });
-    return () => cancelAnimationFrame(id);
+      disposesId.push(requestAnimationFrame(myFrame));
+    };
+    disposesId.push(requestAnimationFrame(myFrame));
+
+    return () => {
+      disposesId.forEach((id) => cancelAnimationFrame(id));
+      disposesId.length = 0;
+    };
   }
   return () => {};
 }
