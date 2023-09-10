1eab3cf468a35c6c1dd714d20800d1737ef1d814
neverland <jait.chen@foxmail.com>
2023-09-10 09:35:08 +0800

    fix(ConfigProvider): failed to set some basic theme vars (#12272)
    * fix(ConfigProvider): failed to set some basic theme vars
    
    * chore: add comments

--- packages/vant/src/config-provider/ConfigProvider.tsx
+++ packages/vant/src/config-provider/ConfigProvider.tsx
@@ -50,10 +50,16 @@ export const configProviderProps = {
 
 export type ConfigProviderProps = ExtractPropTypes<typeof configProviderProps>;
 
+/** map `gray1` to `gray-1` */
+function insertDash(str: string) {
+  return str.replace(/([a-zA-Z])(\d)/g, '$1-$2');
+}
+
 function mapThemeVarsToCSSVars(themeVars: Record<string, Numeric>) {
   const cssVars: Record<string, Numeric> = {};
   Object.keys(themeVars).forEach((key) => {
-    cssVars[`--van-${kebabCase(key)}`] = themeVars[key];
+    const formattedKey = insertDash(kebabCase(key));
+    cssVars[`--van-${formattedKey}`] = themeVars[key];
   });
   return cssVars;
 }
