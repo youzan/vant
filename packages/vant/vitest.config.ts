a3180c3aafd3cc6166683f1f65480f2434e76a32
inottn <inottn@outlook.com>
2023-12-24 20:33:56 +0800

    chore: update vitest config (#12530)

--- packages/vant/vitest.config.ts
+++ packages/vant/vitest.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vitest/config';
 import vitePluginVue from '@vitejs/plugin-vue';
 import vitePluginJsx from '@vitejs/plugin-vue-jsx';
-import { cpus } from 'os';
+import { cpus, totalmem } from 'os';
 
 const cpuNum = Math.max(cpus().length - 1, 1);
+const memory = totalmem();
 
 export default defineConfig({
   test: {
@@ -20,12 +21,19 @@ export default defineConfig({
       reporter: ['lcov', 'text-summary'],
       reportsDirectory: './test/coverage',
     },
+    pool: 'vmThreads',
+    poolOptions: {
+      vmThreads: {
+        // limit the memory to avoid OOM
+        memoryLimit:
+          typeof memory === 'number'
+            ? memory * Math.min(1 / (cpuNum * 2), 0.1)
+            : undefined,
+      },
+    },
     environment: 'jsdom',
     include: ['src/**/*.spec.[jt]s?(x)'],
     restoreMocks: true,
-    experimentalVmThreads: true,
-    // limit the memory to avoid OOM
-    experimentalVmWorkerMemoryLimit: Math.min(1 / (cpuNum * 2), 0.1),
   },
   plugins: [vitePluginVue(), vitePluginJsx()],
 });
