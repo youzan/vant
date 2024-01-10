dbf8027c2d9b538694ffd5889ba9f4010d93fb6a
Jungzl <13jungzl@gmail.com>
2024-01-10 10:20:28 +0800

    fix(Image): ImagePosition type is not exported before (#12549)

--- packages/vant/src/image/types.ts
+++ packages/vant/src/image/types.ts
@@ -1,3 +1,9 @@
+import type { CSSProperties } from 'vue';
+
+export type ImageFit = CSSProperties['objectFit'];
+
+export type ImagePosition = CSSProperties['objectPosition'];
+
 export type ImageThemeVars = {
   imagePlaceholderTextColor?: string;
   imagePlaceholderFontSize?: string;
