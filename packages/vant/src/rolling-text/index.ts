8d4b48a3ef0ee359efc9212bd5a59007bbf11d1b
nemo shen <nemoshen94@gmail.com>
2024-01-25 23:45:12 +0800

    chore: remove unuse code

--- packages/vant/src/rolling-text/index.ts
+++ packages/vant/src/rolling-text/index.ts
@@ -1,9 +1,7 @@
 import { withInstall } from '../utils';
 import _RollingText from './RollingText';
-import _RollingTextItem from './RollingTextItem';
 
 export const RollingText = withInstall(_RollingText);
-export const RollingTextItem = withInstall(_RollingTextItem);
 export default RollingText;
 export { rollingTextProps } from './RollingText';
 export type { RollingTextProps } from './RollingText';
@@ -17,6 +15,5 @@ export type {
 declare module 'vue' {
   export interface GlobalComponents {
     VanRollingText: typeof _RollingText;
-    VanRollingTextItem: typeof _RollingTextItem;
   }
 }
