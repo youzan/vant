2325599536fe0535b560d34f1e2baae5e234daf2
neverland <jait.chen@foxmail.com>
2024-02-25 20:48:38 +0800

    test: skip image SSR test case (#12644)

--- packages/vant/src/image/test/demo-ssr.skip.ts
+++ packages/vant/src/image/test/demo-ssr.skip.ts
@@ -1,6 +1,9 @@
 /**
  * @vitest-environment node
  */
+// TODO
+// enable this case and update snapshot after bump Vue new version
+// see: https://github.com/vuejs/core/pull/10397
 import Demo from '../demo/index.vue';
 import { snapshotDemo } from '../../../test/demo';
 
