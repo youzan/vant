0b0e343aaa26421498b522f13899c48a97a500a5
qiang <qw13131wang@gmail.com>
2024-09-11 11:51:47 +0800

    feat: use useId from vue

--- packages/vant/src/composables/use-id.ts
+++ packages/vant/src/composables/use-id.ts
@@ -1,25 +1,16 @@
-import { getCurrentInstance, inject } from 'vue';
-
-const initial = { current: 0 };
-const ID_INJECTION_KEY = 'van-id-injection';
+import { getCurrentInstance, useId as _useId } from 'vue';
 
 export function useId() {
   const vm = getCurrentInstance();
-  const idInjection = getCurrentInstance()
-    ? inject(ID_INJECTION_KEY, initial)
-    : initial;
   const { name = 'unknown' } = vm?.type || {};
+  if (vm) {
+    vm.appContext.config.idPrefix = name;
+  }
 
   // keep test snapshot stable
   if (process.env.NODE_ENV === 'test') {
     return name;
   }
 
-  if (typeof window === 'undefined' && !inject(ID_INJECTION_KEY)) {
-    console.warn(
-      '[vant useId] Looks like you are using server rendering, we suggest injecting an initial value for the ID. eg: app.provide("van-id-injection", { current: 0 })',
-    );
-  }
-
-  return `${name}-${++idInjection.current}`;
+  return _useId();
 }
