c15d6e6ce4b46990cdb82675acd126ca77cd8304
lizhen <1154651716@qq.com>
2024-10-28 12:45:53 +0800

    fix(tabBarItem): issues：TabbarItem component in the route mode fails to bind with v-model

--- packages/vant/src/tabbar-item/TabbarItem.tsx
+++ packages/vant/src/tabbar-item/TabbarItem.tsx
@@ -59,11 +59,17 @@ export default defineComponent({
         const { $route } = vm;
         const { to } = props;
         const config = isObject(to) ? to : { path: to };
-        return !!$route.matched.find((val) => {
-          const pathMatched = 'path' in config && config.path === val.path;
-          const nameMatched = 'name' in config && config.name === val.name;
-          return pathMatched || nameMatched;
-        });
+        const pathMatched =
+          'path' in config &&
+          $route.matched.some((val) => config.path === val.path);
+        const nameMatched =
+          'name' in config &&
+          $route.matched.some((val) => config.name === val.name);
+        const isActive = pathMatched || nameMatched;
+        if (isActive) {
+          parent.setActive(props.name ?? index.value, () => {});
+        }
+        return isActive;
       }
 
       return (props.name ?? index.value) === modelValue;
