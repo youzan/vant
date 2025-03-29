e36e8faf6eeb178b5671f9cfad4694cdefbbabad
neverland <chenjiahan.jait@bytedance.com>
2025-03-29 18:12:09 +0800

    test: fix Vitest segmentation fault

--- packages/vant/vitest.config.ts
+++ packages/vant/vitest.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import vitePluginVue from '@vitejs/plugin-vue';
 import vitePluginJsx from '@vitejs/plugin-vue-jsx';
-import { cpus } from 'os';
-
-const cpuNum = Math.max(cpus().length - 1, 1);
 
 export default defineConfig({
   test: {
@@ -19,13 +16,6 @@ export default defineConfig({
       ],
       reporter: ['lcov', 'text-summary'],
       reportsDirectory: './test/coverage',
-    },
-    pool: 'vmThreads',
-    poolOptions: {
-      vmThreads: {
-        // limit the memory to avoid OOM
-        memoryLimit: Math.min(1 / (cpuNum * 2), 0.1),
-      },
     },
     environment: 'jsdom',
     include: ['src/**/*.spec.[jt]s?(x)'],
