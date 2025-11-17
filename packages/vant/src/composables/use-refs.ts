c3a907b47f2791ce7079d206ad606c4d65d1d60a
黄帆 <fan.huang@haiziwang.com>
2025-11-17 17:45:43 +0800

    fix(Tabs): incorrect titleRefs status

--- packages/vant/src/composables/use-refs.ts
+++ packages/vant/src/composables/use-refs.ts
@@ -1,17 +1,17 @@
-import { ref, Ref, onBeforeUpdate } from 'vue';
+import { ref, Ref } from 'vue';
 
 export function useRefs<T = Element>() {
   const refs = ref([]) as Ref<T[]>;
   const cache: Array<(el: unknown) => void> = [];
 
-  onBeforeUpdate(() => {
-    refs.value = [];
-  });
-
   const setRefs = (index: number) => {
     if (!cache[index]) {
       cache[index] = (el: unknown) => {
-        refs.value[index] = el as T;
+        if (el) {
+          refs.value[index] = el as T;
+        } else {
+          delete refs.value[index];
+        }
       };
     }
     return cache[index];
