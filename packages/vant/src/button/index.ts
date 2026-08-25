15e7d9b80ea590ea4ef5f1ce2b920a88bb36397f
junwei <gavinwjw@163.com>
2026-08-25 15:58:00 +0400

    feat(ButtonGroup): support group component

--- packages/vant/src/button/index.ts
+++ packages/vant/src/button/index.ts
@@ -1,10 +1,15 @@
 import { withInstall } from '../utils';
 import _Button from './Button';
+import _ButtonGroup from './ButtonGroup';
 
 export const Button = withInstall(_Button);
+export const ButtonGroup = withInstall(_ButtonGroup);
 export default Button;
 export { buttonProps } from './Button';
+export { buttonGroupProps, BUTTON_GROUP_KEY } from './ButtonGroup';
+
 export type { ButtonProps } from './Button';
+export type { ButtonGroupProps, ButtonGroupProvide } from './ButtonGroup';
 export type {
   ButtonType,
   ButtonSize,
@@ -16,5 +21,6 @@ export type {
 declare module 'vue' {
   export interface GlobalComponents {
     VanButton: typeof Button;
+    VanButtonGroup: typeof ButtonGroup;
   }
 }
