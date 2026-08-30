e66000425a11c7fcff341dd9116391d36510a387
neverland <chenjiahan.jait@bytedance.com>
2026-08-30 10:25:38 +0800

    chore: disable new rslint rules

--- rslint.config.ts
+++ rslint.config.ts
@@ -11,8 +11,10 @@ export default defineConfig([
     },
     rules: {
       'no-undef': 'off',
+      'no-useless-assignment': 'off',
       '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/no-unused-expressions': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
     },
